f8e68e00bafd963b11572464bc1e96dfb34cf8fa
Jason Cater <jason.cater@ucsf.edu>
2016-10-19 16:15:53 -0700

    Adding support for the 'redis_index' variable, in the Redis Store.

--- server/service/redis_service.rb
+++ server/service/redis_service.rb
@@ -5,6 +5,28 @@ class RedisService
   def initialize()
 
     @redis = Redis.new(:host => 'localhost', :port => 6379, :db => 0)
+    
+    if @redis.get('index') == nil
+
+      # Replace this with a bit of code that finds the maximum index value in
+      # the keys.
+      @redis.set('index', 0)
+    end 
+  end
+
+  def get_new_index()
+
+    @redis.incr('index')
+    index = @redis.get('index')
+    index = index.to_i.to_s(16)
+    
+    if(index.length < 8)
+      
+      pad_length = 8 - index.length
+      index = ('0' * pad_length) + index
+    end
+
+    return index
   end
 
   def retrive_file_status(key)
