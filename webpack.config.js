72a7a57b25ad83174ae62bf86f3dd152d292a298
Jason Cater <jason.cater@ucsf.edu>
2017-01-05 12:44:46 -0800

    Adding webpack config file for more complex bundling.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/*
+ * This file is used by webpack to package different components into separate
+ * end points needed by the application. In this way we can keep all of our JS
+ * modular and just drop application components where we need them and add a
+ * line here for packaging.
+ *
+ * This file can also be viewed as describing the different JS sub applications
+ * that are used in totality.
+ */
+
+module.exports = {
+
+  'entry': {
+
+    'metis-main': './client/js/metis-uploader.js',
+    'admin-main': './client/js/admin-console.js'
+  },
+  'output': {
+
+    'path': './client/js',
+    'filename': '[name].bundle.js'
+  }
+}
