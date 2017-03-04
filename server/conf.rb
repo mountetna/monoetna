700bef425623a53f04db27759b98492edc02fb54
Jason Cater <jason.cater@ucsf.edu>
2017-03-03 18:55:03 -0800

    Adding the error/warn codes

--- server/conf.rb
+++ server/conf.rb
@@ -1,4 +1,17 @@
 module Conf
 
-  TEMP = 'temp'
+  JANUS_ADDR = 'http://janus-dev.ucsf.edu'
+
+  # Warning and error messages that will end up in the log
+  WARNS = [
+
+    :PARAMS_NOT_PRESENT,  # 0
+    :NOT_ADMIN_USER      # 1
+  ]
+
+  ERRORS = [
+
+    :JANUS_SERVER_ERROR, # 0
+    :CONNECTION_ERROR    # 1
+  ]
 end
