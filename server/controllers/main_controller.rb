3c2483fd36d2f9a808a21090ab78ceb4c20bf5e3
Jason Cater <jason.cater@ucsf.edu>
2017-04-05 13:37:41 -0700

    Addig code to pull active uploads.

--- server/controllers/main_controller.rb
+++ server/controllers/main_controller.rb
@@ -12,6 +12,7 @@ class MainController < BasicController
 
     project_names = extract_project_names()
     file_list = pull_file_metadata(project_names)
+    file_list = pull_upload_metadata(file_list)
     { :success=> true, :file_list=> file_list }
   end
 
@@ -29,9 +30,22 @@ class MainController < BasicController
     file_metadata = []
     project_names.each do |project_name|
 
-      #puts project_name
+      project_files = PostgresService::get_files_by_project_name(project_name)
+      if project_files then file_metadata.concat(project_files) end
     end
+    return file_metadata
+  end
 
-    return []
+  def pull_upload_metadata(file_list)
+
+    file_list.map do |file|
+
+      if file[:upload_by] == @user.email()
+
+        upload = FileModel::Upload[:file_id=> file[:file_id]]
+        if upload then file.concat(upload) end
+      end
+      file
+    end
   end
 end
