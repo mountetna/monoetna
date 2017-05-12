ba98c55b412838f6521fc4881bf30f25da5549f8
Jason Cater <jason.cater@ucsf.edu>
2017-05-12 13:46:21 -0700

    Fixing a missing comma.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   'output': {
 
-    'path': path.resolve(__dirname, './client/js')
+    'path': path.resolve(__dirname, './client/js'),
     'filename': '[name].bundle.js'
   }
 }
