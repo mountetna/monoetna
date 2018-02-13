8df6f23c330937c12d7ac9bcbbc99958e30aec1d
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-02-13 01:52:18 +0000

    simplify main_controller

--- lib/models/upload.rb
+++ lib/models/upload.rb
@@ -2,8 +2,13 @@ class Metis
   class Upload < Sequel::Model
     many_to_one :file
 
-    def self.upload_started?(project_name, file_name)
-      !self.where(project_name: project_name, file_name: file_name).empty?
+    def to_json
+      [ :status, :current_byte_position, :current_blob_size, :next_blob_size, :next_blob_hash ].map do |s|
+        [ s, send(s) ]
+      end.to_h.merge(
+        project_name: file.project_name,
+        file_name: file.file_name
+      ).to_json
     end
   end
 end
--- lib/server/controllers/main_controller.rb
+++ lib/server/controllers/main_controller.rb
@@ -1,54 +1,7 @@
 class MainController < Metis::Controller
-  def run()
-    set_user()
-    raise_err(:BAD_REQ, 3, __method__) if !@user.valid?()
-
-    return send(@action).to_json()
-  end
-
-  def retrieve_files()
-
-    project_names = extract_project_names()
-    file_list = pull_file_metadata(project_names)
-    file_list = pull_upload_metadata(file_list)
-    { :success=> true, :file_list=> file_list }
-  end
-
-  private
-  def extract_project_names()
-
-    @user.permissions.map() do |permission|
-
-      permission['project_name']
-    end
-  end
-
-  def pull_file_metadata(project_names)
-
-    file_metadata = []
-    project_names.each() do |project_name|
-
-      project_files = PostgresService::get_files_by_project_name(project_name)
-      if project_files then file_metadata.concat(project_files) end
-    end
-    return file_metadata
-  end
-
-  def pull_upload_metadata(file_list)
-
-    file_list.map() do |file|
-
-      if file[:upload_by] == @user.email()
-
-        upload = FileModel::Upload[:file_id=> file[:id]]
-        if upload 
-
-          file = file.merge(upload.to_hash)
-          file[:status] = 'failed'
-          file.delete(:finish_upload)
-        end
-      end
-      file
-    end
+  def retrieve_files
+    files = Metis::Files.where(project_name: @user.projects).all
+    uploads = files.select { |f| f.uploader == @user.email && f.upload }.map(&:upload)
+    success({ files: files, uploads: uploads }.to_json, 'application/json')
   end
 end
