28b09cfef577d7c64eea0c119a3aeac441ee75f3
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-11-02 17:09:55 +0000

    simplify header style

--- lib/client/jsx/components/nav/title-bar.jsx
+++ lib/client/jsx/components/nav/title-bar.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 const TitleBar = (props) => 
   <div id='title-menu'>
     <button className='title-menu-title'>
-      Metis : File Browser
+      Metis
     </button>
   </div>;
 
