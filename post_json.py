730468707fb51f20667ae71f46f21ad780d634ca
Leoard Lupin-Jimenez <lupified@gmail.com>
2019-11-22 09:15:14 -0800

    post_json test script

---
+++ post_json.py
@@ -0,0 +1,23 @@
+# importing the requests library 
+import requests 
+import json
+import pprint
+
+# defining the api-endpoint  
+server_url = "http://127.0.0.1:5000/json/"
+  
+# your source code here 
+post_dict = {"func" : "manifesto",
+             "args" : []}
+
+post_json = json.dumps(post_dict)
+
+# data to be sent to api 
+data = post_json
+  
+# sending post request and saving response as response object 
+r = requests.post(url = server_url, data = data) 
+
+# extracting response text  
+pprint.pprint(json.loads(r.text))
+print("return type: ", type(json.loads(r.text)))
