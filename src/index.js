28b01b042b35864f0b7916c7ed097936d73a8b16
Cole S <4930129+coleshaw@users.noreply.github.com>
2020-06-10 18:46:04 +0000

    clean up and add README

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import UploadControl from './jsx/components/upload-control';
-import UploadMeter from './jsx/components/upload-meter';
-import * as UploadActions from './jsx/actions/upload_actions';
-import workDispatcher from './jsx/dispatchers/work-dispatcher';
-
-export { UploadControl, UploadMeter, UploadActions, workDispatcher };
