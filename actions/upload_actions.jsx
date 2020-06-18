9fbdf2875c816aa95fff2ee209f7f9774ee42516
Cole S <4930129+coleshaw@users.noreply.github.com>
2020-06-18 16:46:47 +0000

    add missing UPLOAD_SPEED action

--- actions/upload_actions.jsx
+++ actions/upload_actions.jsx
@@ -11,6 +11,7 @@ export const UNQUEUE_UPLOADS = 'UNQUEUE_UPLOADS';
 export const CANCEL_UPLOAD = 'CANCEL_UPLOAD';
 export const CONTINUE_UPLOAD = 'CONTINUE_UPLOAD';
 export const PAUSE_UPLOAD = 'PAUSE_UPLOAD';
+export const UPLOAD_SPEED = 'UPLOAD_SPEED';
 export const WORK_FAILED = 'WORK_FAILED';
 
 
