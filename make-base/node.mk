5f830ea5fac9776e18db7436a4156b8fc14926b4
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-07-02 16:31:51 -0700

    Fix for setup-links

--- make-base/node.mk
+++ make-base/node.mk
@@ -1,6 +1,3 @@
 app_service_name=${app_name}_app
 test::
 				@ docker-compose run -e RUN_NPM_INSTALL=1 -e SKIP_RUBY_SETUP=1 --rm ${app_service_name} npm test
-
-setup-links::
-	@ docker-compose run -e SKIP_RUBY_SETUP=1 --rm ${app_service_name} [ -e ../etna ] && npm link ../etna/packages/etna-js || true
