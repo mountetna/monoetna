9af6f8ccd1b8caec1624a6fc92dcdbdadc3db2b3
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-07-02 16:30:28 -0700

    Be more lenient on setting up links

--- make-base/node.mk
+++ make-base/node.mk
@@ -3,4 +3,4 @@ test::
 				@ docker-compose run -e RUN_NPM_INSTALL=1 -e SKIP_RUBY_SETUP=1 --rm ${app_service_name} npm test
 
 setup-links::
-	@ docker-compose run -e SKIP_RUBY_SETUP=1 --rm ${app_service_name} [ -e ../etna ] && npm link ../etna/packages/etna-js
+	@ docker-compose run -e SKIP_RUBY_SETUP=1 --rm ${app_service_name} [ -e ../etna ] && npm link ../etna/packages/etna-js || true
