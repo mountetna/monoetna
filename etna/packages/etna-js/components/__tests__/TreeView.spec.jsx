00e18d74a677949649afe51e8844ea66e396897d
Cole <4930129+coleshaw@users.noreply.github.com>
2020-07-17 16:18:22 -0400

    update etna-js expected test output

--- etna/packages/etna-js/components/__tests__/TreeView.spec.jsx
+++ etna/packages/etna-js/components/__tests__/TreeView.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import TreeView, {getSelectedLeaves} from "../TreeView";
+import TreeView, {getSelectedLeaves} from '../TreeView';
 
 class TreeViewTest {
   constructor() {
@@ -10,19 +10,33 @@ class TreeViewTest {
   }
 
   get mounted() {
-    return (this._mounted = this._mounted || mount(<TreeView options={this.options} selectable={this.selectable}
-                                                             onSelectionsChange={this.onSelectionsChange}/>));
+    return (this._mounted =
+      this._mounted ||
+      mount(
+        <TreeView
+          options={this.options}
+          selectable={this.selectable}
+          onSelectionsChange={this.onSelectionsChange}
+        />
+      ));
   }
 
-
   findInput(path, node) {
-    return this.mounted.find('input[type="checkbox"]').findWhere(
-      i => i.props()['data-path'].join(',') === path.join(',') &&
-        node === i.props()['data-node']);
+    return this.mounted
+      .find('input[type="checkbox"]')
+      .findWhere(
+        (i) =>
+          i.props()['data-path'].join(',') === path.join(',') &&
+          node === i.props()['data-node']
+      );
   }
 
   get selected() {
-    return this.mounted.find('input[type="checkbox"]').getElements().filter(e => e.props.checked).map(e => e.props['data-path'].concat([e.props['data-node']]))
+    return this.mounted
+      .find('input[type="checkbox"]')
+      .getElements()
+      .filter((e) => e.props.checked)
+      .map((e) => e.props['data-path'].concat([e.props['data-node']]));
   }
 
   get lastSelectionsByCallback() {
@@ -38,16 +52,18 @@ describe('TreeView', () => {
   describe('with some options', () => {
     beforeEach(() => {
       test.options = [
-        ['Top Level 1', [
-          ['Inner 1'],
-          ['Inner 2', [
-            ['Inner Inner 1'],
-            ['Inner Inner 2'],
-            ['Inner Inner 3'],
-          ]],
-          ['Inner 3'],
-        ]],
-        ['Top Level 2'],
+        [
+          'Top Level 1',
+          [
+            ['Inner 1'],
+            [
+              'Inner 2',
+              [['Inner Inner 1'], ['Inner Inner 2'], ['Inner Inner 3']]
+            ],
+            ['Inner 3']
+          ]
+        ],
+        ['Top Level 2']
       ];
     });
 
@@ -60,57 +76,57 @@ describe('TreeView', () => {
         ['Top Level 1', 'Inner 2', 'Inner Inner 2'],
         ['Top Level 1', 'Inner 2', 'Inner Inner 3'],
         ['Top Level 1', 'Inner 3'],
-        ['Top Level 2'],
+        ['Top Level 2']
       ]);
     });
 
     describe('selecting a top level', () => {
       beforeEach(() => {
         test.findInput([], 'Top Level 1').simulate('change');
-      })
+      });
 
       it('disables recursively child nodes', () => {
-        expect(test.selected).toEqual([[
-          'Top Level 2',
-        ]]);
+        expect(test.selected).toEqual([['Top Level 2']]);
 
-        expect(test.lastSelectionsByCallback).toEqual([
-          'Top Level 2',
-        ])
+        expect(test.lastSelectionsByCallback).toEqual(['Top Level 2']);
       });
 
       describe('selecting each leaf', () => {
         it('continually enables those leaves and eventually the parents again, too', () => {
-          test.findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 1').simulate('change');
+          test
+            .findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 1')
+            .simulate('change');
           expect(test.selected).toEqual([
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 1' ],
-            [ 'Top Level 2', ]
+            ['Top Level 1', 'Inner 2', 'Inner Inner 1'],
+            ['Top Level 2']
           ]);
           expect(test.lastSelectionsByCallback).toEqual([
             'Inner Inner 1',
-            'Top Level 2',
-          ])
+            'Top Level 2'
+          ]);
 
-          test.findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 2').simulate('change');
+          test
+            .findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 2')
+            .simulate('change');
           expect(test.selected).toEqual([
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 1' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 2' ],
-            [ 'Top Level 2', ]
+            ['Top Level 1', 'Inner 2', 'Inner Inner 1'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 2'],
+            ['Top Level 2']
           ]);
           expect(test.lastSelectionsByCallback).toEqual([
             'Inner Inner 1',
             'Inner Inner 2',
-            'Top Level 2',
-          ])
+            'Top Level 2'
+          ]);
 
           test.findInput(['Top Level 1'], 'Inner 1').simulate('change');
           test.findInput(['Top Level 1'], 'Inner 3').simulate('change');
           expect(test.selected).toEqual([
-            [ 'Top Level 1', 'Inner 1' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 1' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 2' ],
-            [ 'Top Level 1', 'Inner 3' ],
-            [ 'Top Level 2', ]
+            ['Top Level 1', 'Inner 1'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 1'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 2'],
+            ['Top Level 1', 'Inner 3'],
+            ['Top Level 2']
           ]);
 
           expect(test.lastSelectionsByCallback).toEqual([
@@ -118,19 +134,21 @@ describe('TreeView', () => {
             'Inner Inner 1',
             'Inner Inner 2',
             'Inner 3',
-            'Top Level 2',
-          ])
+            'Top Level 2'
+          ]);
 
-          test.findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 3').simulate('change');
+          test
+            .findInput(['Top Level 1', 'Inner 2'], 'Inner Inner 3')
+            .simulate('change');
           expect(test.selected).toEqual([
-            [ 'Top Level 1' ],
-            [ 'Top Level 1', 'Inner 1' ],
-            [ 'Top Level 1', 'Inner 2' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 1' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 2' ],
-            [ 'Top Level 1', 'Inner 2', 'Inner Inner 3' ],
-            [ 'Top Level 1', 'Inner 3' ],
-            [ 'Top Level 2', ]
+            ['Top Level 1'],
+            ['Top Level 1', 'Inner 1'],
+            ['Top Level 1', 'Inner 2'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 1'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 2'],
+            ['Top Level 1', 'Inner 2', 'Inner Inner 3'],
+            ['Top Level 1', 'Inner 3'],
+            ['Top Level 2']
           ]);
           expect(test.lastSelectionsByCallback).toEqual([
             'Inner 1',
@@ -138,10 +156,10 @@ describe('TreeView', () => {
             'Inner Inner 2',
             'Inner Inner 3',
             'Inner 3',
-            'Top Level 2',
-          ])
+            'Top Level 2'
+          ]);
         });
-      })
+      });
 
       describe('and then re-selecting that top level', () => {
         beforeEach(() => {
@@ -157,18 +175,19 @@ describe('TreeView', () => {
             ['Top Level 1', 'Inner 2', 'Inner Inner 2'],
             ['Top Level 1', 'Inner 2', 'Inner Inner 3'],
             ['Top Level 1', 'Inner 3'],
-            ['Top Level 2'],
+            ['Top Level 2']
           ]);
 
           expect(test.lastSelectionsByCallback).toEqual([
+            'Inner 1',
             'Inner Inner 1',
             'Inner Inner 2',
             'Inner Inner 3',
             'Inner 3',
-            'Top Level 2',
-          ])
+            'Top Level 2'
+          ]);
         });
-      })
+      });
     });
-  })
-})
+  });
+});
