fe679bc065ce82508dba90d7e14c85aa9edd3d6d
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-07-30 09:34:45 -0700

    WIP

--- make-base/docker-compose.mk
+++ make-base/docker-compose.mk
@@ -1,29 +1,34 @@
 export COMPOSE_PROJECT_NAME=monoetna
 app_service_name=${app_name}_app
 
-.docker-build-mark: $(wildcard docker/**/*) docker-compose.yml
-				@ $(MAKE) build
+prepare-compose::
+	export COMPOSE_MIXINS="docker-compose.shared.yml $COMPOSE_MIXINS"
 
-up:: .docker-build-mark
+docker-compose.yml:: prepare-compose $(wildcard ../docker/*.shared.yml) ../docker/default-compose
+	../docker/default-compose docker-compose.yml
+
+.PHONY: images
+images: docker-compose.yml
+	../docker/build_images_of docker-compose.yml
+
+docker-ready:: docker-compose.yml images
+
+up:: docker-ready
 				@ docker-compose up -d
 
-down::
+down:: docker-compose.yml
 				@ docker-compose down --remove-orphans
 
 ps::
 				@ docker-compose ps
 
-build::
-				@ docker-compose build
-				@ touch .docker-build-mark
-
-restart::
+restart:: docker-ready
 				@ docker-compose restart
 
-bash::
+bash:: docker-ready
 				@ docker-compose run -e SKIP_RUBY_SETUP=1 --rm $(app_service_name) bash
 
-prepare::
+install-into-volumes:: docker-ready
 				@ docker-compose run -e RUN_NPM_INSTALL=1 --rm $(app_service_name) echo 1
 
 logs::
