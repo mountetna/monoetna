0b59899c2a6b0b2d4c5bbadc5bfdbc314f0e2fe9
GithubAction <mountetna@users.noreply.github.com>
2020-07-30 13:20:36 +0000

    Artifact npm-etna-js for sha c379f564de4c76ea248d733c2e30bdfa0c2f052b

---
+++ upload/workers/__tests__/worker.spec.jsx
@@ -0,0 +1,20 @@
+import { setupWorker } from '../worker';
+
+describe('setupWorker', () => {
+  it('prepares a dispatch and subscribe', () => {
+    const mockExecute = jest.fn();
+    const mockWorker = {
+      postMessage: jest.fn(),
+      addEventListener: jest.fn()
+    };
+
+    const { dispatch, subscribe } = setupWorker(mockWorker);
+    const data = [];
+    dispatch(data)
+    expect(mockWorker.postMessage).toHaveBeenCalledWith(data);
+
+    expect(mockWorker.addEventListener).not.toHaveBeenCalled();
+    subscribe(mockExecute)
+    expect(mockWorker.addEventListener).toHaveBeenCalled();
+  });
+});
