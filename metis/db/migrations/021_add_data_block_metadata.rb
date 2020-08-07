55fb3575a4fc17e94ca2ac576cccd8015e1b7bf1
Zachary Collins <recursive.cookie.jar@gmail.com>
2020-08-07 13:59:11 -0700

    Force table name

--- metis/db/migrations/021_add_data_block_metadata.rb
+++ metis/db/migrations/021_add_data_block_metadata.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   up do
-    create_table(:data_block_metadatas) do
+    create_table(:data_block_metadata) do
       primary_key :id
 
       foreign_key :data_block_id, :data_blocks, null: false
@@ -10,13 +10,13 @@ Sequel.migration do
       DateTime :updated_at, null: false
     end
 
-    alter_table(:data_block_metadatas) do
+    alter_table(:data_block_metadata) do
       add_index [:key, :data_block_id], name: "data_block_metadata_key_data_block_id_idx", unique: true
     end
   end
 
   down do
-    drop_table(:data_block_metadatas)
+    drop_table(:data_block_metadata)
     drop_index "data_block_metadata_key_data_block_id_idx"
   end
 end
--- metis/lib/models/data_block_metadata.rb
+++ metis/lib/models/data_block_metadata.rb
@@ -1,5 +1,5 @@
 class Metis
-  class DataBlockMetadata < Sequel::Model
+  class DataBlockMetadata < Sequel::Model(:data_block_metadata)
     plugin :timestamps, update_on_create: true
     many_to_one :data_block
   end
