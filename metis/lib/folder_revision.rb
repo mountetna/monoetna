1dbbae79f4f88efc0af6d13f7fbe2c4a423c128b
Cole <4930129+coleshaw@users.noreply.github.com>
2020-08-14 14:38:24 -0400

    clarify some methods

--- metis/lib/folder_revision.rb
+++ metis/lib/folder_revision.rb
@@ -37,14 +37,19 @@ class Metis
     private
 
     def full_folder_path(mpath_w_objs)
+      # Because of how the MetisPath regex works, the actual
+      #   folder name gets returned as the mpath.file_name, so
+      #   here we have to reconstruct the entire path by concatenating
+      #   mpath.folder_path + mpath.file_name.
       mpath_w_objs.mpath.folder_path ?
         "#{mpath_w_objs.mpath.folder_path}/#{mpath_w_objs.mpath.file_name}" :
         mpath_w_objs.mpath.file_name
     end
 
-    def validate_target_folder(mpath_w_objs, folder_check_type='source')
+    def validate_source_folder(mpath_w_objs)
       # NOTE: the "mpath_w_objs.folder" is the parent folder, not the
-      #   actual folder, so we have to account for that in the validations.
+      #   actual folder, so we have to account for that in the validations and
+      #   can't use the superclass #validate_folder()
       errors_found = false
 
       target_folder = Metis::Folder.from_path(
@@ -56,7 +61,7 @@ class Metis
         errors_found = true
       end
 
-      if target_folder&.read_only? && folder_check_type == 'source'
+      if target_folder&.read_only?
         @errors.push("Folder \"#{mpath_w_objs.mpath.path}\" is read-only")
         errors_found = true
       end
@@ -64,8 +69,11 @@ class Metis
       return errors_found
     end
 
-    def validate_dest_file(mpath_w_objs)
-      # Because we don't want users overwriting files with a folder
+    def validate_dest_is_not_file(mpath_w_objs)
+      # Because we don't want users overwriting files with a folder.
+      # Since we just need to check for file existence and don't
+      #   care about the file.read_only? flag, we can't use
+      #   the superclass #validate_file() method.
       errors_found = false
 
       file = Metis::File.from_folder(
@@ -84,7 +92,7 @@ class Metis
     def validate_source (source_mpath_w_objs)
       return unless validate_mpath(source_mpath_w_objs.mpath)
       return unless validate_bucket(source_mpath_w_objs)
-      return unless validate_target_folder(source_mpath_w_objs, 'source')
+      return unless validate_source_folder(source_mpath_w_objs)
       return unless validate_folder(source_mpath_w_objs, 'source')
     end
 
@@ -92,7 +100,7 @@ class Metis
       return unless validate_mpath(dest_mpath_w_objs.mpath)
       return unless validate_bucket(dest_mpath_w_objs)
       return unless validate_folder(dest_mpath_w_objs, 'dest')
-      return unless validate_dest_file(dest_mpath_w_objs)
+      return unless validate_dest_is_not_file(dest_mpath_w_objs)
     end
   end
 end
