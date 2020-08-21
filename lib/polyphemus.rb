44205689169c1b89767b458ceaa0d51bea0d45b5
Zach Collins <recursive.cookie.jar@gmail.com>
2020-08-21 14:30:26 -0700

    Zc/cascade restricted (#66)
    Adding Command to handle mvir1 restrictions

--- lib/polyphemus.rb
+++ lib/polyphemus.rb
@@ -1,10 +1,15 @@
 require_relative 'commands'
 require 'etna/clients/magma'
+require 'etna/clients/metis'
 
 class Polyphemus
   include Etna::Application
 
-  def magma_client
-    @magma_client ||= Etna::Clients::Magma.new({ token: ENV['TOKEN'] }.update(config(:magma)))
+  def magma_client(token = ENV['TOKEN'])
+    Etna::Clients::Magma.new(token: token, host: config(:magma)[:host])
+  end
+
+  def metis_client(token = ENV['TOKEN'])
+    Etna::Clients::Metis.new(token: token, host: config(:metis)[:host])
   end
 end
