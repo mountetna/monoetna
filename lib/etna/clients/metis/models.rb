ee15b02a982c5e72907ecc59bd44672d6d39d0d4
GithubAction <mountetna@users.noreply.github.com>
2020-08-26 17:44:15 +0000

    Artifact gem-etna for sha 861fea046e4446c448c1010b1bee77f639427a90

---
+++ lib/etna/clients/metis/models.rb
@@ -0,0 +1,142 @@
+require_relative '../../json_serializable_struct'
+
+
+module Etna
+  module Clients
+    class Metis
+      class ListFoldersRequest < Struct.new(:project_name, :bucket_name, :offset, :limit, keyword_init: true)
+        include JsonSerializableStruct
+
+        def initialize(**params)
+          super({}.update(params))
+        end
+
+        def to_h
+          # The :project_name comes in from Polyphemus as a symbol value,
+          #   we need to make sure it's a string because it's going
+          #   in the URL.
+          super().compact.transform_values(&:to_s)
+        end
+      end
+
+      class RenameFolderRequest < Struct.new(:project_name, :bucket_name, :folder_path, :new_bucket_name, :new_folder_path, keyword_init: true)
+        include JsonSerializableStruct
+
+        def initialize(**params)
+          super({}.update(params))
+        end
+
+        def to_h
+          # The :project_name comes in from Polyphemus as a symbol value,
+          #   we need to make sure it's a string because it's going
+          #   in the URL.
+          super().compact.transform_values(&:to_s)
+        end
+      end
+
+      class ListFolderRequest < Struct.new(:project_name, :bucket_name, :folder_path, keyword_init: true)
+        include JsonSerializableStruct
+
+        def initialize(**params)
+          super({}.update(params))
+        end
+
+        def to_h
+          # The :project_name comes in from Polyphemus as a symbol value,
+          #   we need to make sure it's a string because it's going
+          #   in the URL.
+          super().compact.transform_values(&:to_s)
+        end
+      end
+
+      class CreateFolderRequest < Struct.new(:project_name, :bucket_name, :folder_path, keyword_init: true)
+        include JsonSerializableStruct
+
+        def initialize(**params)
+          super({}.update(params))
+        end
+
+        def to_h
+          # The :project_name comes in from Polyphemus as a symbol value,
+          #   we need to make sure it's a string because it's going
+          #   in the URL.
+          super().compact.transform_values(&:to_s)
+        end
+      end
+
+      class FoldersResponse
+        attr_reader :raw
+
+        def initialize(raw = {})
+          @raw = raw
+        end
+
+        def folders
+          Folders.new(raw[:folders])
+        end
+      end
+
+      class FoldersAndFilesResponse < FoldersResponse
+        def files
+          Files.new(raw[:files])
+        end
+      end
+
+      class Files
+        attr_reader :raw
+
+        def initialize(raw = {})
+          @raw = raw
+        end
+
+        def all
+          raw.map { |file| File.new(file) }
+        end
+      end
+
+      class Folders
+        attr_reader :raw
+
+        def initialize(raw = {})
+          @raw = raw
+        end
+
+        def all
+          raw.map { |folder| Folder.new(folder) }
+        end
+      end
+
+      class File
+        attr_reader :raw
+
+        def initialize(raw = {})
+          @raw = raw
+        end
+
+        def file_path
+          raw[:file_path]
+        end
+
+        def file_name
+          raw[:file_name]
+        end
+      end
+
+      class Folder
+        attr_reader :raw
+
+        def initialize(raw = {})
+          @raw = raw
+        end
+
+        def folder_path
+          raw[:folder_path]
+        end
+
+        def bucket_name
+          raw[:bucket_name]
+        end
+      end
+    end
+  end
+end
