f1633c39a174e4eb419784d5dd8de63683a52992
Cole <4930129+coleshaw@users.noreply.github.com>
2020-09-03 13:19:05 -0400

    use loop instead of recursion for finding folder path

--- metis/lib/folder_path_calculator.rb
+++ metis/lib/folder_path_calculator.rb
@@ -18,16 +18,22 @@ class Metis
     end
 
     def get_folder_path(folder)
-      # No parent folder, so just return this (root) folder's folder_name
-      return folder.folder_name if !folder.folder_id
+      path = [folder.folder_name]
+      folder_to_check = folder
+      loop do
+        # No parent folder, so just return this (root) folder's folder_name
+        break if !folder_to_check.folder_id
 
-      # Use the cached path value for the parent folder if it exists
-      return "#{@path_cache[folder.folder_id.to_s.to_sym]}/#{folder.folder_name}" if
-        @path_cache.has_key?(folder.folder_id.to_s.to_sym)
+        # Use the cached path value for the parent folder if it exists
+        return path.unshift(@path_cache[folder_to_check.folder_id.to_s.to_sym]) if
+          @path_cache.has_key?(folder_to_check.folder_id.to_s.to_sym)
 
-      # Find the path for the parent folder, recursively
-      parent_folder = @folders_by_id[folder.folder_id].first
-      "#{get_folder_path(parent_folder)}/#{folder.folder_name}"
+        # Find the path for the parent folder, recursively
+        parent_folder = @folders_by_id[folder_to_check.folder_id].first
+        path.unshift(parent_folder.folder_name)
+        folder_to_check = parent_folder
+      end
+      path.join('/')
     end
   end
 end
