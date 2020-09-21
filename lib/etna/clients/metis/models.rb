d697bf2b02ba6cd4be7d0a7cd287c229e372ea27
GithubAction <mountetna@users.noreply.github.com>
2020-09-21 21:43:40 +0000

    Squashed 'etna/' changes from 698b3a87..23a30690
    23a30690 Adding commands that enable copying models between magma instances (#85)
    
    git-subtree-dir: etna
    git-subtree-split: 23a30690573a9691fc62eb72f4f3d8d4c0e75389

--- lib/etna/clients/metis/models.rb
+++ lib/etna/clients/metis/models.rb
@@ -1,5 +1,5 @@
 require_relative '../../json_serializable_struct'
-
+require 'ostruct'
 
 module Etna
   module Clients
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'bundler'
 require 'securerandom'
 require 'timecop'
 require 'webmock/rspec'
+require 'vcr'
 
 Bundler.setup(:default, :test)
 
@@ -22,6 +23,63 @@ def setup_app(server, layer=nil, config={ test: {} })
     use Etna::SymbolizeParams
     use *layer if layer
     run server.new
+  end
+end
+
+VCR.configure do |c|
+  c.hook_into :webmock
+  c.cassette_serializers
+  c.cassette_library_dir = ::File.join(__dir__,  'fixtures', 'cassettes')
+  c.allow_http_connections_when_no_cassette = true
+  c.register_request_matcher :try_body do |request_1, request_2|
+    if request_1.headers['Content-Type'].first =~ /application\/json/
+      if request_2.headers['Content-Type'].first =~ /application\/json/
+        JSON.parse(request_1.body) == JSON.parse(request_2.body)
+      else
+        false
+      end
+    else
+      request_1.body == request_2.body
+    end
+  end
+
+  c.default_cassette_options = {
+      serialize_with: :compressed,
+      record: if ENV['IS_CI']
+        :none
+      else
+        ENV['RERECORD'] ? :all : :once
+      end,
+      match_requests_on: [:method, :uri, :try_body]
+  }
+
+  # Filter the authorization headers of any request by replacing any occurrence of that request's
+  # Authorization value with <AUTHORIZATION>
+  c.filter_sensitive_data('<AUTHORIZATION>') do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
+
+  # The model synchronization workflow benefits from testing against production models, but produces a massive
+  # recording file that doesn't fit into git easily.  To assist in that, we prune some large chunks of data
+  # that are generally not useful to recording tests.  But this is fragile and assumes that no tests using vcr depend
+  # on certain response contents.  Ideal solution would be to greatly reduce the size of default magma retrieve and
+  # update_model responses to not be as noisy for large models.
+  c.filter_sensitive_data('{}') do |interaction|
+    if interaction.request.uri =~ /\/update_model/
+      interaction.response.body
+    else
+      "{}"
+    end
+  end
+
+  c.filter_sensitive_data('"options":["one-option"]') do |interaction|
+    match = interaction.response.body.match(/"options":\["ENSG0[^\]]+\]/)
+    match ? match[0] : '"options":["one-option"]'
+  end
+
+  c.filter_sensitive_data('"value":["one-option"]') do |interaction|
+    match = interaction.response.body.match(/"value":\["ENSG0[^\]]+\]/)
+    match ? match[0] : '"value":["one-option"]'
   end
 end
 
