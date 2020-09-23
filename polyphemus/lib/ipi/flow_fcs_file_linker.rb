b8dde424debe5592f9a1acc19db4921e5832aac0
Cole <4930129+coleshaw@users.noreply.github.com>
2020-09-23 16:42:40 -0400

    fix fcs file linker

--- polyphemus/lib/ipi/flow_fcs_file_linker.rb
+++ polyphemus/lib/ipi/flow_fcs_file_linker.rb
@@ -4,7 +4,7 @@ require_relative './ipi_helper'
 
 class IpiFlowFcsLinker < Etna::Clients::Magma::FileLinkingWorkflow
   def initialize(**opts)
-    super(**{bucket_name: 'integral_data', model_name: 'flow_stain'}.update(opts))
+    super(**{bucket_name: 'integral_data', model_name: 'flow'}.update(opts))
   end
 
   def find_matches
@@ -31,7 +31,6 @@ class IpiFlowFcsLinker < Etna::Clients::Magma::FileLinkingWorkflow
     # We need to return dc
     file_name = IpiHelper::file_name_from_path(fcs_file_path).sub('.fcs', '')
     regex_results = fcs_regex(file_name)
-    binding.irb
     regex_results[:stain].downcase
   end
 
@@ -58,7 +57,7 @@ class IpiFlowFcsLinker < Etna::Clients::Magma::FileLinkingWorkflow
   end
 
   def fcs_regex(file_name)
-    regex = Regexp.new('(?<sample_name>IPI\w+\d+_\w+\d)_[flow_]?(?<stain>[dc|innate|nktb|sort|treg])', Regexp::IGNORECASE)
+    regex = Regexp.new('(?<sample_name>IPI\w+\d+_\w+\d)_(?:flow_)?(?<stain>dc|innate|nktb|sort|treg)', Regexp::IGNORECASE)
     raise "FCS filename #{file_name} does not match expected regex" unless regex.match(file_name)
     regex.match(file_name)
   end
@@ -68,19 +67,33 @@ class IpiFlowFcsLinker < Etna::Clients::Magma::FileLinkingWorkflow
     super(match_data).tap do |identifiers|
       identifiers.update(match_data)
       # Below data is to get_or_create the parent records
+      identifiers['flow'] = match_data[:stain_name]
       identifiers['sample'] = sample_name_from_fcs(match_data[:fcs_file])
       identifiers['patient'] = patient_name_from_fcs(match_data[:fcs_file])
       identifiers['experiment'] = IpiHelper::experiment_from_patient_number(identifiers['patient'])
     end
   end
 
+  def revision_for(id, attribute_name, file_path, match_map, record_identifiers)
+    # Make sure the new flow record is linked correctly in the graph, and
+    #   populate the stain attribute.
+    super.update({
+      sample: record_identifiers['sample'],
+      stain: match_map[:stain]
+    })
+  end
+
   def link_files
-    magma_crud.update_records do |update_request|
-      each_revision do |id, revision|
-        puts id
-        puts revision
-        # update_request.update_revision(model_name, id, revision)
-      end
+    each_revision do |id, revision|
+      # Not using magma_crud here with bulk updates, because was getting a
+      #   vague Server Error when submitting all at once.
+      # It might have been dev-environment / proxy related, but doing individual
+      #   requests per revision for sanity's sake and to catch if it is an
+      #   issue with Metis.
+      # Have to inject project_name into the update request.
+      update_request = Etna::Clients::Magma::UpdateRequest.new(project_name: 'ipi')
+      update_request.update_revision(model_name, id, revision)
+      magma_crud.magma_client.update(update_request)
     end
   end
 end
