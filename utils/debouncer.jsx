65f5e673bc672c42f81640f3e82e01d0d98cb192
GithubAction <mountetna@users.noreply.github.com>
2020-09-23 10:49:53 +0000

    Artifact npm-etna-js for sha 23dd215f888c0e75a7391ae634406332444f6402

---
+++ utils/debouncer.jsx
@@ -0,0 +1,52 @@
+export class Debouncer {
+  constructor({eager= false, windowMs = 100, maxGating = null} = {}) {
+    this.windowMs = windowMs;
+    this.lastTimeout = null;
+    this.eager = eager;
+    this.maxGating = maxGating;
+    this.reset();
+  }
+
+  reset() {
+    this.promise = new Promise((resolve) => {
+      this.resolve = resolve;
+    });
+    this.lastTimeout = null;
+    this.lastFire = Date.now();
+    this.f = null;
+  }
+
+  ready(f = null) {
+    if (f) this.f = f;
+    clearTimeout(this.lastTimeout);
+
+    const { promise } = this;
+
+    const fire = () => {
+      const { resolve, f } = this;
+      this.reset();
+
+      if (f) f();
+      resolve();
+    };
+
+    if (this.timeout() === 0) {
+      fire();
+    }
+
+    this.lastTimeout = setTimeout(fire, this.timeout());
+
+    return promise;
+  }
+
+  timeout() {
+    if (this.lastTimeout == null && this.eager) {
+      return 0;
+    }
+
+    const remainingGatingTime = this.maxGating != null ?
+      Math.max(this.maxGating - Date.now() + this.lastFire, 0)
+      : this.windowMs;
+    return Math.min(remainingGatingTime, this.windowMs);
+  }
+}
