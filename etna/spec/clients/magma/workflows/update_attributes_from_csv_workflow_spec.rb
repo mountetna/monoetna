bacc04739182eac7a1158fa3d87d9339c6ac0708
Cole <4930129+coleshaw@users.noreply.github.com>
2020-09-29 07:22:36 -0400

    add new class to handle single-model CSV files

--- etna/spec/clients/magma/workflows/update_attributes_from_csv_workflow_spec.rb
+++ etna/spec/clients/magma/workflows/update_attributes_from_csv_workflow_spec.rb
@@ -1,7 +1,7 @@
 require 'webmock/rspec'
 require 'json'
 
-describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
+describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel do
   let(:magma_client) {Etna::Clients::Magma.new(
       token: '123',
       host: MAGMA_HOST)}
@@ -25,7 +25,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'raises exception for rows that are too short' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/magma_update_attributes_short_row.csv'
@@ -41,7 +41,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'raises exception for rows that are missing data' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/magma_update_attributes_missing_data.csv'
@@ -57,7 +57,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'raises exception for invalid models' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/magma_update_attributes_invalid_model.csv'
@@ -71,7 +71,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'sends valid revisions to magma' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/magma_update_attributes_valid.csv'
@@ -94,7 +94,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'raises exception for invalid attribute name' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/magma_update_attributes_invalid_attribute.csv'
@@ -108,7 +108,7 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
   end
 
   it 'raises exception for invalid filename' do
-    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow.new(
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowMultiModel.new(
       magma_crud: magma_crud,
       project_name: PROJECT,
       filepath: './spec/fixtures/magma/nonexistent_input.csv'
@@ -117,6 +117,106 @@ describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflow do
     expect {
       workflow.update_attributes
     }.to raise_error(StandardError)
+
+    expect(WebMock).not_to have_requested(:post, /#{MAGMA_HOST}\/update/)
+  end
+end
+
+
+describe Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel do
+  let(:magma_client) {Etna::Clients::Magma.new(
+      token: '123',
+      host: MAGMA_HOST)}
+  let(:magma_crud) {Etna::Clients::Magma::MagmaCrudWorkflow.new(
+      magma_client: magma_client, project_name: PROJECT)}
+
+  before(:each) do
+    @all_updates = []
+    stub_magma_models(
+      JSON.parse(File.read('./spec/fixtures/magma/magma_test_model.json')))
+    stub_magma_update
+  end
+
+  def updates
+    @all_updates.inject({}) do |acc, n|
+      n.keys.each do |k|
+        (acc[k] ||= {}).update(n[k])
+      end
+      acc
+    end
+  end
+
+  it 'raises exception for invalid models' do
+    expect {
+      Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel.new(
+        magma_crud: magma_crud,
+        project_name: PROJECT,
+        model_name: 'fake_model',
+        filepath: './spec/fixtures/magma/magma_update_attributes_single_model_valid.csv'
+      )
+    }.to raise_error(RuntimeError, 'Invalid model fake_model for project test.')
+
+    expect(WebMock).not_to have_requested(:post, /#{MAGMA_HOST}\/update/)
+  end
+
+  it 'sends valid revisions to magma' do
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel.new(
+      magma_crud: magma_crud,
+      project_name: PROJECT,
+      model_name: 'model_two',
+      filepath: './spec/fixtures/magma/magma_update_attributes_single_model_valid.csv'
+    )
+
+    workflow.update_attributes
+
+    expect(updates).to eq({
+      "model_two" => {
+        "234" => {"name" => "Record #234","strength" => "5", "invisible" => "false"},
+        "123" => {"name" => "Record #123","strength" => "2","invisible" => "true"}
+      }
+    })
+
+    expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+  end
+
+  it 'raises exception for invalid attribute name' do
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel.new(
+      magma_crud: magma_crud,
+      project_name: PROJECT,
+      model_name: 'model_two',
+      filepath: './spec/fixtures/magma/magma_update_attributes_single_model_invalid_attribute.csv'
+    )
+
+    expect {
+      workflow.update_attributes
+    }.to raise_error(RuntimeError, 'Invalid attribute weight for model model_two.')
+
+    expect(WebMock).not_to have_requested(:post, /#{MAGMA_HOST}\/update/)
+  end
+
+  it 'raises exception for invalid filename' do
+    workflow = Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel.new(
+      magma_crud: magma_crud,
+      project_name: PROJECT,
+      model_name: 'model_two',
+      filepath: './spec/fixtures/magma/nonexistent_input.csv'
+    )
+
+    expect {
+      workflow.update_attributes
+    }.to raise_error(StandardError)
+
+    expect(WebMock).not_to have_requested(:post, /#{MAGMA_HOST}\/update/)
+  end
+
+  it 'raises exception if :model_name not provided' do
+    expect {
+      Etna::Clients::Magma::UpdateAttributesFromCsvWorkflowSingleModel.new(
+        magma_crud: magma_crud,
+        project_name: PROJECT,
+        filepath: './spec/fixtures/magma/nonexistent_input.csv'
+      )
+    }.to raise_error(StandardError, 'Single Model invokation must include keyword :model_name.')
 
     expect(WebMock).not_to have_requested(:post, /#{MAGMA_HOST}\/update/)
   end
