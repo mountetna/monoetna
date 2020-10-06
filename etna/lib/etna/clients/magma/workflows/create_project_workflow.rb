61e38da0eadbd67456d43aeab9c10eaf52c4c664
Cole <4930129+coleshaw@users.noreply.github.com>
2020-10-06 10:59:50 -0400

    more validations of json input

--- etna/lib/etna/clients/magma/workflows/create_project_workflow.rb
+++ etna/lib/etna/clients/magma/workflows/create_project_workflow.rb
@@ -23,7 +23,7 @@ module Etna
         def initialize(**params)
           super({}.update(params))
           @project = Project.new(project_file)
-          raise "Project JSON has errors", project.errors unless project.valid?
+          raise "Project JSON has errors: ", project.errors unless project.valid?
         end
 
         def create_janus_project
@@ -43,11 +43,34 @@ module Etna
         end
       end
 
-      class JsonProject
+      class JsonBase
         attr_reader :errors
-        def initialize(filepath:)
-          @raw = JSON.parse(File.read(filepath), symbolize_names: true)
+        def initialize
           @errors = []
+        end
+
+        def valid?
+          errors.length == 0
+        end
+
+        def nil_or_empty?(value)
+          value.nil? || value.empty?
+        end
+
+        def check_key(label, raw, key)
+          errors << "Missing required key for #{label}: \"#{key}\"." if !raw.key?(key.to_sym)
+          errors << "Invalid #{key} for #{label}: \"#{raw[key.to_sym]}\"." if raw.key?(key.to_sym) && nil_or_empty?(raw[key.to_sym])
+        end
+
+        def check_type(label, raw, key, valid_types)
+          errors << "Invalid #{key} for #{label}: \"#{raw[key.to_sym]}\"." if raw.key?(key.to_sym) && !valid_types.include?(raw[key.to_sym])
+        end
+      end
+
+      class JsonProject < JsonBase
+        def initialize(filepath:)
+          super()
+          @raw = JSON.parse(File.read(filepath), symbolize_names: true)
           validate
         end
 
@@ -67,28 +90,40 @@ module Etna
         def validate
           validate_project_names
           validate_models
-        end
 
-        def valid?
-          errors.length == 0
+          # Make sure that all parent models are defined
         end
 
         def validate_project_names
-          errors << "Missing required key for root project, \"project_name\"." if !@raw.key?(:project_name)
-          errors << "Missing required key for root project, \"project_name_full\"." if !@raw.key?(:project_name_full)
+          check_key('root project', @raw, :project_name)
+          check_key('root project', @raw, :project_name_full)
         end
 
         def validate_models
-          models
+          models.each { |model|
+            @errors += model.errors unless model.valid?
+          }
         end
       end
 
-      class JsonModel
+      class JsonModel < JsonBase
         attr_reader :name
         def initialize(model_name, raw)
+          super()
           @name = model_name
           @raw = raw
+
+          @valid_parent_link_types = [
+            Etna::Clients::Magma::ParentLinkType::CHILD,
+            Etna::Clients::Magma::ParentLinkType::COLLECTION,
+            Etna::Clients::Magma::ParentLinkType::TABLE
+          ]
           validate
+        end
+
+        def attributes
+          @attributes ||= @raw.key?(:attributes) ? @raw[:attributes].map { |attribute_name, attribute_def|
+            JsonAttribute.new(attribute_name, attribute_def) } : []
         end
 
         def validate
@@ -97,18 +132,33 @@ module Etna
         end
 
         def validate_add_model_data
-          raise "Missing required key for model #{name}, \"identifier\"." if !@raw.key?(:identifier)
+          check_key("model #{name}", @raw, :identifier)
           if name != :project
-            raise "Missing required key for model #{name}, \"parent_model_name\"." if !@raw.key?(:parent_model_name)
-            raise "Missing required key for model #{name}, \"parent_link_type\"." if !@raw.key?(:parent_link_type)
+            check_key("model #{name}", @raw, :parent_model_name)
+            check_key("model #{name}", @raw, :parent_link_type)
+            check_type("model #{name}", @raw, :parent_link_type, @valid_parent_link_types)
           end
         end
 
         def validate_attributes
+          return unless @raw.key?(:attributes)
+
           # Check that attribute types are supported
           # Check that any validations are supported
-          # Check that parent_link_type and link_types are supported
-          # Make sure that all parent models are defined
+          # Check that link_types are supported
+        end
+      end
+
+      class JsonAttribute < JsonBase
+        attr_reader :name
+        def initialize(attribute_name, raw)
+          super()
+          @name = attribute_name
+          @raw = raw
+          validate
+        end
+
+        def validate
         end
       end
     end
--- etna/spec/clients/magma/workflows/json_project_spec.rb
+++ etna/spec/clients/magma/workflows/json_project_spec.rb
@@ -6,14 +6,24 @@ describe Etna::Clients::Magma::JsonProject do
   before(:each) do
   end
 
-  it 'raises exception for missing project keys' do
+  it 'reports errors for missing project keys' do
     project = Etna::Clients::Magma::JsonProject.new(
       filepath: './spec/fixtures/create_project/create_project_fixture_missing_project_keys.json'
     )
     expect(project.valid?).to eq(false)
     expect(project.errors.length).to eq(2)
-    expect(project.errors.first).to eq('Missing required key for root project, "project_name".')
-    expect(project.errors.last).to eq('Missing required key for root project, "project_name_full".')
+    expect(project.errors.first).to eq('Missing required key for root project: "project_name".')
+    expect(project.errors.last).to eq('Missing required key for root project: "project_name_full".')
+  end
+
+  it 'reports errors for blank project keys' do
+    project = Etna::Clients::Magma::JsonProject.new(
+      filepath: './spec/fixtures/create_project/create_project_fixture_blank_project_keys.json'
+    )
+    expect(project.valid?).to eq(false)
+    expect(project.errors.length).to eq(2)
+    expect(project.errors.first).to eq('Invalid project_name for root project: "".')
+    expect(project.errors.last).to eq('Invalid project_name_full for root project: "".')
   end
 
   it 'loads a project JSON definition' do
@@ -24,5 +34,37 @@ describe Etna::Clients::Magma::JsonProject do
     expect(project.valid?).to eq(true)
     expect(project.name).to eq('test1')
     expect(project.name_full).to eq('Testing your immune system')
+  end
+
+  it 'reports errors for missing model information' do
+    project = Etna::Clients::Magma::JsonProject.new(
+      filepath: './spec/fixtures/create_project/create_project_fixture_missing_model_keys.json'
+    )
+    expect(project.valid?).to eq(false)
+    expect(project.errors.length).to eq(6)
+    expect(project.errors).to eq([
+      "Missing required key for model assay_name: \"parent_link_type\".",
+      "Missing required key for model document: \"parent_model_name\".",
+      "Missing required key for model document: \"parent_link_type\".",
+      "Missing required key for model assay_pool: \"identifier\".",
+      "Missing required key for model patient: \"parent_model_name\".",
+      "Missing required key for model project: \"identifier\"."
+    ])
+  end
+
+  it 'reports errors for blank model information' do
+    project = Etna::Clients::Magma::JsonProject.new(
+      filepath: './spec/fixtures/create_project/create_project_fixture_blank_model_keys.json'
+    )
+    expect(project.valid?).to eq(false)
+    expect(project.errors.length).to eq(6)
+    expect(project.errors).to eq([
+      "Invalid parent_model_name for model assay_name: \"\".",
+      "Invalid identifier for model document: \"\".",
+      "Invalid parent_link_type for model assay_pool: \"\".",
+      "Invalid parent_link_type for model assay_pool: \"\".",
+      "Invalid parent_link_type for model patient: \"thingamajig\".",
+      "Invalid identifier for model project: \"\"."
+    ])
   end
 end
