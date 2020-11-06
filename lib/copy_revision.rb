83799ad4b81f651ea7e2a8bcd8b03c3de3640dbf
GithubAction <mountetna@users.noreply.github.com>
2020-11-06 12:28:17 +0000

    Squashed 'metis/' changes from adae88ce..887f089c
    887f089c etna reference updates
    f8bccbed Merge branch 'master' into cs/metis-rename-file-across-buckets
    ac4c8b6d refactor revisions
    bd806734 pin puma to the projects
    6931380a add spec for metis file rename
    2495ea22 refactor file rename and allow you to rename across buckets
    
    git-subtree-dir: metis
    git-subtree-split: 887f089cc20c06e72c0ccba30a70061488750cd0

--- lib/copy_revision.rb
+++ lib/copy_revision.rb
@@ -2,30 +2,6 @@ require_relative 'revision'
 
 class Metis
   class CopyRevision < Revision
-    def validate
-      @errors = []
-      validate_source(@source)
-      validate_dest(@dest)
-    end
-
-    def bucket_names
-      # This is kind of weird, but we need the ability to grab
-      #   all relevant bucket names, even before validation of
-      #   the CopyRevision.
-      # So, if @dest doesn't exist, we don't return it
-      source_bucket_names = super
-      if @dest.mpath.valid?
-        return source_bucket_names.push(@dest.mpath.bucket_name)
-      end
-      return source_bucket_names
-    end
-
-    def mpaths
-      source_mpaths = super
-      source_mpaths.push(@dest.mpath) if @dest.mpath.valid?
-      return source_mpaths
-    end
-
     def revise!
       raise 'Invalid revision, cannot revise!' unless valid?
       raise 'Cannot revise without a user' unless @user
@@ -37,14 +13,6 @@ class Metis
         dest_bucket_name: @dest.mpath.bucket_name,
         user: @user
       })
-    end
-
-    def to_hash
-      {
-        dest: @dest.mpath.path,
-        source: @source.mpath.path,
-        errors: @errors
-      }
     end
   end
 end
