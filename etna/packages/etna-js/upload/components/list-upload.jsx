0c26c298f50399052d60cb253d13d5d7e43405cd
Cole <4930129+coleshaw@users.noreply.github.com>
2020-11-06 15:19:53 -0500

    User messaging around some corner cases

--- etna/packages/etna-js/upload/components/list-upload.jsx
+++ etna/packages/etna-js/upload/components/list-upload.jsx
@@ -3,11 +3,13 @@ import {ListEntryColumn, ListEntryTypeColumn} from '../../components/ListEntry';
 import UploadMeter from './upload-meter';
 import UploadControl from './upload-control';
 
-const ListUpload = ({upload, widths}) => (
+const ListUpload = ({upload, widths, override_name}) => (
   <div className='list-entry-group upload'>
     <ListEntryTypeColumn icon='upload' widths={widths} />
     <ListEntryColumn className='name' widths={widths}>
-      <span className='list-entry-file-name'>{upload.file_name}</span>
+      <span className='list-entry-file-name'>
+        {override_name || upload.file_name}
+      </span>
     </ListEntryColumn>
     <ListEntryColumn className='status' widths={widths} />
     <ListEntryColumn className='updated' widths={widths}>
