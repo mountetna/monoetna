b26d7a90ed2889e5860136a4ccdc62918ac5a1db
dtm2451 <Daniel.Bunis@ucsf.edu>
2020-11-16 11:58:52 -0800

    Remove `retrieveAttributeCounts()` function
    & update documentation for `retrieveAttributeNames()`

--- etna/packages/magmaR/R/retrieveAttributes.R
+++ etna/packages/magmaR/R/retrieveAttributes.R
@@ -1,9 +1,4 @@
-#' Assess the downloadable data from magma
-#' @name retrieveAttributes
-#' @param token your TOKEN from https://janus.ucsf.edu
-#' @param projectName the name of the project you would like to download data from, exactly as it appears at https://timur.ucsf.edu
-#' @param modelName the name of the piece of the magma model form which to download data
-#' @param recordNames,attributeNames particular record(s) or attribute(s) to grab names for
+#' Determine the attribute options associated with a given project-model pair.
 #' @inheritParams retrieve
 #' @return a string vector
 #' @examples
@@ -15,10 +10,6 @@
 #'         modelName = "patient")
 #' }
 #' 
-#' @importFrom utils URLencode
-NULL
-
-#' @describeIn retrieveAttributes Assess the names of attributes downloadable from magma
 #' @export
 retrieveAttributeNames <- function(
     projectName = "ipi",
@@ -32,39 +23,4 @@ retrieveAttributeNames <- function(
         projectName, modelName, recordNames,
         attributeNames = "all", format = "tsv",
         names.only = TRUE, token = token, ...)
-}
-
-#' @describeIn retrieveAttributes Assess the counts of records for attributes downloadable from magma
-#' @export
-retrieveAttributeCounts <- function(
-    projectName = "ipi",
-    modelName = "patient",
-    recordNames = "all",
-    attributeNames = "all",
-    token = .get_TOKEN(),
-    ...
-) {
-    
-    if (identical(attributeNames, "all")) {
-        attributeNames <- retrieveAttributeNames(
-            projectName = projectName, modelName = modelName,
-            recordNames = recordNames, token = token, ...)
-    }
-    
-    ### Logic for this function is not complete ###
-    lapply(
-        attributeNames,
-        function(x) {
-            dat <- retrieve(
-                projectName = projectName, modelName = modelName,
-                recordNames = recordNames, attributeNames = x,
-                token = token, ...)
-            if (is.data.frame(dat)) {
-                stop("Function does not work for this data currently")
-            } else if (length(attributeNames)==1) {
-                sum(unlist(lapply(dat, function(x) length(x)==2)))
-            } else {
-                stop("Function does not work for this data currently")
-            }
-        })
 }
