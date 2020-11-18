d4df448e7c828bd3e370ced905d31ecc2fd98944
GithubAction <mountetna@users.noreply.github.com>
2020-11-18 11:51:20 +0000

    Artifact gem-etna for sha de61c828a97a0f29d1076c1d7882e7bc1d75728c

---
+++ lib/etna/clients/janus/client.rb
@@ -0,0 +1,73 @@
+require 'net/http/persistent'
+require 'net/http/post/multipart'
+require 'singleton'
+require_relative '../../client'
+require_relative './models'
+
+module Etna
+  module Clients
+    class Janus
+      def initialize(host:, token:, persistent: true, ignore_ssl: false)
+        raise 'Janus client configuration is missing host.' unless host
+        raise 'Janus client configuration is missing token.' unless token
+        @etna_client = ::Etna::Client.new(
+          host,
+          token,
+          routes_available: false,
+          persistent: persistent,
+          ignore_ssl: ignore_ssl)
+      end
+
+      def get_project(get_project_request = GetProjectRequest.new)
+        html = nil
+        @etna_client.get(
+          "/project/#{get_project_request.project_name}",
+          get_project_request) do |res|
+          html = res.body
+        end
+
+        HtmlResponse.new(html)
+      end
+
+      def add_project(add_project_request = AddProjectRequest.new)
+        @etna_client.post('/add_project', add_project_request) do |res|
+          # Redirect, no response data
+        end
+      end
+
+      def add_user(add_user_request = AddUserRequest.new)
+        @etna_client.post(
+          "/add_user/#{add_user_request.project_name}",
+          add_user_request) do |res|
+          # Redirect, no response data
+        end
+      end
+
+      def update_permission(update_permission_request = UpdatePermissionRequest.new)
+        @etna_client.post(
+          "/update_permission/#{update_permission_request.project_name}",
+          update_permission_request) do |res|
+          # Redirect, no response data
+        end
+      end
+
+      def refresh_token(refresh_token_request = RefreshTokenRequest.new)
+        token = nil
+        @etna_client.get('/refresh_token', refresh_token_request) do |res|
+          token = res.body
+        end
+
+        TokenResponse.new(token)
+      end
+
+      def viewer_token(viewer_token_request = ViewerTokenRequest.new)
+        token = nil
+        @etna_client.get('/viewer_token', viewer_token_request) do |res|
+          token = res.body
+        end
+
+        TokenResponse.new(token)
+      end
+    end
+  end
+end
