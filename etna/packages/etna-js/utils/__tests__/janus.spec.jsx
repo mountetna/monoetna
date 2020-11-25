517e30478d2386c1672098da3dffd3927ffdd27b
Cole <4930129+coleshaw@users.noreply.github.com>
2020-11-25 16:08:16 -0500

    fix specs

--- etna/packages/etna-js/utils/__tests__/janus.spec.jsx
+++ etna/packages/etna-js/utils/__tests__/janus.spec.jsx
@@ -2,33 +2,25 @@ import {projectNameFull} from '../janus';
 
 describe('Janus Utils', () => {
   it('returns the matching project', () => {
-    const state = {
-      janus: {
-        projects: [
-          {
-            project_name: 'xyz1',
-            project_name_full: 'ACME corporation'
-          }
-        ]
+    const projects = [
+      {
+        project_name: 'xyz1',
+        project_name_full: 'ACME corporation'
       }
-    };
-    const result = projectNameFull(state, 'xyz1');
+    ];
+    const result = projectNameFull(projects, 'xyz1');
 
     expect(result).toEqual('ACME corporation');
   });
 
   it('returns the shortname if no matching project', () => {
-    const state = {
-      janus: {
-        projects: [
-          {
-            project_name: 'xyz1',
-            project_name_full: 'ACME corporation'
-          }
-        ]
+    const projects = [
+      {
+        project_name: 'xyz1',
+        project_name_full: 'ACME corporation'
       }
-    };
-    const result = projectNameFull(state, 'xyz2');
+    ];
+    const result = projectNameFull(projects, 'xyz2');
 
     expect(result).toEqual('xyz2');
   });
