cefb744f84668e31ed820c8a23c8a1c140e4dd8b
dtm2451 <Daniel.Bunis@ucsf.edu>
2020-11-25 14:59:30 -0800

    Add tests for `retireve()` & `retrieveJSON()`

--- etna/packages/magmaR/tests/testthat/test-retrieve.R
+++ etna/packages/magmaR/tests/testthat/test-retrieve.R
@@ -3,7 +3,7 @@
 
 .GlobalEnv$.MAGMAR_TOKEN <- Sys.getenv("TOKEN")
 
-test_that("retrieve works with minimal input", {
+test_that("retrieve works with targetted input", {
     vcr::use_cassette("retrieve_targetted", {
         ret <- retrieve(
             "ipi", "experiment",
@@ -13,4 +13,23 @@ test_that("retrieve works with minimal input", {
     
     expect_s3_class(ret, "data.frame")
     expect_equal(dim(ret), c(3,2))
+})
+
+test_that("retrieve & retrieveJSON work with minimal input", {
+    vcr::use_cassette("retrieve_ALLs", {
+        df <- retrieve(
+            "ipi", "experiment")
+    })
+    
+    expect_s3_class(df, "data.frame")
+    expect_true(nrow(df) >= 20 && ncol(df) > 4)
+    
+    vcr::use_cassette("retrieveJSON_ALLs", {
+        json <- retrieveJSON(
+            "ipi", "experiment")
+    })
+    
+    expect_type(json, "list")
+    expect_equal(nrow(df), length(json$models$experiment$documents))
+    expect_gt(length(json$models$experiment$documents[[1]]), 4)
 })
