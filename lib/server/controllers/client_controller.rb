cf84c35c76484f97fd2602470bff6b122df40e85
GithubAction <mountetna@users.noreply.github.com>
2020-11-30 19:09:37 +0000

    Squashed 'metis/' changes from 021e4108..6ac866f5
    6ac866f5 etna reference updates
    6b75030c use janus api for metis
    
    git-subtree-dir: metis
    git-subtree-split: 6ac866f5da14c7376e3a2fe361f61e4e59bc3491

--- lib/server/controllers/client_controller.rb
+++ lib/server/controllers/client_controller.rb
@@ -1,9 +1,10 @@
 # client_controller.rb
-# This controller serves the client pages and code. 
+# This controller serves the client pages and code.
 
 class ClientController < Metis::Controller
   def index
     @token_name = Metis.instance.config(:token_name)
+    @janus_host = Metis.instance.config(:auth_redirect)
     erb_view :index
   end
 end
