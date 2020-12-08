3b647d7bc1b1aaaa8ac0dd1df9ba301a49643247
GithubAction <mountetna@users.noreply.github.com>
2020-12-08 14:21:32 +0000

    Artifact npm-etna-js for sha 842e38390a391118f0f6e0bfc27bc5d4ebb1ac7b

---
+++ utils/__tests__/janus.spec.jsx
@@ -0,0 +1,33 @@
+import {projectNameFull} from '../janus';
+
+describe('Janus Utils', () => {
+  it('returns the matching project', () => {
+    const projects = [
+      {
+        project_name: 'xyz1',
+        project_name_full: 'ACME corporation'
+      }
+    ];
+    const result = projectNameFull(projects, 'xyz1');
+
+    expect(result).toEqual('ACME corporation');
+  });
+
+  it('returns the shortname if no matching project', () => {
+    const projects = [
+      {
+        project_name: 'xyz1',
+        project_name_full: 'ACME corporation'
+      }
+    ];
+    const result = projectNameFull(projects, 'xyz2');
+
+    expect(result).toEqual('xyz2');
+  });
+
+  it('returns the shortname if no projects', () => {
+    const result = projectNameFull(null, 'xyz2');
+
+    expect(result).toEqual('xyz2');
+  });
+});
