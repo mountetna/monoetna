01a4eba2e3e79806974f439bc29d88369ae41ef3
Cole <4930129+coleshaw@users.noreply.github.com>
2020-12-11 12:02:04 -0500

    more progress

--- etna/lib/etna/clients/magma/formatting/models_odm_xml.rb
+++ etna/lib/etna/clients/magma/formatting/models_odm_xml.rb
@@ -9,14 +9,6 @@ module Etna
   end
 end
 
-# NOTE:
-# <redcap:Purpose>0</redcap:Purpose>
-# 0 = Practice / just for fun
-# 1 = Operational Support
-# 2 = Research
-# 3 = Quality Improvement
-# 4 = Other
-
 module Etna
   module Clients
     class Magma
@@ -38,6 +30,43 @@ module Etna
 
           def initialize(project_name:)
             @project_name = project_name
+          end
+
+          def data_type_map
+            @data_type_map ||= begin
+              map = {}
+              map[Etna::Clients::Magma::AttributeType::STRING] = 'text'
+              map[Etna::Clients::Magma::AttributeType::DATE_TIME] = 'date'
+              map[Etna::Clients::Magma::AttributeType::BOOLEAN] = 'text'
+              map[Etna::Clients::Magma::AttributeType::FLOAT] = 'float'
+              map[Etna::Clients::Magma::AttributeType::INTEGER] = 'integer'
+
+              map
+            end
+          end
+
+          def redcap_field_type_map
+            @redcap_field_type_map ||= begin
+              map = {}
+              map[Etna::Clients::Magma::AttributeType::STRING] = 'textarea'
+              map[Etna::Clients::Magma::AttributeType::DATE_TIME] = 'text'
+              map[Etna::Clients::Magma::AttributeType::BOOLEAN] = 'radio'
+              map[Etna::Clients::Magma::AttributeType::FLOAT] = 'text'
+              map[Etna::Clients::Magma::AttributeType::INTEGER] = 'text'
+
+              map
+            end
+          end
+
+          def redcap_text_validation_map
+            @redcap_text_validation_map ||= begin
+              map = {}
+              map[Etna::Clients::Magma::AttributeType::DATE_TIME] = 'date_mdy'
+              map[Etna::Clients::Magma::AttributeType::FLOAT] = 'float'
+              map[Etna::Clients::Magma::AttributeType::INTEGER] = 'int'
+
+              map
+            end
           end
 
           def write_models(models, output_io: nil, filename: nil)
@@ -76,6 +105,14 @@ module Etna
             # Includes general metadata about the project, as well as
             # declarations of all repeating instruments,
             #   which seem like Timepoints to me.
+            # NOTE:
+            # <redcap:Purpose>0</redcap:Purpose>
+            # 0 = Practice / just for fun
+            # 1 = Operational Support
+            # 2 = Research
+            # 3 = Quality Improvement
+            # 4 = Other
+
             xml.GlobalVariables do
               xml.StudyName "#{project_name}"
               xml.StudyDescription "#{project_name} - Data Library integration project"
@@ -96,6 +133,8 @@ module Etna
           end
 
           def repeating_attribute_types(models)
+            # We don't have a good indicator for what is a repeating
+            #   attribute for REDCap...
             # Will this work for models without timepoint, that go
             #   straight from subject -> sample?
             models.model('subject').template.attributes.all.select do |attribute|
@@ -103,9 +142,13 @@ module Etna
             end
           end
 
-          def clinical_attribute_types(models)
-            models.model('subject').template.attributes.all.select do |attribute|
-              Etna::Clients::Magma::AttributeType::TABLE == attribute.attribute_type
+          def clinical_dictionaries(models)
+            # Our indicator for if something needs a REDCap form will be any
+            #   model with a dictionary.
+            models.all.select do |model|
+              model.template.raw['dictionary']
+            end.map do |model|
+              model.template.dictionary
             end
           end
 
@@ -115,15 +158,19 @@ module Etna
             #   Timepoint that hangs off of
             #   a Subject model.
             xml.send('redcap:RepeatingInstrumentsAndEvents') do
-              repeating_attribute_types(models).each do |repeating_attribute|
-                node = xml.send('redcap:RepeatingInstrument')
-                node['redcap:UniqueEventName'] = 'event_1_arm_1'
-                node['redcap:RepeatInstrument'] = repeating_attribute.attribute_name
-                node['redcap:CustomLabel'] = repeating_attribute.display_name || repeating_attribute.attribute_name.capitalize
-
-                node
+              repeating_attribute_types(models).map do |repeating_attribute|
+                write_repeating_instrument_xml(xml, repeating_attribute)
               end
             end
+          end
+
+          def write_repeating_instrument_xml(xml, instrument)
+            node = xml.send('redcap:RepeatingInstrument')
+            node['redcap:UniqueEventName'] = 'event_1_arm_1'
+            node['redcap:RepeatInstrument'] = instrument.attribute_name
+            node['redcap:CustomLabel'] = instrument.display_name || instrument.attribute_name.capitalize
+
+            node
           end
 
           def metadata(xml, models)
@@ -133,30 +180,52 @@ module Etna
               Name: project_name,
               'redcap:RecordIdField': 'record_id'
             ) do
-              # Each Magma model we want here needs a FormDef, with
-              #   ItemGroupRef children for each rorm page (?).
-              # Each ItemGroupRef requires a corresponding ItemGroupDef
-              #   with ItemRef children for each input (?).
-              # Each ItemRef requires a correspdonding ItemDef
-              #   that defines the label and type. and includes
-              #   <Question> as a label (?).
-              # Option validations are present as a CodeList (with
-              #   CodeListItem children).
-              xml.StudyName "#{project_name}"
-              xml.StudyDescription "#{project_name} - Data Library integration project"
-              xml.ProtocolName "#{project_name}"
-              xml.send('redcap:RecordAutonumberingEnabled', 1)
-              xml.send('redcap:CustomRecordLabel')
-              xml.send('redcap:SecondaryUniqueField')
-	            xml.send('redcap:SchedulingEnabled', 0)
-	            xml.send('redcap:SurveysEnabled', 0)
-              xml.send('redcap:SurveyInvitationEmailField')
-              xml.send('redcap:Purpose', 2) # 2 == research
-              xml.send('redcap:PurposeOther')
-              xml.send('redcap:ProjectNotes', "Used to easily ingest clinical data for #{project_name} into the Data Library.")
-              xml.send('redcap:MissingDataCodes')
+              clinical_dictionaries.map do |dictionary|
+                # Each Magma dictionary needs a FormDef, with
+                #   ItemGroupRef children for each form page (?).
+                # Each ItemGroupRef requires a corresponding ItemGroupDef
+                #   with ItemRef children for each input (?).
+                # Each ItemRef requires a correspdonding ItemDef
+                #   that defines the label and type, and includes
+                #   <Question> as a label (?).
+                # Option validations are present as a CodeList (with
+                #   CodeListItem children).
+                write_form_def(xml, dictionary)
+                write_item_group_def(xml, dictionary)
+                write_item_def(xml, dictionary)
+                write_code_list(xml, models, dictionary)
+              end
+            end
+          end
 
-              repeating_instruments(xml, models)
+          def write_form_def(xml, dictionary)
+            xml.FormDef(
+              OID: "Form.#{dictionary.model_name}",
+              Name: dictionary.model_name.capitalize,
+              Repeating: "No",
+              'redcap:FormName': dictionary.model_name
+            ) do
+              # Assume a single item group
+              xml.ItemGroupRef(
+                ItemGroupOID: "#{dictionary.model_name}.attributes",
+                Mandatory: "No"
+              )
+            end
+          end
+
+          def write_item_group_def(xml, dictionary)
+            xml.ItemGroupDef(
+              OID: "#{dictionary.model_name}.attributes",
+              Name: "#{dictionary.model_name.capitalize} Attributes",
+              Repeating: "No"
+            ) do
+              dictionary.attributes.keys.map do |attribute_name|
+                xml.ItemRef(
+                  ItemOID: "#{dictionary.model_name}.#{attribute_name}", # Does this need to be unique across all items?
+                  Mandatory: "No",
+                  'redcap:Variable': attribute_name # Does this need to be unique?
+                )
+              end
             end
           end
         end
