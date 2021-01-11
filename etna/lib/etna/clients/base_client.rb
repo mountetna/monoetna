53dbe7658e827353a0374b4d6883296456491bad
Cole <4930129+coleshaw@users.noreply.github.com>
2021-01-11 08:57:11 -0500

    specify units

--- etna/lib/etna/clients/base_client.rb
+++ etna/lib/etna/clients/base_client.rb
@@ -28,6 +28,7 @@ module Etna
       end
 
       def token_will_expire?(offset=3000)
+        # offset in seconds
         # Will the user's token expire in the given amount of time?
         epoch_seconds = JSON.parse(Base64.urlsafe_decode64(token.split('.')[1]))["exp"]
         expiration = DateTime.strptime(epoch_seconds.to_s, "%s")
