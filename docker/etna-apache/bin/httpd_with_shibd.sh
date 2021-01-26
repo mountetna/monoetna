f6df3dfe790e87769c1a64072b21c8ca1cc38c5d
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-01-26 15:47:47 -0800

    dooo ittt

--- docker/etna-apache/bin/httpd_with_shibd.sh
+++ docker/etna-apache/bin/httpd_with_shibd.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
-cleanup() {
-  kill -- -$(jobs -p)
-}
-
 set -e
-set -m
 
-# Starts shibd in the background
-/usr/sbin/shibd -f -F &>/tmp/shibd.log &
-SHIBD_PID=$!
-# Start httpd
-httpd -DFOREGROUND &>/tmp/httpd.log &
-HTTPD_PID=$!
-trap cleanup EXIT
-tail -f /tmp/*.log /var/log/shibboleth/shibd.log
+/etc/init.d/shibd start
+httpd -DFOREGROUND
 
