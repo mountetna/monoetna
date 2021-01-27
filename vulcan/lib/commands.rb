9071e2aa6d5418837319bd53806d889fe68ce88f
Cole <4930129+coleshaw@users.noreply.github.com>
2021-01-27 13:29:36 -0500

    remove create_routes command

--- vulcan/lib/commands.rb
+++ vulcan/lib/commands.rb
@@ -73,47 +73,6 @@ class Vulcan
     end
   end
 
-  class CreateRoutes < Etna::Command
-    usage 'Create the routes.js file for the Vulcan javascript application to use to match routes'
-
-    def route_js
-      require_relative 'server'
-      <<EOT.chomp
-window.Routes = Object.assign(
-  window.Routes || {},
-  {
-#{
-  Vulcan::Server.routes.map do |route|
-    route_func(route)
-  end.join(",\n")
-}
-  }
-);
-EOT
-    end
-
-    def route_func route
-      required_parts = route.parts.map(&:to_sym)
-      route_path = route.path(
-        Hash[
-          required_parts.zip(
-            required_parts.map{|part| %Q{'+#{part}+'} }
-          )
-        ]
-      )
-
-      <<EOT.chomp
-    #{route.name}_path: (#{required_parts.join(', ')}) => ('#{route_path}')
-EOT
-    end
-
-    def execute
-      File.open('public/js/routes.js','w') do |f|
-        f.puts route_js
-      end
-    end
-  end
-
   class CreateDb < Etna::Command
     usage '# create the initial database per config.yml'
 
