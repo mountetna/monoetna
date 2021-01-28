211fb7381512c9ea5d123c5065f42aa0379016a1
Cole <4930129+coleshaw@users.noreply.github.com>
2021-01-28 07:35:06 -0500

    vulcan icon

--- vulcan/lib/client/jsx/components/workflow/browser.jsx
+++ vulcan/lib/client/jsx/components/workflow/browser.jsx
@@ -8,6 +8,10 @@ import {setLocation} from 'etna-js/actions/location_actions';
 import {useReduxState} from 'etna-js/hooks/useReduxState';
 import {useActionInvoker} from 'etna-js/hooks/useActionInvoker';
 
+import {selectProjects} from 'etna-js/selectors/janus-selector';
+import {projectNameFull} from 'etna-js/utils/janus';
+import {fetchProjectsAction} from 'etna-js/actions/janus-actions';
+
 const loadingDiv = (
   <div className='browser'>
     <div id='loader-container'>
@@ -28,11 +32,22 @@ export default function Browser() {
   const invoke = useActionInvoker();
   const browserState = useReduxState(browserStateOf());
 
-  return <div>Stub for Vulcan!</div>;
+  let {projects} = browserState;
+
+  useEffect(() => {
+    invoke(fetchProjectsAction());
+  });
+
+  return (
+    <div>
+      Select a workflow for{' '}
+      {projectNameFull(projects, CONFIG.project_name) || CONFIG.project_name}
+    </div>
+  );
 }
 
 function browserStateOf() {
   return (state) => {
-    return {};
+    return {projects: selectProjects(state)};
   };
 }
