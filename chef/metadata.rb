9f099abd1c2955ea2e2f0f009d10f7417fbfbc6e
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-03 13:44:02 -0800

    Gvaihir - View Editor (v0.0.1) fix (view.rb) added 'id' to the view model fix (view_actions.jsx) fixed disappearing view object after promise fix (view_editor.jsx) fixed script field to "document" fix (view_reducer) views from DB are fed into store with document Object serialized into JSON string for direct editing in the browser

---
+++ chef/metadata.rb
@@ -0,0 +1,11 @@
+name 'chef'
+maintainer 'DSCOLAB'
+maintainer_email 'Etna-Agent@ucsf.edu'
+license 'Apache-2.0'
+description ''
+version 'abc'
+
+source_url 'https://github.com/chef-cookbooks/docker'
+issues_url 'https://github.com/chef-cookbooks/docker/issues'
+
+depends_on 'mountetna'
