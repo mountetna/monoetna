983e836bc8a0c90d32e79946ffa72f7327610bf9
Cole <4930129+coleshaw@users.noreply.github.com>
2021-02-03 13:57:22 -0500

    update cwl serializer

--- vulcan/lib/client/jsx/serializers/__tests__/cwl.spec.js
+++ vulcan/lib/client/jsx/serializers/__tests__/cwl.spec.js
@@ -20,24 +20,70 @@ describe('CwlSerializer', () => {
       'outputs',
       'steps'
     ]);
-    expect(Object.keys(doc.steps)).toEqual([
+    expect(doc.steps.map((s) => s.name)).toEqual([
       'first_step',
       'ui_pick_subset',
       'final_step'
     ]);
   });
 
-  it('adds the correct step number to each step', () => {
-    const sample_yaml = fs.readFileSync(
-      path.resolve(__dirname, '../../spec/fixtures/sample_cwl.yaml'),
-      'utf8'
-    );
-    const cwl = new CwlSerializer(sample_yaml);
+  it('handles steps with multiple dependencies', () => {
+    // We'll feed the orderSteps method specific JSON, so don't
+    //   need a real YAML input for the constructor.
+    const cwl = new CwlSerializer('');
 
-    const doc = cwl.json;
+    const inputJson = {
+      steps: {
+        last_step: {
+          in: {
+            data1: 'second_step/output',
+            data2: 'third_step/output'
+          }
+        },
+        second_step: {
+          in: {
+            data: 'first_step/output'
+          }
+        },
+        third_step: {
+          in: {
+            data: 'first_step/output2'
+          }
+        },
+        first_step: {
+          in: []
+        }
+      }
+    };
 
-    expect(doc.steps.first_step.step_number).toEqual(1);
-    expect(doc.steps.ui_pick_subset.step_number).toEqual(2);
-    expect(doc.steps.final_step.step_number).toEqual(3);
+    const output = cwl.orderSteps(inputJson);
+
+    expect(output).toEqual({
+      steps: [
+        {
+          in: [],
+          name: 'first_step'
+        },
+        {
+          in: {
+            data: 'first_step/output'
+          },
+          name: 'second_step'
+        },
+        {
+          in: {
+            data: 'first_step/output2'
+          },
+          name: 'third_step'
+        },
+        {
+          in: {
+            data1: 'second_step/output',
+            data2: 'third_step/output'
+          },
+          name: 'last_step'
+        }
+      ]
+    });
   });
 });
--- vulcan/lib/client/jsx/serializers/cwl.js
+++ vulcan/lib/client/jsx/serializers/cwl.js
@@ -6,13 +6,13 @@ export default class CwlSerializer extends Serializer {
   get json() {
     // Convert a CWL YAML file into JSON
     const doc = YAML.parseDocument(this.raw);
-    return injectStepMetadata(doc.toJSON());
+    return this.orderSteps(doc.toJSON());
   }
 
-  injectStepMetadata(json) {
-    // Add some logic to add stepNumber to each Step.
-    // These have to be calculated by graph traversal to figure
-    //   out which inputs are dependent on which other steps.
+  orderSteps(json) {
+    // Turn the json.steps into an Array with the steps
+    //   in order, instead of a Hash. This is because the Hash
+    //   keys don't come out of the YAML in guaranteed order.
     let orderedSteps = [];
     let allSteps = [];
 
@@ -24,12 +24,59 @@ export default class CwlSerializer extends Serializer {
       );
     });
 
-    while (orderedSteps.length < allSteps.length) {}
+    // We loop through allSteps and find all steps whose
+    //   inputs are already in orderedSteps. Note that
+    //   each step could have multiple input dependencies.
+    // An input dependency is noted with a "/" in the
+    //   input value. For example:
+    //
+    //   in: {
+    //     data_from_previous_step: previous_step/output_variable_name
+    //   }
+    const numSteps = allSteps.length;
+    while (orderedSteps.length < numSteps) {
+      allSteps.forEach((step) => {
+        if (orderedSteps.includes(step)) return;
 
-    orderedSteps.forEach((step) => {
-      json.steps[step.name] = Object.assign({}, step);
-    });
+        if (this.dependenciesSatisfied(step, orderedSteps))
+          orderedSteps.push(step);
+      });
+    }
+
+    json.steps = Object.assign([], orderedSteps);
 
     return json;
+  }
+
+  stepDependencies(step) {
+    // Return a list of step names that the given
+    //   step depends on for input. null otherwise.
+    if ([] === step.in) return null;
+
+    let dependencies = [];
+
+    Object.keys(step.in).forEach((stepName) => {
+      const inputVar = step.in[stepName];
+      if (inputVar.includes('/')) dependencies.push(inputVar.split('/')[0]);
+    });
+
+    return dependencies.length > 0 ? dependencies : null;
+  }
+
+  dependenciesSatisfied(step, orderedSteps) {
+    const dependencies = this.stepDependencies(step);
+
+    if (null === dependencies) return true;
+
+    const orderedStepNames = orderedSteps.map((s) => s.name);
+    if (
+      dependencies.every((dependency) => {
+        return orderedStepNames.includes(dependency);
+      })
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
