e1cc4b8f08230627c94f60c15b15be2adc00e8ac
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-02-04 09:20:06 -0800

    More fixes

---
+++ vulcan/lib/client/jsx/vulcan_store.jsx
@@ -0,0 +1,28 @@
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+
+// Reducers.
+import messages from 'etna-js/reducers/message_reducer';
+import exchanges from 'etna-js/reducers/exchanges_reducer';
+import location from 'etna-js/reducers/location_reducer';
+import janus from 'etna-js/reducers/janus-reducer';
+import user from 'etna-js/reducers/user_reducer';
+
+export const VulcanStore = () => {
+  let reducers = combineReducers({
+    messages,
+    exchanges,
+    location,
+    janus,
+    user
+  });
+
+  let middlewares = [thunk];
+
+  if (process.env.NODE_ENV != 'production')
+    middlewares.push(createLogger({collapsed: true}));
+
+  return createStore(reducers, {}, applyMiddleware(...middlewares));
+};
