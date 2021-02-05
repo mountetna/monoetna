2349ba70c40988a13a5f184897b7849b5d1b273c
Cole <4930129+coleshaw@users.noreply.github.com>
2021-02-05 14:05:35 -0500

    tweaking

--- vulcan/lib/client/jsx/components/workflow/output.jsx
+++ vulcan/lib/client/jsx/components/workflow/output.jsx
@@ -8,24 +8,53 @@ import {getData} from '../../api/vulcan';
 
 import Plot from './output_renderers/plot';
 import Raw from './output_renderers/raw';
-import {stepDataUrls} from '../../selectors/workflow_selector';
+import {stepDataUrls, stepData} from '../../selectors/workflow_selector';
+
+const loadingDiv = (
+  <div className='browser'>
+    <div id='loader-container'>
+      <div className='loader'>Loading...</div>
+    </div>
+  </div>
+);
 
 export default function Output() {
   const invoke = useActionInvoker();
   const {workflow, pathIndex, stepIndex, setData} = useContext(VulcanContext);
 
+  const [outputData, setOutputData] = useState(null);
+  const [outputIndex, setOutputIndex] = useState(0);
+  const [outputDatum, setOutputDatum] = useState(null);
+
+  // useEffect(() => {
+  //   let dataUrls = stepDataUrls(workflow, pathIndex);
+  //   dataUrls.forEach((url) => {
+  //     getData(url)
+  //       .then((data) => {
+  //         setData(url, data);
+  //       })
+  //       .catch((e) => {
+  //         invoke(showMessages(e));
+  //       });
+  //   });
+  // }, [pathIndex, stepIndex]);
+
   useEffect(() => {
-    let dataUrls = stepDataUrls(workflow, pathIndex);
-    dataUrls.forEach((url) => {
-      getData(url)
-        .then((data) => {
-          setData(url, data);
-        })
-        .catch((e) => {
-          invoke(showMessages(e));
-        });
+    // When the workflow updates, let's pull out
+    //   the data for the current step.
+    setOutputData(stepData(workflow, pathIndex, stepIndex));
+  }, [workflow]);
+
+  useEffect(() => {
+    setData('https://vulcan.test/blob', {
+      x: [1, 2, 3, 4, 5],
+      y: [5, 4, 3, 2, 1]
     });
-  }, [pathIndex, stepIndex]);
+  }, []);
+
+  useEffect(() => {
+    setOutputDatum(outputData[outputIndex]);
+  }, [outputData, outputIndex]);
 
   const viewOptions = {
     plot: Plot,
@@ -33,18 +62,37 @@ export default function Output() {
   };
 
   let Component;
-  function handleOnSelect(index) {
+  function handleOnSelectView(index) {
     Component = viewOptions[Object.keys(viewOptions)[index]];
   }
 
+  function handleOnSelectOutput(index) {
+    setOutputIndex(index);
+  }
+
+  if (!outputData) return loadingDiv;
+
   return (
     <div className='output-wrapper'>
-      <Dropdown
-        list={Object.keys(viewOptions)}
-        selected_index={0}
-        onSelect={handleOnSelect}
-      ></Dropdown>
-      <div></div>
+      <div>
+        <p>Select which output data to view:</p>
+        <Dropdown
+          list={outputData.map((o) => o.name)}
+          selected_index={0}
+          onSelect={handleOnSelectOutput}
+        ></Dropdown>
+      </div>
+      <div>
+        <p>Select a view option:</p>
+        <Dropdown
+          list={Object.keys(viewOptions)}
+          selected_index={0}
+          onSelect={handleOnSelectView}
+        ></Dropdown>
+      </div>
+      <div>
+        {outputDatum ? 'Some data here' : 'No data yet for that output'}
+      </div>
     </div>
   );
 }
