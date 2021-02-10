53630ac21450a822d7e4f2fa8b72431e9c84d433
GithubAction <mountetna@users.noreply.github.com>
2021-02-10 15:33:17 +0000

    Artifact npm-etna-js for sha 9e9d2746302e2e029a79d76870b04cf50aa876eb

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  globals: {
+    __TEST__: true,
+    CONFIG: {
+      project_name: 'labors',
+      magma_host: "https://magma.test"
+    }
+  },
+  moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy"
+  },
+  testMatch: [
+    "**/__tests__/**/?(*.)(spec|test).js?(x)"
+  ],
+  collectCoverageFrom: [
+    "**/*.js?(x)"
+  ],
+  setupFilesAfterEnv: [
+    "./spec/setup.js"
+  ],
+  setupFiles: [
+    "raf/polyfill"
+  ]
+};
