b3b0315db44c107f95338e41c66d2f22fa5c48c8
Cole <4930129+coleshaw@users.noreply.github.com>
2021-02-10 10:28:53 -0500

    styling plots and some progress? Needs cleanup

--- vulcan/lib/client/jsx/components/workflow/output_renderers/plot.jsx
+++ vulcan/lib/client/jsx/components/workflow/output_renderers/plot.jsx
@@ -44,8 +44,22 @@ export default function Plot({md5sum}) {
       plot={plot}
       config_variables={variables}
       layout={layout}
-      parent_width={width}
+      parent_width={800}
     />
+  );
+}
+
+function padInt(arrayValues, func) {
+  const margin = 1.2;
+  return Math.round(margin * func(...arrayValues));
+}
+
+function vectorize(values) {
+  return new Vector(
+    values.map((val) => ({
+      label: null,
+      value: val
+    }))
   );
 }
 
@@ -62,17 +76,28 @@ function browserStateOf(md5sum, primaryOutputName) {
     //   the consignment?
     const x = primaryConsignment.col_names[0];
     const y = primaryConsignment.col_names[1];
+    let xValues = primaryConsignment.rows.map((r) => r[0]);
+    let yValues = primaryConsignment.rows.map((r) => r[1]);
     let plot = {
       plot_type: 'xy',
       configuration: {
         layout: {
           height: 400,
           width: 600,
-          margin: {}
+          margin: {
+            bottom: 100,
+            left: 100,
+            top: 50,
+            right: 50
+          }
         },
         variables: {
           x_label: x,
-          y_label: y
+          y_label: y,
+          x_min: Math.min(...xValues),
+          y_min: Math.min(...yValues),
+          x_max: Math.max(...xValues),
+          y_max: Math.max(...yValues)
         },
         xLabel: x,
         yLabel: y,
@@ -93,29 +118,62 @@ function browserStateOf(md5sum, primaryOutputName) {
       }
     };
 
-    let xValues = primaryConsignment.rows.map((r) => r[0]);
-    let yValues = primaryConsignment.rows.map((r) => r[1]);
+    // let mockConsignment = {
+    //   x: {
+    //     vector: xValues.map((xVal) => ({
+    //       label: null,
+    //       value: xVal
+    //     }))
+    //   },
+    //   y: {
+    //     vector: yValues.map((yVal) => ({
+    //       label: null,
+    //       value: yVal
+    //     }))
+    //   },
+    //   series0___x: {
+    //     vector: xValues.map((xVal) => ({
+    //       label: null,
+    //       value: xVal
+    //     }))
+    //   },
+    //   series0___y: {
+    //     vector: yValues.map((yVal) => ({
+    //       label: null,
+    //       value: yVal
+    //     }))
+    //   },
+    //   xy____xdomain: {
+    //     vector: [padInt(xValues, Math.min), padInt(xValues, Math.max)]
+    //   },
+    //   xy____ydomain: {
+    //     vector: [padInt(yValues, Math.min), padInt(yValues, Math.max)]
+    //   }
+    // };
+
+    // let test = plotData(plot, mockConsignment);
     let labels = primaryConsignment.row_names;
 
-    const margin = 1.2;
     console.log(xValues);
     console.log(yValues);
+    console.log(labels);
 
     let data = {
-      xdomain: [margin * Math.min(xValues), margin * Math.max(xValues)],
-      ydomain: [margin * Math.min(yValues), margin * Math.max(yValues)],
+      xdomain: [padInt(xValues, Math.min), padInt(xValues, Math.max)],
+      ydomain: [padInt(yValues, Math.min), padInt(yValues, Math.max)],
       plot_series: [
         {
           name: primaryOutputName,
           series_type: 'scatter',
           variables: {
-            x: new Vector(xValues),
-            y: new Vector(yValues),
-            labels: new Vector(labels)
+            x: vectorize(xValues),
+            y: vectorize(yValues),
+            labels: vectorize(labels)
           }
         }
       ]
     };
+    console.log(data);
     return {plot, data};
   };
 }
