f5576100f5d7bde47c2a9e11ff9241e12b8722dd
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-16 14:42:54 -0800

    Gvaihir - Magby (v. 0.0.2): fix (test_Magby)

--- etna/packages/magby/tests/test_Magby.py
+++ etna/packages/magby/tests/test_Magby.py
@@ -35,7 +35,8 @@ class TestMagby(TestCase):
         payload = self.magby._constructPayload(projectName='ipi',
                                                modelName='sample',
                                                recordNames=["IPIADR001.T1"],
-                                               attributeNames=["patient"])
+                                               attributeNames=["patient"],
+                                               format='json')
         self.assertTrue(isinstance(payload, dict))
 
 
