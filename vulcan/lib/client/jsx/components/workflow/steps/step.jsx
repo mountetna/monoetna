f9a1754466c660683c4a34193aa417421ff12f6e
Cole <4930129+coleshaw@users.noreply.github.com>
2021-02-16 14:29:30 -0500

    step icons for status

--- vulcan/lib/client/jsx/components/workflow/steps/step.jsx
+++ vulcan/lib/client/jsx/components/workflow/steps/step.jsx
@@ -1,15 +1,42 @@
 import React, {useState, useContext, useEffect} from 'react';
+
+import Icon from 'etna-js/components/icon';
 import {VulcanContext} from '../../../contexts/vulcan';
 
+// Hacked in for dev
+const STATUS = [];
+STATUS[0] = require('../../../../../server/data/status1.json');
+STATUS[1] = require('../../../../../server/data/status2.json');
+STATUS[2] = require('../../../../../server/data/status3.json');
+STATUS[3] = require('../../../../../server/data/status4.json');
+STATUS[4] = require('../../../../../server/data/status5.json');
+
+// No status for step 6 yet...
+STATUS[5] = require('../../../../../server/data/status5.json');
+
 export default function Step({step, index, active}) {
-  const {setStepIndex} = useContext(VulcanContext);
+  const {setStepIndex, setStatus} = useContext(VulcanContext);
 
   function handleOnClick(index) {
     setStepIndex(index);
+    setStatus(STATUS[index]);
   }
 
+  const icons = {
+    complete: 'check',
+    pending: 'clock',
+    error: 'times-circle'
+  };
+
+  let icon = icons[step.status ? step.status : 'pending'];
+  console.log(icon);
   return (
     <li className={active ? 'active step' : 'step'}>
+      <div className='step-status-icon-wrapper'>
+        <div className='step-status-icon-positioner'>
+          <Icon className='step-status-icon' icon={icon}></Icon>
+        </div>
+      </div>
       {active ? (
         <div className='active-wrapper'>
           <div className='active-indicator'></div>
