cd10f5eea5e3fb6d139ed69b2b604962c9e8e29b
GithubAction <mountetna@users.noreply.github.com>
2021-02-18 06:09:32 +0000

    Artifact npm-etna-js for sha 9046827eb130c851a26af22114ec5f4c1ee0083e

---
+++ selectors/user-selector.jsx
@@ -0,0 +1,23 @@
+import * as Reselect from 'reselect';
+
+export const selectUser = ({user})=>( user || {} );
+
+export const selectUserPermissions = Reselect.createSelector(
+  selectUser,
+  (user)=>{
+    return ('permissions' in user) ? user.permissions : [];
+  }
+);
+
+export const selectUserProjectRole = Reselect.createSelector(
+  selectUserPermissions,
+  (permissions) => {
+    let perm = permissions[CONFIG.project_name];
+    return perm ? perm.role : null
+  }
+);
+
+export const selectIsEditor = Reselect.createSelector(
+  selectUserProjectRole,
+  role => role && (role === 'administrator' || role === 'editor')
+);
