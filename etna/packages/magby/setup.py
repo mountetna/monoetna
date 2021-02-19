a1e2e14fe645b2fd87937df9b952d7e52b70e468
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-18 17:28:43 -0800

    Gvaihir - Magby (v. 0.0.2): fix (branches) - imports

--- etna/packages/magby/setup.py
+++ etna/packages/magby/setup.py
@@ -12,6 +12,7 @@ setup(
     python_requires='>=3.6',
 	install_requires=['requests',
                       'pandas',
-                      'betamax'],
+                      'betamax',
+                      'betamax_serializers'],
 
 )
