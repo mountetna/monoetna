03f85064df1ca5a6083757142ab9a07737fd69ee
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-19 11:55:23 -0800

    Gvaihir - Geoline (v. 0.0.3):  (initial)

---
+++ Geoline/Geoline/Geoline.py
@@ -0,0 +1,14 @@
+
+'''
+call in the CLI (url, token)
+prompt select project
+get models
+prompt select model or go back
+get attrs
+prompt select attr o
+
+'''
+
+class Geoline:
+    def __init__(self):
+        pass
