dd57ebbcaa59f64d5d48f7dcbb1d9c69b0983135
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-22 09:09:55 -0800

    Gvaihir - Geoline (v. 0.0.3): feat (Geoline) - WIP

--- Geoline/Geoline/Geoline.py
+++ Geoline/Geoline/Geoline.py
@@ -1,14 +1,63 @@
+from typing import List, Dict
 
+
+from magby import Magby
 '''
 call in the CLI (url, token)
 prompt select project
-get models
-prompt select model or go back
-get attrs
-prompt select attr o
-
+autopopulate "Series"
+prompt assays or cancel
+for each column in "Sample" suggest model attr
+    confirm, suggest your own, back, cancel
+"Processed data files" 
+    confirm, suggest your own, back, cancel
+"RAW data files" 
+    confirm, suggest your own, back, cancel
+"Pair End" 
+    confirm, suggest your own, back, cancel
+    
+START
 '''
 
 class Geoline:
-    def __init__(self):
-        pass
+    def __init__(self, url: str, token: str) -> None:
+        self._url = url
+        self._token = token
+
+
+    def _samplesSection(self,
+                        title: str,
+                        sourceName: str,
+                        organism: str,
+                        characteristics: Dict,
+                        molecule: str,
+                        description: str,
+                        processedDataFile: str,
+                        rawFile: Dict):
+        out = {
+            'title': title,
+            'source name': sourceName,
+            'organism': organism,
+            'characteristics': characteristics,
+            'molecule': molecule,
+            'description': description,
+            'processed data file': processedDataFile,
+            'raw file': rawFile
+        }
+        return self._flatten(out)
+
+
+    def _flatten(self, dictionary, parent_key='', sep=' '):
+        items = []
+        for currKey, currVal in dictionary.items():
+            new_key = parent_key + sep + currKey if parent_key else currKey
+            if isinstance(currVal, dict):
+                items.extend(self._flatten(currVal, new_key, sep=sep).items())
+            else:
+                items.append((new_key, currVal))
+        return dict(items)
+
+
+
+
+
---
+++ Geoline/tests/test_Geoline.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from magby import Magby
+
+class TestGeoline(TestCase):
+    def setUp(self) -> None:
+        self.geoline = Geoline(url, token)
+
+
