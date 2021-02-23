f0d04155d2f82658f9b1e1c5d6f7c22292d55ad7
Cole <4930129+coleshaw@users.noreply.github.com>
2021-02-23 10:22:02 -0500

    Artifact npm-etna-js for sha 054749276887c09bb9818cb2a5769985e8f0594e

---
+++ actions/__tests__/location.test.js
@@ -0,0 +1,36 @@
+import * as actions from '../location_actions';
+import {mockStore} from '../../spec/helpers';
+
+const PROJECT_NAME = 'labors';
+
+describe('setLocation', () => {
+  it('adds a new state to the history and updates the store', () => {
+    const store = mockStore({});
+    const link = 'http://localhost/new_location';
+
+    store.dispatch(actions.setLocation(link));
+
+    // we have a new location
+    expect(location.href).toEqual(link);
+    expect(store.getActions()).toEqual([{link, type: 'UPDATE_LOCATION'}]);
+
+    // the history is the same length
+    expect(history.length).toEqual(1);
+  });
+});
+
+describe('pushLocation', () => {
+  it('adds a new state to the history and updates the store', () => {
+    const store = mockStore({});
+    const link = 'http://localhost/new_location';
+
+    store.dispatch(actions.pushLocation(link));
+
+    // we have a new location
+    expect(location.href).toEqual(link);
+    expect(store.getActions()).toEqual([{link, type: 'UPDATE_LOCATION'}]);
+
+    // the history is longer
+    expect(history.length).toEqual(2);
+  });
+});
