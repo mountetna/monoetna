fd52ff7b2b3f8093b988c182e27a54e7bc707157
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-02-23 20:52:07 -0800

    Gvaihir - Geoline (v. 0.0.3): feat (Geoline) - parsing a dictionary for each section, not tested.

--- Geoline/Geoline/Geoline.py
+++ Geoline/Geoline/Geoline.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 from ..Geoline.workflows import *
-
+from functools import partial
 from magby import Magby
 '''
 call in the CLI (url, token)
@@ -34,9 +34,22 @@ class Geoline:
             else:
                 samples.update({field: aw})
 
-    # THIS
-    def _updater(self, prev, curr):
-        pass
+
+    ### TEST THIS STUFF
+    def _updater(self, section: dict, prev: int=0, curr: int=0):
+        if (prev < 0) | (curr > len(section)):
+            return section
+        curr = section.items()[curr]
+        aw = askAttribute(curr[0], curr[1])
+        switch = {
+            'Y': partial(self._updater, curr, curr + 1),
+            '0': partial(self._updater, prev - 1, curr - 1),
+            'STOP': lambda x: x
+        }
+        default =  lambda x: self._updater(x.update({curr[0]: curr[1]}), curr, curr + 1)
+        executor = switch.get(aw, default)
+        return executor(section)
+
 
 
 
