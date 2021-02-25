184fa0c9f6c6a0d6f814fb251d0c389e1ca4d100
Zach Collins <recursive.cookie.jar@gmail.com>
2021-02-25 13:11:15 -0800

    Vulcan now returns a linear, serialized path as the first result (#249)

--- etna/spec/directed_graph_spec.rb
+++ etna/spec/directed_graph_spec.rb
@@ -41,4 +41,24 @@ describe DirectedGraph do
       end
     end
   end
+
+  describe '#serialized_path_from' do
+    it 'generates a linear serialization of convergent and divergent paths' do
+      graph = DirectedGraph.new
+      graph.add_connection('a', 'b')
+      graph.add_connection('a', 'c')
+      graph.add_connection('c', 'd')
+      graph.add_connection('b', 'd')
+      graph.add_connection('d', 'e')
+      graph.add_connection('d', 'f')
+      graph.add_connection('e', 'g')
+      graph.add_connection('f', 'g')
+      graph.add_connection('g', 'h')
+      graph.add_connection('g', 'i')
+
+      expect(graph.serialized_path_from('a')).to eql([
+          "a", "c", "b", "d", "f", "e", "g", "h", "i"
+      ])
+    end
+  end
 end
