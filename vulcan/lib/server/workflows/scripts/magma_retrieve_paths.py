76db7a771c09bb194823d56a86e26ba47e3f0b4e
dtm2451 <Daniel.Bunis@ucsf.edu>
2021-02-25 09:44:52 -0800

    Get POOL -> indiv records working

--- vulcan/lib/server/workflows/scripts/magma_retrieve_paths.py
+++ vulcan/lib/server/workflows/scripts/magma_retrieve_paths.py
@@ -7,11 +7,11 @@ token = input_var('token')
 # Improve these with magby.getModels or similar in future
 seq_pool_model_name = 'sc_rna_seq_pool' 
 seq_model_name = 'sc_rna_seq'
-h5_attr_name = ['raw_counts_h5']
+h5_attr_name = 'raw_counts_h5'
 project_name = "mvir1" # <-- should be some sort of environment variable in future
 magma = Magby.Magby(url='https://magma.ucsf.edu', token=token) # <-- <-- same as above comment
 
-singlular_record_ids = []
+singular_record_ids = []
 # Loop per record
 for rec in input_records:
     
@@ -20,26 +20,28 @@ for rec in input_records:
         ids_ret = magma.retrieve(
             projectName= project_name,
             modelName= seq_pool_model_name,
-            recordNames= '[' + rec + ']',
+            recordNames= rec,
             attributeNames= seq_model_name,
             dataType= 'meta')
         ids_str = ids_ret[seq_model_name]
         # split by ", "
-        new_recs = ids_str.split(", ")
+        new_recs = ids_str[0].split(", ")
+
+        singular_record_ids.extend(new_recs)
     else:
         # else, add id directly
         new_recs = rec
 
-    singlular_record_ids.append(new_recs)
+        singular_record_ids.append(new_recs)
 
 # Ensure uniqueness
-singlular_record_ids = list(set(singlular_record_ids))
+singular_record_ids = list(set(singular_record_ids))
 
 # Retrieve dataframe of record_id, raw_counts_h5 file location
 h5_locations = magma.retrieve(
     projectName= project_name,
     modelName= seq_model_name,
-    recordNames= singlular_record_ids,
+    recordNames= singular_record_ids,
     attributeNames= h5_attr_name,
     dataType= 'meta')
 
