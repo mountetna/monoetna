b09a6ac7345028c288443ace795fe301b61576e7
Cole <4930129+coleshaw@users.noreply.github.com>
2021-03-01 13:08:16 -0500

    consolidate code

--- etna/packages/etna-js/upload/api/upload_api.jsx
+++ etna/packages/etna-js/upload/api/upload_api.jsx
@@ -1,6 +1,6 @@
-// Use the etna-js/upload/utils/fetch version
+// Use the etna-js/utils/base_fetch version
 //   instead of etna-js/utils/fetch.
-import { form_post, json_post } from '../utils/upload_fetch';
+import { form_post, json_post } from '../../utils/base_fetch';
 
 export const postAuthorizeUpload = (base_url, project_name, bucket_name, file_path) => {
   let request = { project_name, file_path, bucket_name };
