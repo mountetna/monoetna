853be991205f46ec470703d8d65a44c85181ca0a
Cole <4930129+coleshaw@users.noreply.github.com>
2021-03-04 09:42:12 -0500

    throb the vulcan logo

--- vulcan/lib/client/jsx/components/browser.jsx
+++ vulcan/lib/client/jsx/components/browser.jsx
@@ -2,15 +2,12 @@
 import React from 'react';
 import 'regenerator-runtime/runtime';
 
-import {VulcanProvider} from '../contexts/vulcan';
 import WorkflowManager from './workflow/workflow_manager';
 
 export default function Browser() {
   return (
     <main className='vulcan-browser browser'>
-      <VulcanProvider>
-        <WorkflowManager></WorkflowManager>
-      </VulcanProvider>
+      <WorkflowManager></WorkflowManager>
     </main>
   );
 }
