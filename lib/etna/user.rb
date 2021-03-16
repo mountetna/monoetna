85d345201d7ed3a79495235b6f0e9a43e7f00348
GithubAction <mountetna@users.noreply.github.com>
2021-03-16 00:08:36 +0000

    Squashed 'etna/' changes from 1eec41bb9..90dac9990
    90dac9990 nit
    d7e17cbd9 support js side
    91d78675a add super viewer and editor
    
    git-subtree-dir: etna
    git-subtree-split: 90dac999002fefaefe9e33137bbde336a98edbbf

--- lib/etna/user.rb
+++ lib/etna/user.rb
@@ -46,7 +46,7 @@ module Etna
       viewer: /[Vv]/,
       restricted: /[AEV]/,
     }
-    def has_roles(project, *roles)
+    def has_any_role?(project, *roles)
       perm = permissions[project.to_s]
 
       return false unless perm
@@ -55,15 +55,23 @@ module Etna
     end
 
     def is_superuser? project=nil
-      has_roles(:administration, :admin)
+      has_any_role?(:administration, :admin)
+    end
+
+    def is_supereditor? project=nil
+      has_any_role?(:administration, :admin, :editor)
+    end
+
+    def is_superviewer? project=nil
+      has_any_role?(:administration, :admin, :editor, :viewer)
     end
 
     def can_edit? project
-      is_superuser? || has_roles(project, :admin, :editor)
+      is_supereditor? || has_any_role?(project, :admin, :editor)
     end
 
     def can_view? project
-      is_superuser? || has_roles(project, :admin, :editor, :viewer)
+      is_superviewer? || has_any_role?(project, :admin, :editor, :viewer)
     end
 
     # superusers - administrators of the Administration group - cannot
@@ -75,7 +83,7 @@ module Etna
     end
 
     def is_admin? project
-      is_superuser? || has_roles(project, :admin)
+      is_superuser? || has_any_role?(project, :admin)
     end
 
     def active? project=nil
