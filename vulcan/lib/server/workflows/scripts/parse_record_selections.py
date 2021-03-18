82a087e3e119e9de9c136f3301c5fe701c4a88b9
dtm2451 <daniel.bunis@ucsf.edu>
2021-03-18 02:10:41 -0700

    get options script and improve parsing

--- vulcan/lib/server/workflows/scripts/parse_record_selections.py
+++ vulcan/lib/server/workflows/scripts/parse_record_selections.py
@@ -11,19 +11,19 @@ tubes = input_json('tubes')
 ### Need to make these actually do what they say!
 def ignoreAllWhenOthers(x):
     return x
-def removeBlank(x):
+def removeBlankWhenOthers(x):
     return x
 experiments = ignoreAllWhenOthers(experiments)
 tissues = ignoreAllWhenOthers(tissues)
 ### May not need these actually?
-pools = removeBlank(pools)
-tubes = removeBlank(tubes)
+pools = removeBlankWhenOthers(pools)
+tubes = removeBlankWhenOthers(tubes)
 
 # Setup query targets
-expt_model = 'experiment'
-expt_attr = '::' + 'identifier'
-tiss_model = 'biospecimen'
-tiss_attr = '::' + 'biospecimen_type'
+# expt_model = 'experiment'
+# expt_attr = '::' + 'identifier'
+# tiss_model = 'biospecimen'
+# tiss_attr = '::' + 'biospecimen_type'
 project_name = "xcrs1"  # <-- should be some sort of environment variable in future
 
 magma = Magby.Magby(url=magma_host, token=token,
@@ -35,18 +35,22 @@ tube_records = []
 # Experiment and Tissue (AND logic)
 selection_terms = []
 if experiments!='all':
-    selection_terms.append([expt_model, expt_attr, '::in', experiments])
+    selection_terms.append(
+        ["biospecimen", "::all",
+        "subject", "::all",
+        "experiment", "::identifier", '::in', experiments])
 if tissues!='all':
-    selection_terms.append([tiss_model, tiss_attr, '::in', tissues])
+    selection_terms.append(
+        ["biospecimen", "biospecimen_type", '::in', tissues])
 
 if len(selection_terms)!=0:
-    if len(selection_terms)>1:
-        selection_terms = ['::and', selection_terms]
-    tube_records = magma.query(projectName=project_name, 
+    selection_terms = ['::and', selection_terms, '::all', '::identifier']
+    tube_records = magma.query(
+        projectName=project_name, 
         queryTerms=[
             seq_model_name,
-            selection_terms,
-            '::all', '::identifier'])['answer']
+            selection_terms
+            )['answer']
 
 # Pools and Tubes (OR logic / just add them!)
 tube_records = tube_records.extend(
---
+++ vulcan/lib/server/workflows/scripts/retrieve_selection_options.py
@@ -0,0 +1,51 @@
+from archimedes.functions.dataflow import input_var, output_json
+from archimedes.functions.environment import token, magma_host, app_env
+from archimedes.functions.magby import Magby
+from archimedes.functions.list import unique, flatten
+
+project = "xcrs1"
+seq_model_name = "sc_seq"
+seq_pool_model_name = "sc_seq_pool"
+magma = Magby.Magby(url=magma_host, token=token,
+                    verify=(app_env == 'production'))
+
+experiments = magma.query(
+    project,
+    queryTerms=[
+        "experiment",
+        ["subject", "::all",
+        "biospecimen", "::all",
+        seq_model_name, "::all", '::has', 'raw_counts_h5'],
+        "::all", '::identifier'])['answer']
+# add "all" and flatten/unique
+experiments = ["all"].extend(unique(flatten(experiments)))
+
+tissues = magma.query(
+    project,
+    queryTerms=[
+        "biospecimen",
+        [seq_model_name, "::all", '::has', 'raw_counts_h5'],
+        "::all", '::identifier'])['answer']
+tissues = ["all"].extend(unique(flatten(tissues)))
+
+pools = magma.query(
+    project,
+    queryTerms=[
+        seq_model_name,
+        ['::has', 'raw_counts_h5'],
+        "::all", '::identifier'])['answer']
+# Add "" and flatten/unique
+pools = [""].extend(unique(flatten(pools)))
+
+recs = magma.query(
+    project,
+    queryTerms=[
+        seq_model_name,
+        ['::has', 'raw_counts_h5'],
+        "::all", '::identifier'])['answer']
+recs = [""].extend(unique(flatten(recs)))
+
+output_json(experiments, 'experiments')
+output_json(tissues, 'tissues')
+output_json(pools, 'pools')
+output_json(recs, 'records')
