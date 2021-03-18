111ff68447601c2b7eb20d0f6a361e56fa6c69a0
Cole <4930129+coleshaw@users.noreply.github.com>
2021-03-18 15:04:08 -0400

    close primary inputs when done

--- vulcan/lib/client/jsx/components/workflow/session/input_group.jsx
+++ vulcan/lib/client/jsx/components/workflow/session/input_group.jsx
@@ -1,15 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+
+import {VulcanContext} from '../../../contexts/vulcan';
 
 import UserInput from '../user_interactions/inputs/user_input';
 import {inputGroupName} from '../../../utils/workflow';
+import {
+  completedUiStepsSelector,
+  nextUiStepsSelector
+} from '../../../selectors/workflow';
 
 export default function InputGroup({inputs, onChange}) {
+  const context = useContext(VulcanContext);
+  let {session} = context;
+
   const [open, setOpen] = useState(true);
   let groupName = inputGroupName(inputs[0]);
 
   function toggleInputs() {
     setOpen(!open);
   }
+
+  useEffect(() => {
+    if (session && session.inputs) {
+      // Automatically collapse the input group if there are
+      //   pending or completed UI input steps.
+      let uiSteps = completedUiStepsSelector(context);
+      let nextUiSteps = nextUiStepsSelector(context);
+
+      if (uiSteps.length > 0 || nextUiSteps.length > 0) setOpen(false);
+    }
+  }, [session]);
 
   return (
     <div className='inputs-pane'>
