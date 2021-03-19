5eb2f5a8dba34e7f683d0cb8471a0e73f3e8f11e
dtm2451 <daniel.bunis@ucsf.edu>
2021-03-18 18:28:06 -0700

    finish options grab script

--- vulcan/lib/server/workflows/scripts/retrieve_selection_options.py
+++ vulcan/lib/server/workflows/scripts/retrieve_selection_options.py
@@ -1,16 +1,15 @@
 from archimedes.functions.dataflow import input_var, output_json
-from archimedes.functions.environment import token, magma_host, app_env
+from archimedes.functions.environment import token, magma_host, project_name, app_env
 from archimedes.functions.magby import Magby
 from archimedes.functions.list import unique, flatten
 
-project = "xcrs1"
 seq_model_name = "sc_seq"
 seq_pool_model_name = "sc_seq_pool"
 magma = Magby.Magby(url=magma_host, token=token,
                     verify=(app_env == 'production'))
 
 experiments = magma.query(
-    project,
+    project_name,
     queryTerms=[
         "experiment",
         ["subject", "::all",
@@ -21,30 +20,30 @@ experiments = magma.query(
 experiments = ["all"] + unique(flatten(experiments))
 
 tissues = magma.query(
-    project,
+    project_name,
     queryTerms=[
         "biospecimen",
         [seq_model_name, "::all", '::has', 'raw_counts_h5'],
-        "::all", '::identifier'])['answer']
+        "::all", 'biospecimen_type'])['answer']
 tissues = ["all"] + unique(flatten(tissues))
 
 pools = magma.query(
-    project,
+    project_name,
     queryTerms=[
         seq_pool_model_name,
         [seq_model_name, "::all",
         '::has', 'raw_counts_h5'],
         "::all", '::identifier'])['answer']
 # Add "" and flatten/unique
-pools = [""] + unique(flatten(pools))
+pools = ["NO DIRECT SELECTIONS"] + unique(flatten(pools))
 
 recs = magma.query(
-    project,
+    project_name,
     queryTerms=[
         seq_model_name,
         ['::has', 'raw_counts_h5'],
         "::all", '::identifier'])['answer']
-recs = [""] + unique(flatten(recs))
+recs = ["NO DIRECT SELECTIONS"] + unique(flatten(recs))
 
 output_json(experiments, 'experiments')
 output_json(tissues, 'tissues')
