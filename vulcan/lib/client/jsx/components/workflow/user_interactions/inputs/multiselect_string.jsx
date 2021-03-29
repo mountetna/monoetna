11f43061fae103a1e44b2c7625d417b4aef71a37
Cole <4930129+coleshaw@users.noreply.github.com>
2021-03-29 07:23:58 -0400

    sort the multiselect options

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/multiselect_string.jsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/multiselect_string.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 import ListInput from 'etna-js/components/inputs/list_input';
 import DropdownInput from 'etna-js/components/inputs/dropdown_input';
+import {nextUiStepsSelector} from '../../../../selectors/workflow';
 
 export default function MultiselectStringInput({input, onChange}) {
   if (!input || !onChange) return null;
+
+  var collator = new Intl.Collator(undefined, {
+    numeric: true,
+    sensitivity: 'base'
+  });
 
   return (
     <ListInput
@@ -18,7 +24,7 @@ export default function MultiselectStringInput({input, onChange}) {
           : []
       }
       itemInput={DropdownInput}
-      list={input.options || []}
+      list={(input.options && input.options.sort(collator.compare)) || []}
       onChange={(e) => {
         onChange(input.name, e);
       }}
