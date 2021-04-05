e8d4c55ef1dbcb7dd7550a7c923b82de15af2002
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2021-04-05 13:37:06 -0700

    add generate token workflow

--- etna/lib/etna/clients/janus/client.rb
+++ etna/lib/etna/clients/janus/client.rb
@@ -62,6 +62,20 @@ module Etna
         token = nil
         @etna_client.post('/api/tokens/task/validate', validate_task_token_request)
       end
+
+      def get_nonce
+        @etna_client.get('/api/tokens/nonce').body
+      end
+
+      def generate_token(token_type, signed_nonce: nil, project_name: nil)
+        response = @etna_client.with_headers(
+          'Authorization' => signed_nonce ? "Signed-Nonce #{signed_nonce}" : nil
+        ) do
+          post('/api/tokens/generate', token_type: token_type, project_name: project_name)
+        end
+
+        response.body
+      end
     end
   end
 end
