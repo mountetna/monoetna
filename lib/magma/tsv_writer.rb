dc131600d6a0a08908247ab25a82d909150af165
GithubAction <mountetna@users.noreply.github.com>
2021-05-05 11:58:51 +0000

    Squashed 'magma/' changes from 8f8fcaa36..5b5d91108
    5b5d91108 fix comment
    a394859d2 refactor per feedback
    e032bd582 tweak
    f5e12f5e1 use output format to determine sliced column names
    9208e37a3 refactor
    37eaf36f2 tweak
    62f8d8911 tweak
    20fe3f1b3 transpose TSV
    55541dd6b show matrix data even if not expanded
    e8004111f reword from unmelt to expand
    f7dfe34f9 tweak wording and separator
    9a2f380fb spec
    3a6321974 update spec
    2e2582144 bug with forms
    97bb0f7c7 spec
    c57e4f5c9 wording
    ece6f6eba tweak
    49f162db1 more
    155d47894 clean up
    9b15a30cf add unmelt_matrices flag
    c16d8661b refactor
    fc1521548 functional but very, very ugly
    f256bda39 download TSV also sends output_predicates
    cca7d25ee WIP
    da3a32bfc set matrix filters to ::has
    4cf56c4b0 splitting out an output_predicate param to /retrieve
    
    git-subtree-dir: magma
    git-subtree-split: 5b5d9110862de57b812019109ce79b4a5f659410

--- lib/magma/tsv_writer.rb
+++ lib/magma/tsv_writer.rb
@@ -1,14 +1,27 @@
 class Magma
   class TSVWriter
-    def initialize(model, retrieval, payload)
+    def initialize(model, retrieval, payload, opts={})
       @model = model
       @retrieval = retrieval
       @payload = payload
+
+      @expand_matrices = opts[:expand_matrices]
+      @transpose = opts[:transpose]
     end
 
-    def write_tsv
+    def write_tsv(&block)
       @payload.add_model(@model, @retrieval.attribute_names)
+      @payload.set_output_format(@retrieval.output_format)
+      @payload.set_options(
+        expand_matrices: @expand_matrices
+      )
 
+      @transpose ?
+        transpose_records(&block) :
+        standard_records(&block)
+    end
+
+    def standard_records
       yield @payload.tsv_header
 
       @retrieval.each_page do |records|
@@ -16,6 +29,35 @@ class Magma
         yield @payload.to_tsv
         @payload.reset(@model)
       end
+    end
+
+    def transpose_records
+      # Collect all data, then yield the transposed results
+      [].tap do |buffer|
+        buffer << tsv_row_to_array(@payload.tsv_header)
+
+        @retrieval.each_page do |records|
+          @payload.add_records(@model, records)
+          buffer.concat(tsv_records_to_array(@payload.to_tsv))
+          @payload.reset(@model)
+        end
+      end.transpose.each do |row|
+        yield array_to_tsv_row(row)
+      end
+    end
+
+    private
+
+    def tsv_row_to_array(row)
+      CSV.parse_line(row, col_sep: "\t")
+    end
+
+    def tsv_records_to_array(records)
+      CSV.parse(records, col_sep: "\t")
+    end
+
+    def array_to_tsv_row(array)
+      array.join("\t") + "\n"
     end
   end
 end
