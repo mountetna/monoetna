9722946d8650ef227cf1b77ac57c64ebb7e85f77
Daniel Bunis <daniel.bunis@ucsf.edu>
2021-05-10 12:26:07 -0700

    add error status reporting to archimedes curl_data fxn

--- archimedes/archimedes/functions/dataflow/grab.py
+++ archimedes/archimedes/functions/dataflow/grab.py
@@ -2,9 +2,11 @@ from requests import Session
 from archimedes.functions.environment import token
 
 def curl_data(url, token = token, verify = True):
-    return Session().get(
+    get = Session().get(
         url,
         headers={
             "Content-Type" : "application/json",
             "Authorization" : f"Etna {token}"},
         verify=verify)
+    get.raise_for_status()
+    return get
