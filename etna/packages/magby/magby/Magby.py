b3749da93663c11c39d63e30946196612d3617e5
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-05-11 11:56:47 -0700

    Revert "Gvaihir - Magby (v. 0.0.3):" fix (everywhere) - reformatting

--- etna/packages/magby/magby/Magby.py
+++ etna/packages/magby/magby/Magby.py
@@ -6,13 +6,14 @@ from magby.Magma import *
 
 _session = Session()
 
+
 class Magby(object):
     def __init__(self, url: str, token: str):
-        '''
+        """
         Client-facing class to interface with Magma (or Janus - for projects)
         :param url: str. Magma url
         :param token: str. Private token
-        '''
+        """
         self._url = url.strip('/')
         self._token = token
 
@@ -32,16 +33,15 @@ class Magby(object):
     def token(self, newToken: str) -> None:
         self._token = newToken
 
-
     def retrieve(self,
                  projectName: str,
                  modelName: str,
-                 recordNames: Union[List, str]='all',
-                 attributeNames: Union[List, str]='all',
-                 dataType: str='meta',
-                 filter='',
+                 recordNames: Union[List, str] = 'all',
+                 attributeNames: Union[List, str] = 'all',
+                 dataType: str = 'meta',
+                 filterBy='',
                  **kwargs) -> Union[pd.DataFrame, Dict]:
-        '''
+        """
         Method to retrieve data from Magma
         :param projectName: str. Project name in Magma
         :param modelName: str. Name of a model in Magma
@@ -52,17 +52,17 @@ class Magby(object):
                         'json' - works for any type of data in Magma. Output - Dictionary
                         'mtx' - matrix of floats. Suitable for gene counts or  similar. Corresponds to 'matrix'
                                 in Magma data types. Output - pandas.DataFrame
-        :param filter: str. Filter to pass to Magma query. See magma docs
+        :param filterBy: str. Filter to pass to Magma query. See magma docs
         :param kwargs: Additional kwargs to pass to Magma class. Used for passing requests.Session with custom proxies
                         and other attributes
         :return: Data from magma in the form of pandas.DataFrame or Dict, depending on the dataType argument
-        '''
+        """
         attributeNames = self._isAll(attributeNames)
         if (dataType == 'mtx') & (len(attributeNames) > 1):
             raise MagmaError('Magby.retrieve(): retrieval of matrix data is limited to a single attribute at a time')
         typeSelection = self._selectFormat(dataType)
         payload = self._constructPayload(projectName, modelName, recordNames, attributeNames,
-                                         format=typeSelection[0], filter=filter)
+                                         formatBy=typeSelection[0], filterBy=filterBy)
         magma = self._recordMagmaObj(endpoint='retrieve', fmt=typeSelection[0], **kwargs)
         content, _ = self._call_api(payload, magma)
         if dataType == 'mtx':
@@ -70,19 +70,18 @@ class Magby(object):
         else:
             return self._wrapReturn(typeSelection[1])(content)
 
-
     def query(self,
               projectName: str,
               queryTerms: List,
               **kwargs) -> Dict:
-        '''
+        """
         Performs an expressive query to Magma.
         :param projectName: str. Project name in Magma
         :param queryTerms: List[str]. Query terms to magma. See Magma documentation
         :param kwargs: Additional kwargs to pass to Magma class. Used for passing requests.Session with custom proxies
                         and other attributes
         :return: Dict
-        '''
+        """
         typeSelection = self._selectFormat('json')
         payload = {
             "project_name": projectName,
@@ -93,36 +92,32 @@ class Magby(object):
 
         return self._wrapReturn(typeSelection[1])(content)
 
-
     def getProjects(self, **kwargs) -> List:
-        '''
+        """
         Gets available projects in Magma for a given user (token)
         :param kwargs: Additional kwargs to pass to Magma class. Used for passing requests.Session with custom proxies
                         and other attributes
         :return: List of Dict with abbreviated project names, full project names and descriptions
-        '''
+        """
         janusUrl = self._janusUrl()
         magma = self._recordMagmaObj('retrieve', 'json', **kwargs)
         janusProjects = magma._janusProjectsCall(janusUrl)
         return janusProjects['projects']
 
-
-
-    ### PRIVATE METHODS
     def _constructPayload(self,
                           projectName: str,
                           modelName: Union[List, str],
                           recordNames: Union[List, str],
                           attributeNames: Union[List, str],
-                          format: str,
-                          filter: str='') -> Dict:
+                          formatBy: str,
+                          filterBy: str = '') -> Dict:
         payload = {
             "project_name": projectName,
             "model_name": modelName,
             "record_names": self._isAll(recordNames),
             "attribute_names": self._isAll(attributeNames),
-            "format": format,
-            "filter": filter
+            "format": formatBy,
+            "filter": filterBy
         }
         return payload
 
@@ -141,24 +136,22 @@ class Magby(object):
 
     def _janusUrl(self) -> str:
         janusUrl = self._url.replace('magma', 'janus')
-        return ('/'.join([janusUrl, 'projects']))
+        return '/'.join([janusUrl, 'projects'])
 
-
-    def _recordMagmaObj(self, endpoint: str, fmt: str= 'json', session=_session) -> Magma:
+    def _recordMagmaObj(self, endpoint: str, fmt: str = 'json', session=_session) -> Magma:
         return Magma(url=self._url, token=self._token, endpoint=endpoint,
                      fmt=fmt, session=session)
-
 
     def _call_api(self, payload: Dict, magma: Magma):
         return magma.magmaCall(payload)
 
     @staticmethod
     def _selectFormat(dataType: str) -> List:
-        '''
+        """
         Selector for types of outputs to retrieve from magma
         :param dataType: str
         :return: List where [0] is magma output format, [1] is a desired Magby output
-        '''
+        """
         selection = {
             'meta': ['tsv', 'meta'],
             'json': ['json', 'json'],
@@ -176,19 +169,10 @@ class Magby(object):
 
     @staticmethod
     def _walkMatrix(response: Dict, modelName: str, attributeName: str):
-        observations = {x: response['models'][modelName]['documents'][x][attributeName] for x in response['models'][modelName]['documents']}
+        observations = {x: response['models'][modelName]['documents'][x][attributeName] for x in
+                        response['models'][modelName]['documents']}
         features = response['models'][modelName]['template']['attributes'][attributeName]['options']
         return pd.DataFrame(observations, index=features)
-
-
-
-
-
-
-
-
-
-
 
     # TODO
 
@@ -201,12 +185,7 @@ class Magby(object):
     def _getAttributes(self, payload) -> List:
         pass
 
-
     # TODO functions
 
-
     def _validUrl(self, url: str) -> bool:
-        pass    # TODO implement URL validation
-
-
-
+        pass  # TODO implement URL validation
--- etna/packages/magby/magby/Magma.py
+++ etna/packages/magby/magby/Magma.py
@@ -7,26 +7,25 @@ _session = Session()
 
 
 class MagmaError(Exception):
-    '''
+    """
     errors corresponding to misuse of Magma
-    '''
+    """
 
 
 class Magma(object):
     def __init__(self, url: str,
                  token: str,
                  endpoint: str,
-                 fmt: str='json',
-                 session: Session=_session) -> None:
-
-        '''
+                 fmt: str = 'json',
+                 session: Session = _session) -> None:
+        """
         Private class to interface with Magma
         :param url: Magma url
         :param token: Private Janus token
         :param endpoint: Magma (and janus) endpoints for HTTP requests
         :param fmt: Format of Magma output
         :param session: Instance of requests.Session. Needed to modify proxies and SSL certificate verification
-        '''
+        """
 
         allowedEndpoints = ['update', 'retrieve', 'query', 'update_model']
         if endpoint not in allowedEndpoints:
@@ -35,9 +34,8 @@ class Magma(object):
         self._token = token
         self._fmt = fmt
         self._session = session
-        self._headers = {"Content-Type" : "application/json",
-                         "Authorization" : f"Etna {self._token}"}
-
+        self._headers = {"Content-Type": "application/json",
+                         "Authorization": f"Etna {self._token}"}
 
     def getResponseContent(self, response) -> Union[Dict, StringIO]:
         switch = {
@@ -46,21 +44,18 @@ class Magma(object):
         }
         return switch[self._fmt](response)
 
-
     def _parseJSON(self, response) -> Dict:
         return json.loads(response.text, strict=False)
-
 
     def _parseText(self, response) -> StringIO:
         return StringIO(response.text)
 
-
     def magmaCall(self, payload: Dict) -> Tuple:
-        '''
+        """
         Makes an API call to Magma and returns data and current headers
         :param payload: Dict. Payload
         :return: Tuple[Dict, Dict]. Dictionary of content, dictionary of response headers
-        '''
+        """
         response = self._session.post(
             self._url,
             data=json.dumps(payload),
@@ -69,18 +64,13 @@ class Magma(object):
         return content, response.headers
 
     def _janusProjectsCall(self, url: str) -> Dict:
-        '''
+        """
         Get a projects object form Janus for a given user/token
         :param url: Janus/projects url
         :return: Dictionary of projects
-        '''
+        """
         response = self._session.get(
             url,
             headers=self._headers)
         content = self.getResponseContent(response)
         return content
-
-
-
-
-
