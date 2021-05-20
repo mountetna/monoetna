15f215df7293367bcacd51e9b777f91deaec9755
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-05-20 16:09:52 -0700

    Gvaihir - vulcan-dex (v. 0.0.1): fix (data_from_remote) - no use of `if __name__ == "__main__"`???

--- vulcan/lib/server/workflows/scripts/dge.py
+++ vulcan/lib/server/workflows/scripts/dge.py
@@ -1,11 +1,14 @@
 from archimedes.functions.dataflow.DataIO import LocalData
-from archimedes.functions.dataflow import input_path
+from archimedes.functions.dataflow import input_path, input_var
 from archimedes.functions.scanpy import scanpy as sc
 
-def dge_on_features(scdata: AnnData, **kwargs) -> AnnData:
-    pass
+def dge_on_features() -> Dict:
+    rank_genes_groups_kwargs = {
+        input_var('dge_method')
+    }
+    return rank_genes_groups_kwargs
 
-def main(rank_genes_groups_kwargs: Dict):
+def main(rank_genes_groups_kwargs: Dict) -> None:
     local_anndata = LocalData(input_path('leiden_anndata.h5ad'))
     local_anndata.io_wrapper(
         inner_func=sc.tl.rank_genes_groups,
