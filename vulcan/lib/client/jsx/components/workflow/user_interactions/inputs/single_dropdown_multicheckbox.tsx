10733edf5cfedde2d36fa21dc6614f781a14fc2b
antonogorodnikov <anton.ogorodnikov@ucsf.edu>
2021-05-24 09:16:19 -0700

    Gvaihir - vulcan-dex (v. 0.0.1): feat (single_dropdown_multicheckbox) - wip on the drop down and checkboxes component

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/single_dropdown_multicheckbox.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/single_dropdown_multicheckbox.tsx
@@ -1,57 +1,26 @@
-// Input component that takes nested object
-//   and shows the keys one level at a time.
-// Returns the last "Leaf" that the user selects.
+// Input component that takes a simple object and
+// shows values based on a selected key
+
 import React, {useState, useEffect, useMemo, useContext, useCallback} from 'react';
 import * as _ from 'lodash';
 
 import DropdownAutocomplete from 'etna-js/components/inputs/dropdown_autocomplete';
+import CheckboxesInput from './checkboxes';
+import MultiselectStringInput, {getAllOptions} from './multiselect_string';
 import {InputBackendComponent} from "./input_types";
 
-/*
-function getPath(options: OptionSet, leaf: string): string[] {
-  for (let [key, value] of Object.entries(options)) {
-    if (value && typeof value === 'object') {
-      // Look one step ahead for our leaf nodes
-      if (Object.keys(value).includes(leaf) && null == value[leaf])
-        return [key, leaf];
-      let path = getPath(value, leaf);
-      if (path.length > 0) return [key, ...path];
-    } else if (null == value && key == leaf) {
-      return [key];
-    }
+const handleClickOption = useCallback((option: any) => {
+  let copy = [...selectedOptions];
+  if (!selectedOptions.includes(option)) {
+    copy.push(option);
+  } else {
+    copy = selectedOptions.filter((opt) => option !== opt);
   }
+  setSelectedOptions(copy);
+}, [selectedOptions]);
 
-  return [];
-}
-
-function getOptions(desiredPath: string[], optionSet: OptionSet): string[] | null {
-  if (null == desiredPath || desiredPath.length === 0) return Object.keys(optionSet);
-  // _.at always returns an array, so unwrap it.
-  let results = _.at(optionSet, desiredPath.join('.'))[0];
-  if (results) return Object.keys(results);
-  return null;
-}
-
-function LeafOptions({options, depth, handleSelect}: {options: string[] | null, depth: number, handleSelect: (value: string | null, depth: number) => void}) {
-  if (!options) return null;
-
-  return (
-    <DropdownAutocomplete
-      key={`${depth}-${options.slice(0, 5).join('-')}`}
-      onSelect={(e: any) => {
-        handleSelect(e, depth);
-      }}
-      list={options}
-      defaultValue={null}
-    />
-  );
-}
-
-type OptionSet = {[k: string]: null | OptionSet};
-*/
 
 const SingleDropdownMulticheckbox: InputBackendComponent = ({input, onChange}) => {
-  /*const [path, setPath] = useState([] as string[]);
   const {data} = input;
 
   if (!input || !onChange) return null;
@@ -94,31 +63,26 @@ const SingleDropdownMulticheckbox: InputBackendComponent = ({input, onChange}) =
   return (
     <div>
       <div>
-        {path.length > 0
-          ? path.map((value, index) => {
-            const options = getOptions(path.slice(0, index), allOptions);
-              return (
-                <DropdownAutocomplete
-                  key={index}
-                  onSelect={(e: string | null) => {
-                    handleSelect(e, index);
-                  }}
-                  list={options}
-                  defaultValue={value}
-                />
-              );
-            })
-          : null}
+        <MultiselectStringInput
+          onAll={handleAllInputs}
+          onClear={handleClearInputs}
+          onChange={onChange}
+          input={input} />
       </div>
-      <div>
-        <LeafOptions
-          options={getOptions(path, allOptions)}
-          depth={path.length}
-          handleSelect={handleSelect}
-        />
+      <div className='checkbox-input-wrapper'>
+        {options.map((option, index) => {
+          return (
+            <CheckboxInput
+              option={option}
+              key={index}
+              checked={selectedOptions.includes(option)}
+              onChange={handleClickOption}
+            />
+          );
+        })}
       </div>
     </div>
-  );*/
+  );
 }
 
 export default SingleDropdownMulticheckbox;
