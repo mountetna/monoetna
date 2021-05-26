fb9593f643f1e304ec039d71863a8b6be2e3e0ff
GithubAction <mountetna@users.noreply.github.com>
2021-05-26 02:57:54 +0000

    Squashed 'etna/' changes from 3a408a5af..670fc855f
    670fc855f update etna.d.ts
    51cdcf197 add session open and save
    4d67f33b4 use flat button
    
    git-subtree-dir: etna
    git-subtree-split: 670fc855f5984c7446b83f7a407d21f9a18d0f3d

---
+++ packages/etna-js/components/flat-button.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Icon from './icon';
+require('./flat-button.css');
+
+const FlatButton = ({icon, className, disabled, title, label, onClick}) => (
+  <div title={title} className={ `flat-button ${className} ${disabled ? 'disabled' : ''}` } onClick={onClick}>
+    { label && <span className='flat-label'>{label}</span> }
+    { icon && <Icon disabled={disabled} className='flat-icon' icon={icon} /> }
+  </div>
+)
+
+export default FlatButton;
+
