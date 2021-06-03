8c4838214901e94654186a329987fd52d5c4ac64
Cole <4930129+coleshaw@users.noreply.github.com>
2021-06-03 15:25:26 -0400

    adding icon to indicate steps can be expanded or closed

--- vulcan/lib/client/jsx/components/workflow/steps/step_user_input_wrapper.tsx
+++ vulcan/lib/client/jsx/components/workflow/steps/step_user_input_wrapper.tsx
@@ -1,29 +1,40 @@
-import React, {useContext, useState, useEffect, useCallback, useMemo} from 'react';
+import React, {
+  useContext,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo
+} from 'react';
 
 import {VulcanContext} from '../../../contexts/vulcan_context';
 
 import StepName from './step_name';
 
 import {
-  allDataNonEmpty, allExpectedOutputSources,
+  allDataNonEmpty,
+  allExpectedOutputSources,
   dataOfSource,
-  statusOfStep, stepOfSource,
-} from "../../../selectors/workflow_selectors";
-import {patchInputs} from "../../../actions/vulcan";
-import {UIStep} from "../user_interactions/inputs/input_types";
-import StepUserInputDrawer from "./step_user_input_drawer";
-import StepUserInput from "./step_user_input";
-import {STATUS} from "../../../api_types";
+  statusOfStep,
+  stepOfSource
+} from '../../../selectors/workflow_selectors';
+import {patchInputs} from '../../../actions/vulcan';
+import {UIStep} from '../user_interactions/inputs/input_types';
+import StepUserInputDrawer from './step_user_input_drawer';
+import StepUserInput from './step_user_input';
+import {STATUS} from '../../../api_types';
 
-export default function StepUserInputWrapper({step}: { step: UIStep['step'] }) {
+export default function StepUserInputWrapper({step}: {step: UIStep['step']}) {
   const [open, setOpen] = useState(true);
   const {dispatch, state} = useContext(VulcanContext);
 
   const {status, workflow, data, session} = state;
 
-  const handleInputChange = useCallback((inputName: string, value: any) => {
-    dispatch(patchInputs({[inputName]: value}));
-  }, [dispatch]);
+  const handleInputChange = useCallback(
+    (inputName: string, value: any) => {
+      dispatch(patchInputs({[inputName]: value}));
+    },
+    [dispatch]
+  );
 
   const toggleInputs = useCallback(() => setOpen(!open), [setOpen, open]);
 
@@ -31,13 +42,28 @@ export default function StepUserInputWrapper({step}: { step: UIStep['step'] }) {
   // also closing it every time any incidental change to data or session happens that doesn't result in
   // an effective change to this condition (see the useEffect below).
   const allInnerSources = useMemo(() => allExpectedOutputSources(step), [step]);
-  const allInnerStepNames: string[] = useMemo(() => allInnerSources.map(stepOfSource).filter(v => v != null),
-      [allInnerSources]) as string[];
-  const allInnerStatus = useMemo(() => allInnerStepNames.map(stepName => statusOfStep(stepName, status)), [allInnerStepNames, status])
-  const allStepsComplete = useMemo(() => allInnerStatus.every(s => s && s.status === STATUS.COMPLETE), [allInnerStatus]);
-  const allDatas = useMemo(() => allInnerSources.map(source =>
-      dataOfSource(source, workflow, status, data, session)), [allInnerSources, workflow, status, data, session]);
-  const allDataIsNonEmpty = useMemo(() => allDataNonEmpty(allDatas), [allDatas]);
+  const allInnerStepNames: string[] = useMemo(
+    () => allInnerSources.map(stepOfSource).filter((v) => v != null),
+    [allInnerSources]
+  ) as string[];
+  const allInnerStatus = useMemo(
+    () => allInnerStepNames.map((stepName) => statusOfStep(stepName, status)),
+    [allInnerStepNames, status]
+  );
+  const allStepsComplete = useMemo(
+    () => allInnerStatus.every((s) => s && s.status === STATUS.COMPLETE),
+    [allInnerStatus]
+  );
+  const allDatas = useMemo(
+    () =>
+      allInnerSources.map((source) =>
+        dataOfSource(source, workflow, status, data, session)
+      ),
+    [allInnerSources, workflow, status, data, session]
+  );
+  const allDataIsNonEmpty = useMemo(() => allDataNonEmpty(allDatas), [
+    allDatas
+  ]);
 
   useEffect(() => {
     if (!allDataIsNonEmpty) {
@@ -63,30 +89,36 @@ export default function StepUserInputWrapper({step}: { step: UIStep['step'] }) {
   //   hide their contents (i.e. the drawer close effect)
   //   if edited and Run is clicked, but that seems more minor
   //   compared to resetting user input values.
-  const inner = 'isGroup' in step ?
+  const inner =
+    'isGroup' in step ? (
       <StepUserInputDrawer
-          key={`${step.name}`}
-          step={step}
-          handleInputChange={handleInputChange}
-      /> :
+        key={`${step.name}`}
+        step={step}
+        handleInputChange={handleInputChange}
+      />
+    ) : (
       <StepUserInput
-          key={`${step.name}`}
-          step={step}
-          handleInputChange={handleInputChange}
-      />;
+        key={`${step.name}`}
+        step={step}
+        handleInputChange={handleInputChange}
+      />
+    );
 
   return (
-      <div className='step-user-input step'>
-        <div onClick={toggleInputs}>
-          <StepName step={step}/>
-        </div>
-        <div
-            className={`step-user-input-inputs sliding-panel vertical ${
-                open ? 'open' : 'closed'
-            }`}
-        >
-          {inner}
-        </div>
+    <div className='step-user-input step'>
+      <div
+        onClick={toggleInputs}
+        className={`toggle ${open ? 'open' : 'closed'}`}
+      >
+        <StepName step={step} showToggle={true} open={open} />
       </div>
+      <div
+        className={`step-user-input-inputs sliding-panel vertical ${
+          open ? 'open' : 'closed'
+        }`}
+      >
+        {inner}
+      </div>
+    </div>
   );
 }
