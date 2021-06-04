392afaed4c04ad9604ad0a23bdf953d9ae083087
Cole <4930129+coleshaw@users.noreply.github.com>
2021-06-04 12:01:51 -0400

    WIP

--- vulcan/lib/client/jsx/components/workflow/user_interactions/outputs/plot_effects.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/outputs/plot_effects.tsx
@@ -1,3 +1,27 @@
-import {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useMemo} from 'react';
 
-export function useLegendHover(plotRef: RefObject<HTMLDivElement>) {}
+export function useLegendHover(plotRef: React.RefObject<HTMLDivElement>) {
+  const elements = useMemo(() => {
+    if (plotRef.current) {
+      return plotRef.current.getElementsByClassName('legendtoggle');
+    }
+
+    return [];
+  }, [plotRef]);
+
+  useEffect(() => {
+    for (var i = 0; i < elements.length; i++) {
+      elements[9].addEventListener('hover', onHoverLegendToggle);
+    }
+
+    return () => {
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].removeEventListener('hover', onHoverLegendToggle);
+      }
+    };
+  }, [elements]);
+
+  function onHoverLegendToggle() {
+    console.log('hovering over a toggle');
+  }
+}
