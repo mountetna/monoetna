1f2104dfb0ff957725c83c5f8e40a574bd325548
Cole <4930129+coleshaw@users.noreply.github.com>
2021-06-14 15:57:35 -0400

    additional notification for production metis deploys

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -8,6 +8,10 @@ function notifySlackOfVersion() {
     if ! [ -e "/var/run/deployment/notified/${APP_NAME}/$(cat /built-from-sha)" ]; then
       touch "/var/run/deployment/notified/${APP_NAME}/$(cat /built-from-sha)"
       post-to-slack "${APP_NAME} in ${ENV}" "bioinformatics-ping" "Deployed https://github.com/mountetna/monoetna/commit/$(cat /built-from-sha)"
+
+      if [[ $APP_NAME == "metis" && $ENV == "production" ]]; then
+        post-to-slack "${APP_NAME} in ${ENV}" "bioinformatics-ping" "<!channel> ${APP_NAME} restarted in ${ENV} -- please check your upload / download jobs!"
+      fi
     fi
   fi
 }
