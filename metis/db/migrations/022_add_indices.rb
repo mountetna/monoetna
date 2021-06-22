29dfc4396457a4429e1f9de27d917358cb839a77
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2021-06-22 15:09:40 -0700

    rm index project_name

--- metis/db/migrations/022_add_indices.rb
+++ metis/db/migrations/022_add_indices.rb
@@ -4,17 +4,11 @@ Sequel.migration do
         add_index :bucket_id
         add_index :folder_id
         add_index :data_block_id
-        add_index :project_name
       end
 
       alter_table(:folders) do
         add_index :bucket_id
         add_index :folder_id
-        add_index :project_name
-      end
-
-      alter_table(:buckets) do
-        add_index :project_name
       end
     end
   end
