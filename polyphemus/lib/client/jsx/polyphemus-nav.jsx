cb0180d53043782e4ceaa8be8e3708ddbabb2d2e
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2021-07-07 21:51:40 -0700

    roving eye

--- polyphemus/lib/client/jsx/polyphemus-nav.jsx
+++ polyphemus/lib/client/jsx/polyphemus-nav.jsx
@@ -1,9 +1,52 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import Nav from 'etna-js/components/Nav';
 import {selectUser} from 'etna-js/selectors/user-selector';
 import {useReduxState} from 'etna-js/hooks/useReduxState';
 
-const Logo = () => <div id='logo'/>;
+import { ReactSVG } from 'react-svg';
+
+const dist = (p1,p2) => Math.sqrt( (p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y) );
+
+const norm = (p) => {
+  let n = dist(p, {x:0,y:0});
+  return { x: p.x / n, y: p.y/n };
+}
+
+const moveEye = (e) => {
+  const iris = document.querySelector('#iris');
+
+  if (!iris) return;
+
+  const center = { x: 55, y: 55 };
+  const maxdist = dist(center, { x: window.innerWidth, y: window.innerHeight});
+  const maxradius = 20;
+
+  const point = { x: e.clientX, y: e.clientY };
+
+  const radius = maxradius / maxdist * dist(center, point);
+
+  const vector = norm({ x: point.x - center.x, y: point.y - center.y });
+
+  iris.setAttribute('transform',
+      `translate(${vector.x * radius},${vector.y*radius})`)
+}
+
+const Logo = () => {
+
+  useEffect( () => {
+    document.addEventListener('mousemove', moveEye );
+    return () => document.removeEventListener('mousemove', moveEye);
+  }, []);
+
+  return <div id='logo'>
+      <ReactSVG src="/images/polyphemus.svg" 
+        renumerateIRIElements={false}
+        beforeInjection={(svg) => {
+          svg.setAttribute('style', 'width: 50px; height: 50px')
+        }}
+      />
+    </div>
+}
 
 const NavBar = ({user}) => <div id='nav'></div>
 
