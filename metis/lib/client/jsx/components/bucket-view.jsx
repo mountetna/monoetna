b8c5263e101f7bf6f0b2f67f5021d45d400af607
Cole <4930129+coleshaw@users.noreply.github.com>
2021-08-09 13:40:27 -0400

    remove ingest widgets from metis UI

--- metis/lib/client/jsx/components/bucket-view.jsx
+++ metis/lib/client/jsx/components/bucket-view.jsx
@@ -1,11 +1,7 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useEffect} from 'react';
 
-import {selectUser} from 'etna-js/selectors/user-selector';
 import {useReduxState} from 'etna-js/hooks/useReduxState';
-import {isAdmin} from 'etna-js/utils/janus';
-import {useFeatureFlag} from 'etna-js/hooks/useFeatureFlag';
 import {useActionInvoker} from 'etna-js/hooks/useActionInvoker';
-import {showMessages} from 'etna-js/actions/message_actions';
 
 import ListBucket from './list/list-bucket';
 import ListHead from './list/list-head';
@@ -13,8 +9,6 @@ import FolderBreadcrumb from './folder-breadcrumb';
 import ControlBar from './control-bar';
 
 import {selectBuckets} from 'etna-js/selectors/directory-selector';
-import {listHosts} from '../api/polyphemus/ingest_api';
-import {IngestContext} from '../contexts/ingest_context';
 
 const COLUMNS = [
   {name: 'type', width: '60px'},
@@ -37,9 +31,6 @@ const COLUMN_WIDTHS = COLUMNS.reduce((widths, column) => {
 const BucketView = () => {
   const invoke = useActionInvoker();
   const buckets = useReduxState((state) => selectBuckets(state));
-  const user = useReduxState((state) => selectUser(state));
-  const canIngest = useFeatureFlag('ingest');
-  const {state, setHosts} = useContext(IngestContext);
 
   function createBucket(bucket) {
     invoke({type: 'CREATE_BUCKET', bucket});
@@ -66,25 +57,6 @@ const BucketView = () => {
   useEffect(() => {
     invoke({type: 'RETRIEVE_BUCKETS'});
   }, []);
-
-  useEffect(() => {
-    const isAdminUser = isAdmin(user, CONFIG.project_name);
-    if (
-      CONFIG.project_name &&
-      isAdminUser &&
-      canIngest &&
-      0 === Object.keys(state.hosts).length
-    ) {
-      listHosts()
-        .then(({hosts}) => {
-          setHosts(hosts);
-        })
-        .catch((e) => {
-          console.error(e);
-          invoke(showMessages(e.errors || [e.error] || [e]));
-        });
-    }
-  }, [CONFIG.project_name, user, canIngest, state.hosts, setHosts]);
 
   return (
     <div className='bucket-view-group'>
