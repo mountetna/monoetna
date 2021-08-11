c43216d255b65fc031519f1d75fb661b908f2c87
Cole <4930129+coleshaw@users.noreply.github.com>
2021-08-11 08:40:24 -0400

    switch migration columns to accomodate existing metis data

--- polyphemus/db/migrations/004_update_ingest_files.rb
+++ polyphemus/db/migrations/004_update_ingest_files.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
   change do
     alter_table(:ingest_files) do
-      rename_column :ingested_at, :triage_ingested_at
-      add_column :archive_ingested_at, DateTime
+      rename_column :ingested_at, :archive_ingested_at
+      add_column :triage_ingested_at, DateTime
     end
   end
 end
