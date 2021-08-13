5f12a6f472f2d3706d85ff0bd7f152e67fd83ca2
Cole <4930129+coleshaw@users.noreply.github.com>
2021-08-13 09:58:44 -0400

    mkdir for tests

--- polyphemus/lib/polyphemus.rb
+++ polyphemus/lib/polyphemus.rb
@@ -29,6 +29,7 @@ class Polyphemus
 
   def setup_ssh
     ssh_dir = ::File.expand_path(".ssh", "~")
+    FileUtils.mkdir_p(ssh_dir)
     ::File.open(::File.join(ssh_dir, "known_hosts"), "w") do |known_hosts|
       Polyphemus.instance.config(:ingest).each do |ingest_config_type, ingest_config|
         next unless ingest_config.is_a?(Array)
