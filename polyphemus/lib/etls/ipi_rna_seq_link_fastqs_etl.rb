9ee558e0ba65eced63548a771ee5db3f206a0494
Cole <4930129+coleshaw@users.noreply.github.com>
2021-08-16 10:12:20 -0400

    fix fastq linking etl and add spec

--- polyphemus/lib/etls/ipi_rna_seq_link_fastqs_etl.rb
+++ polyphemus/lib/etls/ipi_rna_seq_link_fastqs_etl.rb
@@ -3,9 +3,6 @@ require_relative "../ipi/ipi_helper"
 
 class Polyphemus::IpiRnaSeqLinkFastQsEtl < Polyphemus::MetisFileForMagmaModelEtl
   PATH_REGEX = /.*\/(?<record_name>.*)\/(?<original_file_name>.*\.fastq\.gz)$/
-  SAMPLE_NAME_REGEX = /^(?<sample_name>IPI.*\.[A-Z]+\d)\..*/
-  PATIENT_IPI_NUMBER_REGEX = /^(?<ipi_number>IPI.*)\.[A-Z]+\d\..*/
-  NON_CANCER_REGEX = /(NASH|NAFLD)/
   PROJECT = "ipi"
   BUCKET = "integral_data"
   MODEL = "rna_seq"
@@ -19,21 +16,18 @@ class Polyphemus::IpiRnaSeqLinkFastQsEtl < Polyphemus::MetisFileForMagmaModelEtl
     )
   end
 
-  def process(cursor, files)
-    files_by_record_name = files.group_by do |file|
-      record_name(file.file_path)
-    end
-
-    logger.info("Processing files: #{files.map { |f| f.file_path }.join(",")}")
+  def process(cursor, files_by_record_name)
+    logger.info("Processing files for: #{files_by_record_name.map { |f| f.record_name }.join(",")}")
 
     update_request = Etna::Clients::Magma::UpdateRequest.new(
       project_name: PROJECT,
     )
 
-    files_by_record_name.each do |record_name, files|
-      update_request.update_revision("rna_seq", record_name, {
-        "raw_fastq_files": files_payload(files),
+    files_by_record_name.each do |file_record|
+      update_request.update_revision("rna_seq", file_record.record_name, {
+        "raw_fastq_files": files_payload(file_record.files),
       })
+      logger.info("Found #{file_record.files.length} files for #{file_record.record_name}: #{file_record.files.map { |f| f.file_path }}")
     end
 
     magma_client.update_json(update_request)
