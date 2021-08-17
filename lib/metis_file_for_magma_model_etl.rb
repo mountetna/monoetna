0b46f3305bcefc2a3f73d2ac2b1d0c232e8310ce
GithubAction <mountetna@users.noreply.github.com>
2021-08-17 15:17:21 +0000

    Squashed 'polyphemus/' changes from b92d74bff..a63b62988
    a63b62988 do not re-flag removed files
    24d622751 tweak
    02be0920a small refactor
    3deb31621 update spec
    21583bd1e remove unused param
    36c7f4deb update comment
    4de4e7eec rename
    13faaf011 make fewer magma and metis requests
    089cea7fb revert change -- unnecessary
    7503f8d5a update fixture and spec
    b10132534 Revert "move renames to config"
    066560135 move renames to config
    8adc662a9 fix fastq linking etl and add spec
    6d7a38bea fix metis file for magma model etl to use hash based scanning
    f14d77351 remove unused code
    3d69be146 Merge branch 'cs/ipi-etl-rna-seq-renames' into cs/ipi-etl-bulk-rna-seq-raw-fastqs
    7531e0a24 wip
    5c81c4c27 Merge branch 'cs/ipi-etl-rna-seq-renames' into cs/ipi-etl-bulk-rna-seq-raw-fastqs
    e50c1ab42 Merge branch 'master' into cs/ipi-etl-bulk-rna-seq-raw-fastqs
    25d9b99d6 basic etl for finding metis files based on magma model, with spec
    cbc7838bf update completion file
    
    git-subtree-dir: polyphemus
    git-subtree-split: a63b629881256213a84e65e7df191ec27d058aec

---
+++ lib/metis_file_for_magma_model_etl.rb
@@ -0,0 +1,147 @@
+# Returns a set of Metis files, given a Magma model
+
+require_relative "./metis_file_etl"
+
+require_relative "etl"
+require_relative "hash_scan_based_etl_scanner"
+
+class Polyphemus
+  class MetisFileForMagmaModelEtlCursor < EtlCursor
+    def initialize(job_name:, project_name:, bucket_name:, model_name:)
+      raise "project_name cannot be nil" if project_name.nil?
+      raise "bucket_name cannot be nil" if bucket_name.nil?
+      raise "model_name cannot be nil" if model_name.nil?
+      super("#{job_name}_metis_files_for_magma_model_#{project_name}_#{bucket_name}_#{model_name}")
+      self[:project_name] = project_name
+      self[:bucket_name] = bucket_name
+      self[:model_name] = model_name
+    end
+
+    def reset!
+      super { self[:seen_ids] = [] }
+    end
+  end
+
+  class MetisFileForMagmaModelEtl < Etl
+    def initialize(
+      project_bucket_model_tuples:,
+      metis_client: nil,
+      magma_client: nil,
+      file_name_globs: [],
+      metis_path_to_record_name_regex: nil
+    )
+      file_cursors = project_bucket_model_tuples.map do |project_name, bucket_name, model_name|
+        MetisFileForMagmaModelEtlCursor.new(
+          job_name: self.class.name,
+          project_name: project_name,
+          bucket_name: bucket_name,
+          model_name: model_name,
+        ).load_from_db
+      end
+
+      @metis_client = metis_client
+      @magma_client = magma_client
+      @file_name_globs = file_name_globs
+      @path_regex = metis_path_to_record_name_regex
+
+      super(
+        cursor_group: EtlCursorGroup.new(file_cursors),
+        scanner: HashScanBasedEtlScanner.new.start_batch_state do |cursor|
+          metis_request = Etna::Clients::Metis::FindRequest.new(
+            project_name: cursor[:project_name],
+            bucket_name: cursor[:bucket_name],
+          )
+
+          magma_request = Etna::Clients::Magma::RetrievalRequest.new(
+            project_name: cursor[:project_name],
+            model_name: cursor[:model_name],
+            attribute_names: ["identifier"],
+            record_names: "all",
+            hide_templates: true,
+          )
+
+          record_names = fetch_magma_record_names(magma_request)
+
+          prepare_metis_find_request(metis_request)
+
+          # Pass both elements back instead of trying
+          #   to include the record names in the Metis
+          #   request directly, so that in
+          #   execute_batch_find we can better handle
+          #   cases when there are no Magma records.
+          [metis_request, record_names]
+        end.result_updated_at do |metis_files_record|
+          metis_files_record.updated_at
+        end.result_file_hashes do |metis_files_record|
+          metis_files_record.file_paths_hashes
+        end.result_id do |metis_files_record|
+          metis_files_record.record_name
+        end.execute_batch_find do |request_records_pair, _|
+          metis_request = request_records_pair.first
+          magma_record_names = request_records_pair.last
+
+          return [] unless magma_record_names.length > 0
+
+          metis_files = self.metis_client.find(metis_request).files.all
+          metis_files_by_record_name = metis_files.group_by do |file|
+            file.file_path.match(@path_regex)[:record_name]
+          end
+
+          magma_record_names.map do |magma_record_name|
+            next if metis_files_by_record_name[magma_record_name].nil?
+
+            MetisFilesForMagmaRecord.new(
+              magma_record_name,
+              metis_files_by_record_name[magma_record_name]
+            )
+          end.compact
+        end,
+      )
+    end
+
+    def prepare_metis_find_request(find_request)
+      # Do not track the cursor's updated_at, because
+      #   in order to find files that were deleted, we always need
+      #   to get all files on Metis that match the given globs.
+      begin
+        @file_name_globs.each do |glob|
+          find_request.add_param(Etna::Clients::Metis::FindParam.new(
+            type: "file",
+            attribute: "name",
+            predicate: "glob",
+            value: glob,
+          ))
+        end
+      end unless @file_name_globs.empty?
+    end
+
+    def fetch_magma_record_names(magma_request)
+      self.magma_client.retrieve(
+        magma_request
+      ).models.model(
+        magma_request.model_name
+      ).documents.document_keys
+    end
+  end
+
+  class MetisFilesForMagmaRecord
+    attr_reader :record_name, :files
+
+    def initialize(magma_record_name, metis_files)
+      @record_name = magma_record_name
+      @files = metis_files
+    end
+
+    def file_paths_hashes
+      files.map do |file|
+        [file.file_path, file.file_hash]
+      end
+    end
+
+    def updated_at
+      files.map do |file|
+        file.updated_at
+      end.minmax.last
+    end
+  end
+end
---
+++ spec/etls/ipi_rna_seq_link_fastqs_etl_spec.rb
@@ -0,0 +1,181 @@
+describe Polyphemus::IpiRnaSeqLinkFastQsEtl do
+  before(:each) do
+    stub_metis_setup
+    @all_updates = []
+  end
+
+  def file(file_name, file_path, file_hash = SecureRandom.hex, updated_at = Time.now)
+    Etna::Clients::Metis::File.new({
+      file_name: file_name,
+      file_path: file_path,
+      updated_at: updated_at,
+      file_hash: file_hash,
+    })
+  end
+
+  describe "updates Magma records" do
+    before(:each) do
+      stub_magma_update_json
+      stub_magma_models(fixture: "spec/fixtures/magma_ipi_models_with_records.json")
+    end
+
+    it "when scanner finds new files" do
+      stub_metis_scan("PATIENT001.T1.comp", [
+        file("PATIENT001.T1.comp.blahblah1.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz"),
+        file("PATIENT001.T1.comp.blahblah2.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz"),
+        file("PATIENT001.T1.comp.blahblah3.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz"),
+      ])
+
+      etl = Polyphemus::IpiRnaSeqLinkFastQsEtl.new
+
+      etl.run_once
+
+      # Make sure rna_seq records are updated
+      expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+                           .with(body: hash_including({
+                                   "revisions": {
+                                     "rna_seq": {
+                                       "PATIENT001.T1.comp": {
+                                         "raw_fastq_files": [{
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                         }],
+                                       },
+                                     },
+                                   },
+                                 }))
+    end
+
+    it "when file has been deleted" do
+      stub_metis_scan("PATIENT001.T1.comp", [
+        file("PATIENT001.T1.comp.blahblah1.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz"),
+        file("PATIENT001.T1.comp.blahblah2.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz"),
+        file("PATIENT001.T1.comp.blahblah3.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz"),
+      ])
+
+      etl = Polyphemus::IpiRnaSeqLinkFastQsEtl.new
+
+      etl.run_once
+
+      # Make sure rna_seq records are updated
+      expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+                           .with(body: hash_including({
+                                   "revisions": {
+                                     "rna_seq": {
+                                       "PATIENT001.T1.comp": {
+                                         "raw_fastq_files": [{
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                         }],
+                                       },
+                                     },
+                                   },
+                                 }))
+
+      stub_metis_scan("PATIENT001.T1.comp", [
+        file("PATIENT001.T1.comp.blahblah1.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz"),
+        file("PATIENT001.T1.comp.blahblah3.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz"),
+      ])
+
+      etl.run_once
+
+      # Make sure rna_seq records are updated
+      expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+                           .with(body: hash_including({
+                                   "revisions": {
+                                     "rna_seq": {
+                                       "PATIENT001.T1.comp": {
+                                         "raw_fastq_files": [{
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                         }],
+                                       },
+                                     },
+                                   },
+                                 }))
+    end
+
+    it "when file hash changes" do
+      stub_metis_scan("PATIENT001.T1.comp", [
+        file("PATIENT001.T1.comp.blahblah1.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz", "hash1"),
+        file("PATIENT001.T1.comp.blahblah2.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz", "hash2"),
+        file("PATIENT001.T1.comp.blahblah3.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz", "hash3"),
+      ])
+
+      etl = Polyphemus::IpiRnaSeqLinkFastQsEtl.new
+
+      etl.run_once
+
+      # Make sure rna_seq records are updated
+      expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+                           .with(body: hash_including({
+                                   "revisions": {
+                                     "rna_seq": {
+                                       "PATIENT001.T1.comp": {
+                                         "raw_fastq_files": [{
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                         }],
+                                       },
+                                     },
+                                   },
+                                 }))
+
+      stub_metis_scan("PATIENT001.T1.comp", [
+        file("PATIENT001.T1.comp.blahblah1.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz", "hash1"),
+        file("PATIENT001.T1.comp.blahblah2.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz", "new-hash2"),
+        file("PATIENT001.T1.comp.blahblah3.fastq.gz", "PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz", "hash3"),
+      ])
+
+      etl.run_once
+
+      # Make sure rna_seq records are again
+      expect(WebMock).to have_requested(:post, /#{MAGMA_HOST}\/update/)
+                           .with(body: hash_including({
+                                   "revisions": {
+                                     "rna_seq": {
+                                       "PATIENT001.T1.comp": {
+                                         "raw_fastq_files": [{
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah1.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah2.fastq.gz",
+                                         }, {
+                                           "path": "metis://ipi/integral_data/PATIENT001.T1.comp/PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                           "original_filename": "PATIENT001.T1.comp.blahblah3.fastq.gz",
+                                         }],
+                                       },
+                                     },
+                                   },
+                                 })).twice
+    end
+  end
+
+  def stub_metis_scan(record_name, change_list)
+    allow_any_instance_of(Polyphemus::HashScanBasedEtlScanner).to receive(:find_batch).and_return(
+      [Polyphemus::MetisFilesForMagmaRecord.new(record_name, change_list)]
+    )
+  end
+end
