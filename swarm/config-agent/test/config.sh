fca3b30053e6f2a82566b52b34a20d9e2ad67948
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-08-23 18:07:27 -0700

    WIP

--- swarm/config-agent/test/config.sh
+++ swarm/config-agent/test/config.sh
@@ -1,6 +1,8 @@
 world=World!
+template testerb.erb "$dest"/abc.conf
 
-applyTemplate testerb.erb "$dest"/abc.conf
-enableConfig
-# docker kill --signal="USR1" httpd
-cat /config/cur/abc.conf
+function onApply() {
+  enableConfig
+  # docker kill --signal="USR1" httpd
+  cat /config/cur/abc.conf
+}
