b0a522a69829cf77f02e7f7c4a84e1d7cfa06442
dtm2451 <daniel.bunis@ucsf.edu>
2021-08-30 11:59:36 -0700

    bar plot - add title editing

--- archimedes/archimedes/functions/plotting/bar_plot.py
+++ archimedes/archimedes/functions/plotting/bar_plot.py
@@ -5,7 +5,21 @@ from .utils import _default_to_if_make_and_logic
 from .colors import colors
 from ..list import unique, order
 
-def bar_plotly(data_frame, x_by, y_by, scale_by = 'counts'):
+def bar_plotly(
+    data_frame,
+    x_by,
+    y_by,
+    scale_by = 'counts',
+    xlab = "make",
+    y_lab = "make",
+    main = "make",
+    legend_title = "make"):
+
+    # Parse dependent defaults
+    xlab = _default_to_if_make_and_logic(xlab, x_by)
+    ylab = _default_to_if_make_and_logic(ylab, y_by + " " + scale_by)
+    plot_title = _default_to_if_make_and_logic(plot_title, ylab)
+    legend_title = _default_to_if_make_and_logic(legend_title, y_by + "\n" + scale_by)
 
     ### Generate a composition summary dataframe from the input df.
     summary_df = pd.DataFrame(data_frame[[x_by, y_by]].value_counts())
@@ -27,5 +41,15 @@ def bar_plotly(data_frame, x_by, y_by, scale_by = 'counts'):
         color="yvals",
         color_discrete_sequence = colors
     )
+
+    # Tweaks
+    fig.update_layout(
+        title_text=plot_title,
+        xaxis_title=xlab,
+        yaxis_title=ylab,
+        legend= {'itemsizing': 'constant'}
+    )
+    fig.update_coloraxes(colorbar_title_text=legend_title)
+
     return fig
 
