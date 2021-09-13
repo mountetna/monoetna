093d60deb40db2a10af8dd37e3da2bd98c7678e0
dtm2451 <daniel.bunis@ucsf.edu>
2021-09-13 16:38:23 -0700

    move y_plot from vulcan script to archimedes, and minor tweaks

---
+++ archimedes/archimedes/functions/plotting/y_plot.py
@@ -0,0 +1,53 @@
+import plotly.express as px
+
+from .utils import _default_to_if_make_and_logic
+from .colors import colors
+
+def y_plot(
+    data_frame,
+    x_by,
+    y_by,
+    color_by = "make",
+    plots = ["violin", "box"],
+    xlab="make",
+    ylab="make",
+    main="make",
+    legend_title ="make"):
+    
+    # Parse dependent defaults
+    color_by = _default_to_if_make_and_logic(color_by, x_by)
+    xlab = _default_to_if_make_and_logic(xlab, x_by)
+    ylab = _default_to_if_make_and_logic(ylab, y_by)
+    main = _default_to_if_make_and_logic(main, ylab)
+    legend_title = _default_to_if_make_and_logic(legend_title, y_by)
+    
+    # Convert from our variables names to px.violin/bar variables names. 
+    the_atributes = {
+        "data_frame": data_frame,
+        "x": x_by,
+        "y": y_by,
+        "color": color_by,
+        "color_discrete_sequence": colors
+        }
+    
+    # Make Plot
+    if "violin" in plots:
+        #use conditional here for checking if box is in plots
+        the_atributes["box"]= "box" in plots
+        fig = px.violin(
+            **the_atributes
+            )
+    elif "box" in plots:
+        fig = px.box(
+            **the_atributes
+            )
+    
+    # Tweaks
+    fig.update_layout(
+        title_text=main,
+        xaxis_title=xlab,
+        yaxis_title=ylab,
+        legend_title_text=legend_title,
+        legend= {'itemsizing': 'constant'}
+    )
+    return fig
