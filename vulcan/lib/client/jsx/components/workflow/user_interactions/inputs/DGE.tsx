0c22e61798d6ee2c442607f6a727d024d9ce2d8b
dtm2451 <daniel.bunis@ucsf.edu>
2021-10-04 17:17:01 -0700

    add to dge widget

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/DGE.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/DGE.tsx
@@ -13,7 +13,8 @@ import { Button, Slider } from '@material-ui/core';
 import { pick } from 'lodash';
 import StringInput from './string';
 import BooleanInput from './boolean';
-import { flattenStringOptions } from './monoids';
+import { flattenStringOptions, joinNesting } from './monoids';
+import { useMemoized } from '../../../../selectors/workflow_selectors';
 
 /*
 This input is closely tied to a python script for running scanpy-based differential expression.
@@ -23,14 +24,14 @@ Input:
 
 Output:
 - A dictionary containing:
-    - method = a string of "btwn-all-de-groups", "btwn-groups", "btwn-groups-multiple"
+    - method = a string of "btwn-all-de-groups", "btwn-sets", "btwn-sets-multiple-groups"
     - (optional) subset_meta = a string naming a metadata to subset by (when: subsetting needed)
     - (optional) subset_use = a string vector of values of the subset meta to keep (when: subsetting needed)
     - de_meta = a string naming a metadata to compare between
-    - de_group_1 = a string vector of values of the de_meta to either compare among (when: "btwn-all-de-groups") or use as group1 (when:"btwn-groups", "btwn-groups-multiple")
-    - (optional) de_group_2 = a string vector of values of the de_meta to use as group2 (when: "btwn-groups", "btwn-groups-multiple")
-    - (optional) groups_meta = a string naming a metadata to use as cell groupings (when: "btwn-groups-multiple")
-    - (optional) groups_use = a string vector indicating which values of the groupings meta to investigate within (when: "btwn-groups-multiple")
+    - de_group_1 = a string vector of values of the de_meta to either compare among (when: "btwn-all-de-groups") or use as group1 (when:"btwn-sets", "btwn-sets-multiple-groups")
+    - (optional) de_group_2 = a string vector of values of the de_meta to use as group2 (when: "btwn-sets", "btwn-sets-multiple-groups")
+    - (optional) groups_meta = a string naming a metadata to use as cell groupings (when: "btwn-sets-multiple-groups")
+    - (optional) groups_use = a string vector indicating which values of the groupings meta to investigate within (when: "btwn-sets-multiple-groups")
 
 Labels:
 - Between all labels of a particular metadata
@@ -42,28 +43,31 @@ export default function DiffExpSC({
   data, onChange, ...props
 }: WithInputParams<{}, DataEnvelope<any>, any>, plotType: string) {
   const value = useSetsDefault({method: null}, props.value, onChange);
-
-  const options: string[] = useMemo(() => {
-    if (data == null) return [];
-    return Object.keys(data)
-    // ToDo: only return keys where values are strings
-  }, [data]);
+  const allOptions = useMemoized(joinNesting, data);
+  const options = Object.keys(allOptions);
+  const [doSubset, setSubset] = useState(false);
   
+  /*
   const subOptions: DataEnvelope<string[]> = useMemo(() => {
     if (data == null) return [];
+    const vals = {...data}
     let opts = {...data}
-    Object.entries(opts).map( ([key,value]) => {
+    Object.entries(vals).map( ([key,value]) => {
       opts[key] = flattenStringOptions({'0': value})
     })
     return opts;
   }, [data])
+  */
+  const addSubset = (vals = value) => {
+    vals['subset_meta'] = null
+    vals['subset_use'] = null
+    onChange(some(vals));
+  }
   
-  const addSubset = ()
-  
-  const removeSubset = (vals = {...props.value} as DataEnvelope<any>) => {
+  const removeSubset = (vals = value) => {
     if (Object.keys(vals).includes('subset_meta')) delete vals['subset_meta']
     if (Object.keys(vals).includes('subset_use')) delete vals['subset_use']
-    onChange(vals);
+    onChange(some(vals));
   }
 
   const updateValue = (newValue: any, key: string, prevValues = {...value}) => {
@@ -81,30 +85,66 @@ export default function DiffExpSC({
     DE_group2
   group_by if there
   */
+ 
+  const SubsetComps = (vals = value, opts = options, allopts = allOptions) => {
+    console.log(Object.keys(vals));
+    let value_select = null;
+    if (Object.keys(vals).includes('subset_meta')) {
+      if (vals['subset_meta']!=null) {
+        value_select = MultiselectInput(
+          'subset_use', updateValue, vals['subset_use'],
+          'Labels to keep', allopts[vals['subset_meta']] as string[])
+      }
+      return(
+        <div>
+          {dropdownInput(
+            'subset_meta', updateValue, vals['subset_meta'],
+            "Subset by:", opts)}
+          {value_select}
+        </div>
+      )
+    } else {
+      return(<div></div>)
+    }
+  }
+  
+  function toggleSubsetting(vals = value) {
+    if (doSubset) removeSubset();
+    if (!doSubset) addSubset();
+    setSubset(!doSubset)
+  }
+  
+  console.log(options)
+  console.log(value)
   
   return (
     <div>
-      {Object.entries(shownValues).map(([key, val]) => {
-        return component_use(key, val, extra_inputs[key])
-      })}
-      <div>
-        <Button
+      Differential Expression Question:
+      <DropdownAutocomplete
+          list={Object.keys(method_labels)}
+          value={value['method']}
+          onSelect={(val: string) => updateValue(method_labels[val], 'method')}
+        />
+      Use all cells or just a subset?
+      <Button
           variant="contained"
           color="primary"
-          onClick={() => {toggleAdvanced()}}
+          onClick={() => {toggleSubsetting()}}
           >
-          {showHide} Advanced Options
+          Subsetting
         </Button>
-      </div>
+      {SubsetComps()}
+      {/* {DEComps} */}
+      {/* {GroupsComps} */}
     </div>
   );
 
 };
 
 const method_labels: DataEnvelope<string> = {
-  'btwn-all-de-groups': 'Between all labels of a particular metadata',
-  'btwn-groups': 'Between particular labels',
-  'btwn-groups-multiple': 'Between particular labels in multiple cell groups'
+  '1 btwn-all-de-groups: Between All labels of one metadata': 'btwn-all-de-groups',
+  '2 btwn-sets: Between particular labels': 'btwn-sets',
+  '3 btwn-sets-multiple-groups: Between particular labels, in multiple cell groups': 'btwn-sets-multiple-groups'
 }
 
 const output_sets: DataEnvelope<DataEnvelope<string|string[]|null>> = {
@@ -113,13 +153,13 @@ const output_sets: DataEnvelope<DataEnvelope<string|string[]|null>> = {
     'de_meta': null,
     'de_group_1': null
   },
-  'btwn-groups': {
+  'btwn-sets': {
     'method': 'btwn-all-de-groups',
     'de_meta': null,
     'de_group_1': null,
     'de_group_2': null
   },
-  'btwn-groups-multiple': {
+  'btwn-sets-multiple-groups': {
     'method': 'btwn-all-de-groups',
     'de_meta': null,
     'de_group_1': null,
@@ -143,21 +183,6 @@ const stringInput = (
       />
     )
   };
-
-// const nestableDropdownInput = (
-//   key: string = "filler", changeFxn: Function, value: string | null,
-//   label: string, options: DataEnvelope<null>) => {
-//    
-//     return(
-//       <NestedSelectAutocompleteInput
-//         key={key}
-//         label={label}
-//         data={{options}} 
-//         value={maybeOfNullable(value)}
-//         onChange={(val) => changeFxn(withDefault(val, null), key)}
-//       />
-//     )
-//   }
 
 const checkboxInput = (
   key: string = "filler", changeFxn: Function, value: boolean = false,
@@ -205,36 +230,3 @@ const MultiselectInput = (
       </div>
     )
   }
-
-const sliderInput = (
-  key: string = "filler", changeFxn: Function, value: number,
-  label: string, min: number = 0.1, max: number = 20) => {
-
-    return(
-        <div key={key}>
-          {label}
-          <Slider
-            value={value}
-            onChange={(event, newValue) => changeFxn(newValue as number, key)}
-            min={min}
-            max={max}
-            valueLabelDisplay="auto"
-          />
-        </div>
-    )
-  }
-
-const comps: DataEnvelope<Function> = {
-  'plot_title': stringInput,
-  'legend_title': stringInput,
-  'xlab': stringInput,
-  'ylab': stringInput,
-  'x_by': dropdownInput,
-  'y_by': dropdownInput,
-  'color_by': dropdownInput,
-  'plots': MultiselectInput,
-  'color_order': dropdownInput,
-  'order_when_continuous_color': checkboxInput,
-  'size': sliderInput,
-  'scale_by': dropdownInput
-}
