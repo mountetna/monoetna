f52bc1b62e2e3d3e3cc6c56d9f19131d773525e0
GithubAction <mountetna@users.noreply.github.com>
2021-10-22 22:16:57 +0000

    Squashed 'polyphemus/' changes from 5c53f16d5..89c4f46fa
    89c4f46fa show errors
    2a4d9403a fix jest
    f96bb54dd fix more ts
    0c66df92d debounce input
    a1a8b6d43 identifier fields
    61889aa86 fix ts
    aaa9457ec run js tests
    3df25970c fix string redcap_field
    89ff7d6f7 bugs
    788cfff05 fix exchange
    21d55ab9b show number, bugfixes
    0d3fe700f use pages
    d04bcd32d fix history
    42778f620 fix saving
    ec6ee7273 redcap form
    c5d6281e6 update magma api
    
    git-subtree-dir: polyphemus
    git-subtree-split: 89c4f46faec91b25f480f97c7c034a792111a3ac

--- lib/client/jsx/etl/configure-pane.tsx
+++ lib/client/jsx/etl/configure-pane.tsx
@@ -6,11 +6,16 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import HistoryIcon from '@material-ui/icons/HistoryRounded';
+import CodeIcon from '@material-ui/icons/Code';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 import ConfigScript from './config-script';
 import EtlPane, { EtlPaneHeader } from './etl-pane';
+import RedcapForm from './redcap-form';
 import RevisionHistory from './revision-history';
 import { formatTime } from './run-state';
 
@@ -19,62 +24,96 @@ const useStyles = makeStyles( theme => ({
     justifyContent: 'space-between',
     width: 'auto'
   },
-  revisions: {
+  buttons: {
     flex: '1 1 auto',
     width: 'auto',
     justifyContent: 'flex-end'
   }
 }));
 
-const ConfigurePane = ({name, project_name, selected, config, schema, update}:{name:string, project_name:string, selected:string|null, config:any, schema:any,update:Function}) => {
+const FORMS:{
+  [key:string]: any
+} = {
+  redcap: RedcapForm
+};
+
+const ConfigurePane = ({name, project_name, selected, config, job, update, error}:{
+  name:string,
+  project_name:string,
+  selected:string|null,
+  config:any,
+  job:Job|undefined,
+  update:Function,
+  error:string
+}) => {
   const classes = useStyles();
 
-  let [ origScript, setOrigScript ] = useState('');
-  let [ editedScript, setEditedScript ] = useState('');
-  let [ comment, setComment ] = useState('');
-  let [ showRevisions, setShowRevisions ] = useState(false);
+  const [ origScript, setOrigScript ] = useState('');
+  const [ editedScript, setEditedScript ] = useState('');
+  const [ editedConfig, setEditedConfig ] = useState({});
+  const [ comment, setComment ] = useState('');
+  const [ showRevisions, setShowRevisions ] = useState(false);
+  const [ showJson, setShowJson ] = useState(false);
+
+  const JobForm = job ? FORMS[job.name] : null;
 
   useEffect( () => {
-    origScript = JSON.stringify(config,null,2);
-    setOrigScript(origScript);
-    setEditedScript(origScript);
+    const script = JSON.stringify(config,null,2);
+    setOrigScript(script);
+    setEditedScript(script);
+    setEditedConfig(config);
   }, [config]);
 
+  const showSave = showJson ? (origScript != editedScript) : (editedConfig != config);
+
   return <EtlPane mode='configure' selected={selected}>
-    <EtlPaneHeader title='Configuration'>
+    <EtlPaneHeader title='Configuration' error={error}>
+      {
+        showSave && 
+          <Grid spacing={1} container className={classes.savebar}>
+            <Grid item><TextField style={{width:300}} value={comment} onChange={ e => setComment(e.target.value) } placeholder='Revision comment'/></Grid>
+            <Grid item><Button disabled={ comment == '' } onClick={ () => {
+              update({ comment, config: showJson ? JSON.parse(editedScript) : editedConfig });
+              setComment('');
+            } } >Save</Button></Grid >
+            <Grid item><Button onClick={() => showJson ? setEditedScript(origScript) : setEditedConfig(config)} color='secondary'>Reset</Button></Grid>
+          </Grid>
+      }
+      <Grid container spacing={1} item className={classes.buttons}>
         {
-          origScript != editedScript && 
-            <Grid spacing={1} container className={classes.savebar}>
-              <Grid item><TextField style={{width:300}} value={comment} onChange={ e => setComment(e.target.value) } placeholder='Revision comment'/></Grid>
-              <Grid item><Button disabled={ comment == '' } onClick={ () => {
-                update({ comment, config: JSON.parse(editedScript) });
-                setComment('');
-              } } >Save</Button></Grid>
-              <Grid item><Button onClick={() => setEditedScript(origScript)} color='secondary'>Reset</Button></Grid>
-            </Grid>
-        }
-        <Grid container spacing={1} item className={classes.revisions}>
-          <Tooltip title='revision history'>
-            <IconButton onClick={() => setShowRevisions(true) } size='small' aria-label='revision history'>
-              <HistoryIcon/>
+          JobForm && <Tooltip title={ showJson ? 'show form' : 'show json' }>
+            <IconButton disabled={showSave} onClick={() => setShowJson(!showJson) } size='small' aria-label='revision history'
+              color={ showJson ? 'primary' : 'default' }>
+              <CodeIcon/>
             </IconButton>
           </Tooltip>
-          <RevisionHistory
-            name={name}
-            project_name={project_name}
-            open={showRevisions}
-            config={config}
-            update={
-              config => {
-                setEditedScript(JSON.stringify(config,null,2));
-                setShowRevisions(false)
-              }
+        }
+        <Tooltip title='revision history'>
+          <IconButton onClick={() => setShowRevisions(true) } size='small' aria-label='revision history'>
+            <HistoryIcon/>
+          </IconButton>
+        </Tooltip>
+        <RevisionHistory
+          name={name}
+          project_name={project_name}
+          open={showRevisions}
+          config={config}
+          update={
+            newConfig => {
+              setEditedConfig(newConfig);
+              setEditedScript(JSON.stringify(newConfig,null,2));
+              setShowRevisions(false);
             }
-            onClose={() => setShowRevisions(false)}
-          />
-        </Grid>
+          }
+          onClose={() => setShowRevisions(false)}
+        />
+      </Grid>
     </EtlPaneHeader>
-    <ConfigScript script={editedScript} schema={schema} onChange={setEditedScript} />
+    {
+      (!JobForm || showJson)
+        ? <ConfigScript script={editedScript} schema={job ? job.schema : null} onChange={setEditedScript} />
+        : <JobForm project_name={project_name} config={editedConfig} job={job} update={ setEditedConfig }/>
+    }
   </EtlPane>
 }
 
