8bf01b65c257050fece106d54232fbbc500119f1
dtm2451 <daniel.bunis@ucsf.edu>
2021-10-29 15:55:09 -0700

    scDGEui - Fix reload bug caused by missed empty hash edge case

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/scDGE.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/scDGE.tsx
@@ -79,8 +79,6 @@ export default function DiffExpSC({
     onChange(some(prevValues));
   };
   
-  console.log(value)
-  
   return (
     <div>
       Step 1: Select your DE Question Type:
@@ -145,7 +143,7 @@ const SubsetComps = (vals: DataEnvelope<any>, opts: DataEnvelope<StringOptions>,
     
     let value_select = null;
     if (Object.keys(vals).includes('subset_meta')) {
-      if (vals['subset_meta']!=null) {
+      if (Object.keys(opts).length>0 && vals['subset_meta']!=null) {
         value_select = MultiselectInput(
           'subset_use', changeFxn, vals['subset_use'],
           'Labels to keep', opts[(vals['subset_meta'])] as string[])
@@ -176,7 +174,7 @@ const DEComps = (
       
       let value_select_1 = null;
       let value_select_2 = null;
-      if (vals['de_meta']!=null) {
+      if (Object.keys(opts).length>0 && vals['de_meta']!=null) {
         if (Object.keys(vals).includes('de_group_1')) {
           value_select_1 = MultiselectInput(
             'de_group_1', changeFxn, vals['de_group_1'],
@@ -216,7 +214,7 @@ const GroupComps = (
     if (Object.keys(vals).includes('group_meta')) {
       
       let value_select = null;
-      if (vals['group_meta']!=null) {
+      if (Object.keys(opts).length>0 && vals['group_meta']!=null) {
         if (Object.keys(vals).includes('group_use')) {
           value_select = MultiselectInput(
             'group_use', changeFxn, vals['group_use'],
