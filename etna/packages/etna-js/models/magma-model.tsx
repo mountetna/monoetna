2ffbf0339effd7a96183f595ee52b42c27fa00e8
Cole <4930129+coleshaw@users.noreply.github.com>
2021-11-17 10:28:44 -0500

    make the query table look like the search table

--- etna/packages/etna-js/models/magma-model.tsx
+++ etna/packages/etna-js/models/magma-model.tsx
@@ -1,4 +1,17 @@
 export type Model = {
   documents: {};
-  template: {};
+  template: {[key: string]: Attribute};
+};
+
+export type Attribute = {
+  attribute_name: string;
+  attribute_type: string;
+  name: string;
+  display_name: string;
+  hidden: boolean;
+  read_only: boolean;
+  validation: {} | null;
+  restricted: boolean;
+  model_name?: string;
+  link_model_name?: string;
 };
