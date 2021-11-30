5df415b7b8376a64b2e3a33a38e997f3b7b9aca3
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-11-30 09:30:38 -0800

    Better tracking of changesets in CI

--- docker/build_support/find-oldest-branch-root.sh
+++ docker/build_support/find-oldest-branch-root.sh
@@ -2,5 +2,20 @@
 
 set -e
 
-git log --pretty='format:%h %an' | grep GithubAction | head -n 1 | cut -d' ' -f1
+if [[ -z "$IS_CI" ]]; then
+  echo "found branch root at HEAD~1" >&2
+  echo HEAD~1
+  exit 0
+fi
+
+if ! [[ -z "$TARGET_ROOT_BRANCH "]]; then
+  echo "found branch root at $TARGET_ROOT_BRANCH" >&2
+  echo $TARGET_ROOT_BRANCH
+  exit 0
+fi
+
+result=$(git log --pretty='format:%h %an' | grep GithubAction | head -n 2 | cut -d' ' -f1)
+echo "found branch root at $result" >&2
+echo "$result"
+
 
