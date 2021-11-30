07f222225aa1b118ca7205a9b3796aabe541f554
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-11-30 09:45:03 -0800

    Better branches and master handling

--- docker/build_support/find-oldest-branch-root.sh
+++ docker/build_support/find-oldest-branch-root.sh
@@ -2,20 +2,11 @@
 
 set -e
 
-if [[ -z "$IS_CI" ]]; then
-  echo "found branch root at HEAD~1" >&2
-  echo HEAD~1
-  exit 0
-fi
-
 if ! [[ -z "$TARGET_ROOT_BRANCH "]]; then
   echo "found branch root at $TARGET_ROOT_BRANCH" >&2
   echo $TARGET_ROOT_BRANCH
   exit 0
 fi
 
-result=$(git log --pretty='format:%h %an' | grep GithubAction | head -n 2 | cut -d' ' -f1)
-echo "found branch root at $result" >&2
-echo "$result"
-
-
+echo "found branch root at HEAD~1" >&2
+echo HEAD~1
