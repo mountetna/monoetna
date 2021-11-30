38e06ba1acf05dcc2ee550c42d4c6933ff1cdd2b
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-11-30 09:55:32 -0800

    Require explicit origin/?

--- docker/build_support/find-oldest-branch-root.sh
+++ docker/build_support/find-oldest-branch-root.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! [[ -z "$TARGET_ROOT_BRANCH "]]; then
+if ! test -z "$TARGET_ROOT_BRANCH"; then
   echo "found branch root at $TARGET_ROOT_BRANCH" >&2
   echo $TARGET_ROOT_BRANCH
   exit 0
