3e4825fe9b61dd3f30b34af61365d76fa6d72411
dtm2451 <daniel.bunis@ucsf.edu>
2021-12-01 15:58:24 -0800

    scRNAseq - all batch correct by record ID

--- vulcan/lib/server/workflows/scripts/determine_batch_by_options.py
+++ vulcan/lib/server/workflows/scripts/determine_batch_by_options.py
@@ -3,7 +3,7 @@ from archimedes.functions.dataflow import output_json, input_json, output_var
 pdat = input_json("project_data")
 
 no_batch='NO BATCH CORRECTION'
-opts = [no_batch] + list(pdat['color_options'].keys())
+opts = [no_batch, 'Record_ID'] + list(pdat['color_options'].keys())
 
 output_json(opts, 'batch_options')
 output_var(no_batch, 'no_batch_string')
