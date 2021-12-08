a549b2a5ed2c9d9d6bc1fcd6a3cc01292cf7e78a
Cole <4930129+coleshaw@users.noreply.github.com>
2021-12-08 14:20:07 -0500

    refactor to shared base class

--- polyphemus/lib/etls/mvir1/linkers/mvir1_rna_seq_files_linker_base.rb
+++ polyphemus/lib/etls/mvir1/linkers/mvir1_rna_seq_files_linker_base.rb
@@ -1,20 +1,4 @@
-require_relative "../../metis_files_linker_base"
+require_relative "../../shared/bulk_rna_seq/linkers/rna_seq_files_linker_base"
 
-class Polyphemus::MvirRnaSeqFilesLinkerBase < Polyphemus::MetisFilesLinkerBase
-  def initialize(project_name: "mvir1", bucket_name:, attribute_regex:, record_name_regex:)
-    super(project_name: project_name, bucket_name: bucket_name, record_name_regex: record_name_regex)
-    @attribute_regex = attribute_regex
-  end
-
-  def link(model_name:, files:, cursor: nil)
-    super(
-      model_name: model_name,
-      files_by_record_name: organize_metis_files_by_magma_record(
-        metis_files: files,
-        magma_record_names: current_magma_record_names(project_name, model_name),
-      ),
-      attribute_regex: @attribute_regex,
-      cursor: cursor,
-    )
-  end
+class Polyphemus::MvirRnaSeqFilesLinkerBase < Polyphemus::RnaSeqFilesLinkerBase
 end
