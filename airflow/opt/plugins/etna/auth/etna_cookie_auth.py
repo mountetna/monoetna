3b1021c14f34c8eedff1ff8865aa99011206e59e
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-12-13 13:26:41 -0800

    WIP

--- airflow/opt/plugins/etna/auth/etna_cookie_auth.py
+++ airflow/opt/plugins/etna/auth/etna_cookie_auth.py
@@ -1,1 +1,61 @@
-import airflow.auth
+import os
+
+import jwt
+from flask_login import login_user, logout_user
+from airflow.www.security import AirflowSecurityManager
+from flask import flash, g, redirect, request
+from flask_appbuilder import expose
+from flask_appbuilder.security.views import AuthView
+from werkzeug.wrappers import Response as WerkzeugResponse
+
+
+class EtnaSecurityManager(AirflowSecurityManager):
+  def auth_user_remote_user(self, username):
+    pass
+
+  pass
+
+
+def deserialize_etna_user(payload: str, algo: str, key: str):
+  try:
+    user_json = jwt.decode(
+      payload,
+      key=key,
+      verify=True,
+      algorithms=[algo],
+    )
+  except jwt.DecodeError:
+    pass
+
+
+class EtnaCookieAuthView(AuthView):
+  login_template = ""
+
+  @expose("/login/")
+  def login(self) -> WerkzeugResponse:
+    cookie_name = self.appbuilder.app.config['ETNA_AUTH_COOKIE_NAME']
+    cookie = request.cookies.get(cookie_name)
+    if cookie:
+      self._try_auth(cookie)
+
+    return redirect(self.appbuilder.get_url_for_index)
+
+  def _try_auth(self, cookie: str):
+    token_algo = self.appbuilder.app.config['ETNA_AUTH_TOKEN_ALGO']
+    public_key = self.appbuilder.app.config['ETNA_AUTH_PUBLIC_KEY']
+    return deserialize_etna_user(cookie, token_algo, public_key)
+
+
+
+    # username = request.environ.get("REMOTE_USER")
+    # if g.user is not None and g.user.is_authenticated:
+    #   return redirect(self.appbuilder.get_url_for_index)
+    # if username:
+    #   user = self.appbuilder.sm.auth_user_remote_user(username)
+    #   if user is None:
+    #     flash("invalid login", "warning")
+    #   else:
+    #     login_user(user)
+    # else:
+    #   flash("invalid login", "warning")
+    # return redirect(self.appbuilder.get_url_for_index)
---
+++ airflow/opt/plugins/etna/auth/etna_user.py
@@ -0,0 +1,106 @@
+import dataclasses
+from enum import Enum
+from types import NoneType
+from typing import Dict, Union, List, Literal, Tuple, Callable
+
+import re
+
+from etna.utils.parser import parse_many_joined_by, Parser, literal_parser, take_left, eof_parser, either_parser, \
+  regex_parser, parser_apply, word_parser, parser_lift, ParseFailure
+
+ROLE_MAP = dict(
+  a='admin',
+  v='viewer',
+  e='editor',
+)
+
+@dataclasses.dataclass
+class Permission:
+  project_name: str
+  see_restricted: bool
+  role: str
+
+@dataclasses.dataclass
+class EtnaUser:
+  name: str
+  email: str
+  flags: List[str]
+  perm: List[Permission]
+
+  @staticmethod
+  def from_dict(payload: Dict[str, str]) -> "EtnaUser":
+    name = payload['name']
+    email = payload['email']
+    flags = payload.get('flags', '').split(';')
+    perm = parse_permissions(payload['perm'])
+
+    return EtnaUser(
+      name=name,
+      email=email,
+      flags=flags,
+      perm=perm,
+    )
+
+  @property
+  def projects(self) -> List[str]:
+    return [p.project_name for p in self.perm]
+
+  def project_permission(self, project_name: str) -> Union[Permission, NoneType]:
+    for p in self.perm:
+      if p.project_name == project_name:
+        return p
+
+  def has_any_role(self, project_name: str, *roles: str) -> bool:
+    permission = self.project_permission(project_name)
+    if permission is None:
+      return False
+
+    return permission.role in roles
+
+  @property
+  def is_superuser(self) -> bool:
+    return self.has_any_role('administration', 'admin')
+
+  @property
+  def is_supereditor(self) -> bool:
+    return self.has_any_role('administration', 'admin', 'editor')
+
+  @property
+  def is_superviewer(self) -> bool:
+    return self.has_any_role('administration', 'admin', 'editor', 'viewer')
+
+  def can_edit(self, project_name) -> bool:
+    return self.is_supereditor or self.has_any_role(project_name, 'admin', 'editor')
+
+  def can_view(self, project_name) -> bool:
+    return self.is_superviewer or self.has_any_role(project_name, 'admin', 'editor', 'viewer')
+
+  def is_admin(self, project_name) -> bool:
+    return self.is_superuser or self.has_any_role(project_name, 'admin')
+
+
+def construct_permission(role: str) -> Callable[[List[str]], List[Permission]]:
+  def construct_permissions(project_names: List[str]) -> List[Permission]:
+    return [
+      Permission(role=ROLE_MAP[role], see_restricted=role==role.upper(), project_name=project_name)
+      for project_name in project_names
+    ]
+  return construct_permissions
+
+role_parser: Parser[str] = take_left(regex_parser(re.compile(r"[aAeEvV]")), literal_parser(':'))
+permission_parser: Parser[List[Permission]] = parser_apply(
+  parser_apply(parser_lift(construct_permission), role_parser),
+  parse_many_joined_by(word_parser, literal_parser(','))
+)
+
+permissions_parser: Parser[List[List[Permission]]] = take_left(
+  parse_many_joined_by(permission_parser, literal_parser(';')),
+  eof_parser)
+
+def parse_permissions(perm: str) -> List[Permission]:
+  result, error = permissions_parser.run_parser(perm)
+
+  if result:
+    return [p for ps in result for p in ps]
+
+  raise error
