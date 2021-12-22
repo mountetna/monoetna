eba47f6dec8d0f9c803321a32525197d78d1a2a4
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-12-21 17:30:25 -0800

    HOKAY

--- airflow/opt/plugins/etna/__init__.py
+++ airflow/opt/plugins/etna/__init__.py
@@ -5,8 +5,34 @@ import tarfile
 from os.path import basename
 from typing import Optional, List, Callable, Any
 
+from etna.operators.docker_operator import DockerOperator
 from etna.operators.docker_operator_base import SwarmSharedData
 from etna.operators.swarm_operator import DockerSwarmOperator
+
+def run_in_container(
+        task_id: str,
+        source_container: str,
+        command: List[str],
+        input_bytes: Optional[bytes] = None,
+        use_compose: bool = True,
+        input_file: Optional[str] = None,
+        input_dir: Optional[str] = None,
+        output_json: bool = False,
+        output_b64: bool = False,
+):
+    # Uses the containers created by the local development docker-compose system.
+    if use_compose:
+        source_container = f"monoetna_{source_container}_1"
+    serialize_last_output, swarm_shared_data = _prepare_input_outputs(input_bytes, input_dir, input_file, output_b64,
+                                                                      output_json)
+
+    return DockerOperator(
+        task_id=task_id,
+        source_container_name=source_container,
+        command=command,
+        swarm_shared_data=swarm_shared_data,
+        serialize_last_output=serialize_last_output,
+    )
 
 
 def run_in_swarm(
@@ -20,9 +46,22 @@ def run_in_swarm(
     output_json: bool = False,
     output_b64: bool = False,
 ):
+    serialize_last_output, swarm_shared_data = _prepare_input_outputs(input_bytes, input_dir, input_file, output_b64,
+                                                                     output_json)
+
+    return DockerSwarmOperator(
+        task_id=task_id,
+        source_service=source_service,
+        command=command,
+        include_external_networks=include_external_network,
+        swarm_shared_data=swarm_shared_data,
+        serialize_last_output=serialize_last_output,
+    )
+
+
+def _prepare_input_outputs(input_bytes, input_dir, input_file, output_b64, output_json):
     swarm_shared_data: List[SwarmSharedData] = []
     serialize_last_output: Optional[Callable[[bytes], Any]] = None
-
     if input_bytes:
         swarm_shared_data.append(
             SwarmSharedData(
@@ -49,17 +88,8 @@ def run_in_swarm(
                 remote_path="/input/" + basename(input_dir) + ".tar",
             )
         )
-
     if output_json:
         serialize_last_output = json.loads
     elif output_b64:
         serialize_last_output = base64.b64decode
-
-    return DockerSwarmOperator(
-        task_id=task_id,
-        source_service=source_service,
-        command=command,
-        include_external_networks=include_external_network,
-        swarm_shared_data=swarm_shared_data,
-        serialize_last_output=serialize_last_output,
-    )
+    return serialize_last_output, swarm_shared_data
