c7de44697e18e922518ae0c044914ca32299c456
GithubAction <mountetna@users.noreply.github.com>
2021-12-22 16:33:36 +0000

    Squashed 'timur/' changes from 522e83138..5c2ad5094
    5c2ad5094 correctly handle image type in view
    cc3de3dec timur image viewer
    9b13fcadb remove snapshot from spec
    05ce5d7fd update specs
    1cc1e8f40 with simple zoom functionality ... needs better styling
    4cea67e73 update specs
    cc155bd7d WIP
    34705bea9 Merge branch 'master' into cs/metis-cache-image-thumbnails
    279765ab2 Merge branch 'cs/thumbnails' into cs/metis-cache-image-thumbnails
    2e4bd73e2 update image attribute to use thumbnail URL param
    
    git-subtree-dir: timur
    git-subtree-split: 5c2ad50949913a86c28565b615b04cc0f22edea1

--- lib/client/jsx/components/attributes/image_attribute.jsx
+++ lib/client/jsx/components/attributes/image_attribute.jsx
@@ -1,19 +1,64 @@
 // Framework libraries.
-import * as React from 'react';
+import React, {useState} from 'react';
 import * as ReactRedux from 'react-redux';
 
+import IconButton from '@material-ui/core/IconButton';
+import ZoomInIcon from '@material-ui/icons/ZoomIn';
+import ZoomOutIcon from '@material-ui/icons/ZoomOut';
+import Tooltip from '@material-ui/core/Tooltip';
+import {makeStyles} from '@material-ui/core/styles';
+
 import FileAttribute from './file_attribute';
+import {STUB} from 'etna-js/actions/file_actions';
+
+const useStyles = makeStyles((theme) => ({
+  zoomBtn: {
+    height: '48px'
+  }
+}));
 
 const ImageAttribute = (props) => {
-  // When we get thumbnails working, we can put this back into
-  //   expose those.
-  // if (mode != "edit") {
-  //   return <div className='attribute'> {
-  //     value
-  //       ? <a href={ value.url } ><img src={ value.thumb }/></a>
-  //       : <div className="document_empty">No file.</div>
-  //   } </div>
-  // }
+  const [zoomed, setZoomed] = useState(false);
+  const classes = useStyles();
+  let {mode, value} = props;
+
+  if (
+    mode != 'edit' &&
+    value &&
+    value !== STUB &&
+    value.path !== STUB &&
+    value.url
+  ) {
+    const zoomVerb = zoomed ? 'out' : 'in';
+    return (
+      <div className='attribute file image'>
+        <Tooltip
+          className={classes.zoomBtn}
+          title={`Zoom ${zoomVerb}`}
+          aria-label={`zoom ${zoomVerb}`}
+        >
+          <IconButton
+            aria-label={`zoom ${zoomVerb}`}
+            onClick={() => {
+              setZoomed(!zoomed);
+            }}
+          >
+            {zoomed ? (
+              <ZoomOutIcon color='action' />
+            ) : (
+              <ZoomInIcon color='action' />
+            )}
+          </IconButton>
+        </Tooltip>
+        <a href={value.url}>
+          <img
+            className={`image-thumbnail ${zoomed ? 'medium' : 'small'}`}
+            src={`${value.url}&thumbnail=true`}
+          />
+        </a>
+      </div>
+    );
+  }
 
   return <FileAttribute {...props} />;
 };
