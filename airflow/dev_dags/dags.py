eb8e75be59d857f6394ad5398253f06d8a194f43
Zachary Collins <recursive.cookie.jar@gmail.com>
2021-12-22 18:24:41 -0800

    WIP

--- airflow/dev_dags/dags.py
+++
@@ -1,8 +0,0 @@
-from airflow.decorators import dag
-
-from etna import run_in_swarm
-
-
-@dag()
-def weird_dag():
-    run_in_swarm('test_swarm_operator', 'test-service-swarm', ['bach', '-c', "echo 1"], output_json=True)
--- airflow/opt/plugins/etna/__init__.py
+++ airflow/opt/plugins/etna/__init__.py
@@ -2,12 +2,100 @@ import base64
 import io
 import json
 import tarfile
+from datetime import datetime, timedelta
 from os.path import basename
-from typing import Optional, List, Callable, Any
+from typing import Callable
+from typing import Optional, List, Any
+
+from airflow import DAG
+from airflow.exceptions import AirflowNotFoundException
+from airflow.hooks.base import BaseHook
+from airflow.models.dag import DagStateChangeCallback, ScheduleIntervalArgNotSet, ScheduleIntervalArg
+from airflow.models.taskinstance import Context, TaskInstance
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 
 from etna.operators.docker_operator import DockerOperator
 from etna.operators.docker_operator_base import SwarmSharedData
 from etna.operators.swarm_operator import DockerSwarmOperator
+
+system_epoch = datetime(2021, 12, 22, 16, 56, 3, 185905)
+
+def _find_first_valid_connection(*options: str) -> str:
+    last_e = None
+    for option in options:
+        try:
+            BaseHook.get_connection(option)
+            return option
+        except AirflowNotFoundException as e:
+            last_e = e
+    if last_e:
+        raise last_e
+
+    raise AirflowNotFoundException("No connection option present!")
+
+
+def _notify_slack_dag_callback(dag_status: str) -> DagStateChangeCallback:
+    def state_change_callback(context: Context):
+        task_instance: TaskInstance = context['task_instance']
+        dag = context['dag']
+        conn_id = _find_first_valid_connection(
+            f"slack_notifications_{task_instance.dag_id}",
+            "slack_notifications"
+        )
+
+        op = SlackAPIPostOperator(
+            task_id=task_instance.task_id,
+            dag=dag,
+            slack_conn_id=conn_id,
+            text=f"Dag {task_instance.dag_id} {dag_status}!\n"
+            f"Logs: {task_instance.log_url}"
+        )
+
+        op.execute(context=context)
+
+    return state_change_callback
+
+def system_dag(
+        interval: timedelta
+):
+    def instantiate_dag(fn):
+        return dag(
+            start_date=system_epoch,
+            schedule_interval=interval,
+            default_args=dict(
+                # on_failure_callback=_notify_slack_dag_callback("failed"),
+            ),
+            catchup=False,
+        )(fn)
+    return instantiate_dag
+
+def dag(
+    on_failure_callback: Optional[DagStateChangeCallback] = None,
+    on_success_callback: Optional[DagStateChangeCallback] = None,
+    schedule_interval: ScheduleIntervalArg = ScheduleIntervalArgNotSet,
+    start_date: Optional[datetime] = None,
+    end_date: Optional[datetime] = None,
+    **kwds,
+):
+    """
+    Creates a dag by running the wrapped function in a dag context whose dag_id will be the function's name,
+    and whose description will be its doc string.
+    :return: Dah'ya like dags?
+    """
+    def instantiate_dag(fn):
+        with DAG(
+                dag_id=fn.__name__,
+            description=fn.__doc__,
+            on_failure_callback=on_failure_callback,
+            on_success_callback=on_success_callback,
+            start_date=start_date,
+            end_date=end_date,
+            schedule_interval=schedule_interval,
+            **kwds
+        ) as dag:
+            fn()
+        return dag
+    return instantiate_dag
 
 def run_in_container(
         task_id: str,
