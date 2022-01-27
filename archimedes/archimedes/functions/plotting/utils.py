1c049e9f55eb4243d640f039af8c0766d3a90085
dtm2451 <daniel.bunis@ucsf.edu>
2022-01-26 20:34:55 -0800

    dittoViz-py in Archimedes - fill in '_col' more + type checking

--- archimedes/archimedes/functions/plotting/utils.py
+++ archimedes/archimedes/functions/plotting/utils.py
@@ -1,3 +1,20 @@
+import numpy as np
+
+DISCRETE_KINDS = 'ObUS'
+CONTINUOUS_KINDS = 'ifuc'
+
+def is_discrete(nda: np.ndarray):
+    return nda.dtype.kind in DISCRETE_KINDS
+
+def is_continuous(nda: np.ndarray):
+    return nda.dtype.kind in CONTINUOUS_KINDS
+
+def scale(y):
+    x = y.copy()
+    x -= x.mean()
+    x /= x.std()
+    return x
+
 def _leave_default_or_null(target, default, none_if = False, default_when = "make"):
     if target == default_when:
         if none_if:
@@ -42,62 +59,45 @@ def _col(col, data_frame, adjustment = None, adj_fxn = None):
     '''
     Returns the values of a column, where values are named by the rownames of data_frame
 
-'col' String, the name of the column to grab.
-'data_frame' A data.frame.
-'adjustment' Ignored if the target metadata is not numeric, a recognized string indicating whether numeric data should be used directly (default) versus adjusted to be
-    "z-score": scaled with the scale() function to produce a relative-to-mean z-score representation
-    "relative.to.max": divided by the maximum expression value to give percent of max values between [0,1]
-'adj.fxn' A lambda function which takes an array of values and returns an array of the same length.
-    For example, `lambda x: log2(x)`.
+    'col' String, the name of the column to grab.
+    'data_frame' A data.frame.
+    'adjustment' Ignored if the target metadata is not numeric, a recognized string indicating whether numeric data should be used directly (default) versus adjusted to be
+        "z-score": scaled with the scale() function to produce a relative-to-mean z-score representation
+        "relative.to.max": divided by the maximum expression value to give percent of max values between [0,1]
+    'adj.fxn' A lambda function which takes an array of values and returns an array of the same length.
+        For example, `lambda x: log2(x)`.
 
-Value: A list.
-@details
-Retrieves the values of a metadata slot from \code{object}, or the clustering slot if \code{meta = "ident"} and the \code{object} is a Seurat.
+    Value: A list.
+    @details
+    Retrieves the values of a metadata slot from \code{object}, or the clustering slot if \code{meta = "ident"} and the \code{object} is a Seurat.
 
-If \code{adjustment} or \code{adj.fxn} are provided, then these requested adjustments are applied to these values (\code{adjustment} first).
-Note: Alterations via \code{adjustment} are only applied when metadata is numeric, but \code{adj.fxn} alterations are applied to metadata of any type.
+    If \code{adjustment} or \code{adj.fxn} are provided, then these requested adjustments are applied to these values (\code{adjustment} first).
+    Note: Alterations via \code{adjustment} are only applied when metadata is numeric, but \code{adj.fxn} alterations are applied to metadata of any type.
 
-Lastly, outputs these values are named as the cells'/samples' names.
-@seealso
-\code{\link{metaLevels}} for returning just the unique discrete identities that exist within a metadata slot
+    Lastly, outputs these values are named as the cells'/samples' names.
+    
+    examples:
+    example(importDittoBulk, echo = FALSE)
+    ._col("groups", object = myRNA)
 
-\code{\link{getMetas}} for returning all metadata slots of an \code{object}
-
-\code{\link{.isCol}} for testing whether something is the name of a metadata slot
-@examples
-example(importDittoBulk, echo = FALSE)
-._col("groups", object = myRNA)
-
-myRNA$numbers <- seq_len(ncol(myRNA))
-._col("numbers", myRNA, adjustment = "z-score")
-._col("numbers", myRNA, adj.fxn = as.factor)
-._col("numbers", myRNA, adj.fxn = function(x) \{log2(x)\})
+    myRNA$numbers <- seq_len(ncol(myRNA))
+    ._col("numbers", myRNA, adjustment = "z-score")
+    ._col("numbers", myRNA, adj.fxn = as.factor)
+    ._col("numbers", myRNA, adj.fxn = function(x) \{log2(x)\})
     '''
-
     if not _isCol(col, data_frame):
         raise Exception("" + col + " is not a column of 'data_frame'.")
-
-    # Retrieve target columns's values
-    values = data_frame[col]
-
-    # Add adjustments, if dtype is numeric.  Need to learn how to do that check with NumPy.
-    # Leaving as R code for now.
-    # if (is.numeric(values)) {
-    #     if (!is.null(adjustment) && !is.na(adjustment)) {
-    #         if (adjustment=="z-score") {
-    #             values <- as.numeric(scale(values))
-    #         }
-    #         if (adjustment=="relative.to.max") {
-    #             values <- values/max(values)
-    #         }
-    #     }
-    # }
-
+    # Retrieve target columns's values and make into numpy array
+    values = np.array(data_frame[col])
+    # Add 'adjustment's, if dtype is numeric.
+    if (adjustment != None and is_continuous(values)):
+        if (adjustment=="z-score"):
+             values = scale(values)
+        if (adjustment=="relative.to.max"):
+             values = values/max(values)
     if adj_fxn!=None:
         values = adj_fxn(values)
-
     # Add names via NumPy??
     # Leaving as R code for now.
     # names(values) <- .all_rows(data_frame)
-
     return values
