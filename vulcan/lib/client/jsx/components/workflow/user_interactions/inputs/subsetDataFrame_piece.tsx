8ca7b7cfe45393eb7094a11eede70c2f31e4dd37
dtm2451 <daniel.bunis@ucsf.edu>
2022-02-07 17:27:03 -0800

    Vulcan Viz - subset input piece cleanup

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/subsetDataFrame_piece.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/subsetDataFrame_piece.tsx
@@ -2,10 +2,11 @@ import React, { useMemo } from 'react';
 import {DataEnvelope} from './input_types';
 import DropdownAutocomplete from 'etna-js/components/inputs/dropdown_autocomplete';
 import { checkboxInput, dropdownInput, MultiselectInput, rangeInput } from './user_input_pieces';
-import { Button } from '@material-ui/core';
+import { Button, PropTypes } from '@material-ui/core';
 
 /*
 This script defines a component that behaves like all other 'user_input_pieces'.
+See 'user_input_pieces.tsx' for additional documentation.
 It's a piece that recieves, as additional inputs:
  - 'data_frame' = a hash that corresponds to the json file of python pandas.DataFrame. Keys are column names, values are arrays of data where all elements have the same type.
  - 'sorted' = whether column name dropdowns should be alphanumerically sorted versus left in the same order as they are in the 'data_frame'.
@@ -13,171 +14,181 @@ It's a piece that recieves, as additional inputs:
 
 
 export function subsetDataFrameInput(
-    key: string = "filler", changeFxn: Function, value: DataEnvelope<(string|number|null)[][]> = {'methods':[[null]]},
-    label: string, data_frame: DataEnvelope<any>, sorted = false) {
-      
-      const values = {...value}
-      const columns: string[] = Object.keys(data_frame)
-      const updateCurrent = (
-        newColDef: (string|number|null)[] | null,
-        newLogic: string | null,
-        index: number,
-        current_full = values
-        ) => {
-        let next_full = {...current_full};
-        if (newColDef) {
-          next_full['methods'][index] = newColDef
-        }
-        if (newLogic && index>0) {
-          next_full['logic'][index-1] = [newLogic]
-        }
-        changeFxn(next_full, key)
-      }
-      
-      const single_definition = (index: number, Def: (string|number|null)[] = [null]) => {
-        
-        // updateFxns for inner piece calls
-        // 'x' is a dummy variable needed because each piece will send a dummy key in addition the 'newDef' value.
-        const updateLogic = (newLogic: string|null, x: string) => {
-          updateCurrent(null, newLogic, index)
-        }
-        const updateDef = (newDef: (string|number|null)[], x: string) => {
-          // Inners will not have the column_name
-          const col = [Def[0]]
-          updateCurrent(col.concat(...newDef), null, index)
-        }
-        
-        const clearDef = () => {
-          const full = {...values};
-          // methods
-          let next_methods = full['methods']
-          next_methods.splice(index,1)
-          if (next_methods.length==0) {next_methods = [[null]]}
-          // logic
-          changeFxn(
-            {methods: next_methods},
-            key)
-        }
-        
-        let this_def_comps = null;
-        let def_comp = null;
-        const pick_column = (
-          <div key={key+index} style={{display: 'inline-flex'}}>
-            Feature {" "+(index+1)}:
-            <DropdownAutocomplete
-              list={columns}
-              value={Def[0]}
-              onSelect={(newVal: string) => updateCurrent([newVal], null, index)}
-              sorted={sorted}
-            />
-          </div>
-        )
-        const clear_comp = (
-          <Button onClick={(x) => {clearDef()}}>
-            Clear
-          </Button>
-        )
-        const logic_comp = (index == 0) ? null : 
-          dropdownInput(
-              key+index+index+index, updateLogic, values['logic'][index-1][0] as string,
-              "Combination Logic:", ["and", "or"], false
-            )
-        
-        if (Def[0]!=null) {
-
-          const target_data = Object.values(data_frame[Def[0]])
-          
-          let inner_def = [...Def]
-          inner_def.shift()
-                        
-          if (typeof target_data[0] == "number") {
-            if (inner_def.length == 0) {
-              inner_def = ["exactly", null, "below", null]
-            }
-            def_comp = rangeInput(
-              key+index+index, updateDef, inner_def,
-              ""
-              )
-          }
-          
-          if (typeof target_data[0] == "string") {
-            function onlyUnique(value: any, index: number, self: any) {
-              return self.indexOf(value) === index;
-            }
-            const options = target_data.filter(onlyUnique) as string[];
-            def_comp = MultiselectInput(
-              key+index+index, updateDef, inner_def as string[],
-              "Keep:", options
-              )
-          }
-          
-          if (typeof target_data[0] == "boolean") {
-            let use = ""
-            if (inner_def.length > 0) {
-              use = inner_def[0] as string
-            }
-            def_comp = dropdownInput(
-              key+index+index, updateDef, use,
-              "", ["True", "False"], false
-              )
-          }
-        }
-        
-        this_def_comps = (
-          <div style={{display: 'inline-flex'}}>
-            <div>
-              {logic_comp}
-              {pick_column}
-              {def_comp}
-            </div>
-            {clear_comp}
-          </div>
-        )
-        
-        return(
-          <div key={key+index} style={{
-            paddingLeft: "5px",
-            paddingTop: "10px"}}>
-            {this_def_comps}
-          </div>
-        )
-      }
-      
-      const startOrClear = (doSubset: boolean, x:any) => {
-        let new_full = {};
-        if (doSubset) {
-          new_full = {
-            'methods':[[null]],
-            'logic':[[]]
-          }
-        }
-        changeFxn(new_full, key)
-      }
-      
-      const base = checkboxInput(
-        key, startOrClear, Object.keys(values).includes('methods'),
-        label)
-      
-      let inner = null;
-      if (values && values['methods']) {
-        inner = (
-          <div>
-            {values['methods'].map((Def, index) => {
-              return single_definition(index, Def)
-              })
-            }
-            <Button onClick={(x) => {updateCurrent([null], "and", values['methods'].length)}}>
-              Add another condition?
-            </Button>
-          </div>
-        )
-      }
-      return(
-        <div key={key}>
-          <p></p>
-          {base}
-          {inner}
-          <p></p>
-        </div>
-      )
+  key: string = "filler", changeFxn: Function, value: DataEnvelope<(string|number|null)[][]> = {},
+  label: string, data_frame: DataEnvelope<any>, sorted = false, color: PropTypes.Color = "primary") {
+    
+  const values = {...value}
+  
+  const startOrClear = (doSubset: boolean, x:any) => {
+    const new_full = (doSubset) ? {
+      'methods':[[null]],
+      'logic':[[]]
+    } : {}
+    changeFxn(new_full, key)
+  }
+  const updateCurrent = (
+    newColDef: (string|number|null)[] | null,
+    newLogic: string | null,
+    index: number,
+    current_full = values
+    ) => {
+    let next_full = {...current_full};
+    if (newColDef) {
+      next_full['methods'][index] = newColDef
     }
+    if (newLogic && index>0) {
+      next_full['logic'][index-1] = [newLogic]
+    }
+    changeFxn(next_full, key)
+  }
+  
+  const base = checkboxInput(
+    key, startOrClear, Object.keys(values).includes('methods'),
+    label)
+  
+  const meat = (values && values['methods']) ? (
+    <div>
+      {values['methods'].map((def, index) => {
+        return singleMethod(
+          def, index, data_frame, updateCurrent, changeFxn, key, values, sorted, color)
+        })
+      }
+      <Button
+        color={color}
+        onClick={(x) => {updateCurrent([null], "and", values['methods'].length)}}>
+        Add another condition?
+      </Button>
+    </div>
+    ) : null;
+  // console.log(values);
+  return(
+    <div key={key}>
+      <p></p>
+      {base}
+      {meat}
+      <p></p>
+    </div>
+  )
+}
+
+const singleMethod = (
+  def: (string|number|null)[] = [null],
+  index: number,
+  data_frame: DataEnvelope<any>,
+  updateCurrent: Function,
+  overallChangeFxn: Function,
+  key: string,
+  values: DataEnvelope<(string|number|null)[][]>,
+  sorted: boolean,
+  color: PropTypes.Color
+  ) => {
+    
+  // updateFxns for inner piece calls
+  // 'x' is a dummy variable needed because each piece will send a dummy key in addition the 'newDef' value.
+  const updateLogic = (newLogic: string|null, x: string) => {
+    updateCurrent(null, newLogic, index)
+  }
+  const updateDefTargets = (newDef: (string|number|null)[], x: string) => {
+    // Data target inners will not have the column_name
+    const col = [def[0]]
+    updateCurrent(col.concat(...newDef), null, index)
+  }
+  const updateDefColumn = (newCol: string, x: string) => {
+    // (Resets away any data targets)
+    updateCurrent([newCol], null, index)
+  }
+  
+  const columns: string[] = Object.keys(data_frame)
+  
+  const clearDef = () => {
+    const full = {...values};
+    // methods
+    let next_methods = full['methods']
+    next_methods.splice(index,1)
+    if (next_methods.length==0) {next_methods = [[null]]}
+    // logic
+    overallChangeFxn(
+      {methods: next_methods},
+      key)
+  }
+  
+  const pick_column = dropdownInput(
+    key+index, updateDefColumn, def[0] as string|null,
+    "Feature " + (index+1) + ":", columns, sorted
+  )
+  const clear_comp = (
+    <Button
+      color={color}
+      onClick={(x) => {clearDef()}}>
+      Clear
+    </Button>
+  )
+  const logic_comp = (index == 0) ? null : 
+    dropdownInput(
+        key+index+index+index, updateLogic, values['logic'][index-1][0] as string,
+        "Combination Logic:", ["and", "or"], false
+      )
+  const def_comp = targetSelectionComponent(
+    def, data_frame, updateDefTargets, key, index)
+  
+  return(
+    <div key={key+index}
+      style={{
+        paddingLeft: "5px",
+        paddingTop: "10px",
+        display: 'inline-flex'}}>
+      <div>
+        {logic_comp}
+        {pick_column}
+        {def_comp}
+      </div>
+      {clear_comp}
+    </div>
+  )
+}
+
+function targetSelectionComponent(
+  def: (string|number|null)[],
+  data_frame: DataEnvelope<any>,
+  updateFxn: Function,
+  key: string,
+  index: number
+  ) {
+    
+  if (def[0]==null) return null
+  
+  let inner_def = [...def]
+  inner_def.shift()
+  
+  const target_data = Object.values(data_frame[def[0]])              
+  
+  if (typeof target_data[0] == "number") {
+    return rangeInput(
+      key+index+index, updateFxn,
+      (inner_def.length > 0) ? inner_def : ["exactly", null, "below", null],
+      ""
+      )
+  }
+  if (typeof target_data[0] == "string") {
+    // Need to build the unique options set first.
+    function onlyUnique(value: any, index: number, self: any) {
+      return self.indexOf(value) === index;
+    }
+    const options = target_data.filter(onlyUnique) as string[];
+    return MultiselectInput(
+      key+index+index, updateFxn,
+      inner_def as string[],
+      "Keep:", options
+      )
+  }
+  if (typeof target_data[0] == "boolean") {
+    return dropdownInput(
+      key+index+index, updateFxn,
+      (inner_def.length > 0) ? inner_def[0] as string : "",
+      "Keep:", ["True", "False"], false
+      )
+  }
+  return(
+    <div>Not yet implemented Data Taype. Please followup with someone from the Data Library Team!</div>
+  ) 
+}
