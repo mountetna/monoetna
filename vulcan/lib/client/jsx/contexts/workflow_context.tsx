9eebd374c2b7a0b6f3bd1133b3ef3903935529f1
Cole <4930129+coleshaw@users.noreply.github.com>
2022-02-10 10:51:30 -0500

    complete should check that there are status steps to review

--- vulcan/lib/client/jsx/contexts/workflow_context.tsx
+++ vulcan/lib/client/jsx/contexts/workflow_context.tsx
@@ -1,17 +1,16 @@
 import React, {useContext, useMemo} from 'react';
 import {VulcanContext} from "./vulcan_context";
-import {defaultWorkflow, Workflow} from "../api_types";
-import {allWorkflowPrimaryInputSources, statusOfStep, uiOutputOfStep} from "../selectors/workflow_selectors";
+import {defaultWorkflow} from "../api_types";
 
 export function useWorkflow() {
   const {state} = useContext(VulcanContext);
   const workflow = state.workflow || defaultWorkflow;
-  const {status, session} = state;
+  const {status} = state;
 
   const hasPendingEdits = state.bufferedSteps.length > 0;
 
   const complete = useMemo(
-    () => status[0].every(({status}) => status === 'complete'),
+    () => status[0].length > 0 && status[0].every(({status}) => status === 'complete'),
     [status]
   );
 
