141a2370ac6ea12338a480b355b8809943cbf9ca
Zachary Collins <recursive.cookie.jar@gmail.com>
2022-02-15 11:08:26 -0800

    Adjustment

--- airflow/opt/providers/etna/etna/etls/etl_task_batching.py
+++ airflow/opt/providers/etna/etna/etls/etl_task_batching.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Tuple
 
-from airflow.models.taskinstance import Context
+from airflow.models.taskinstance import Context, TaskInstance
 from airflow.utils.timezone import utc
 
 batch_start_context_key = 'data_interval_start'
@@ -11,9 +11,10 @@ batch_end_context_key = 'data_interval_end'
 LOWEST_BOUND = datetime(2010, 1, 1).replace(tzinfo=utc)
 
 def get_batch_range(context: Context) -> Tuple[datetime, datetime]:
-    if context['prev_execution_date'] is not None:
-        start_date = context[batch_start_context_key]
-    else:
+    ti: TaskInstance = context['ti']
+    if ti.get_previous_dagrun() is None:
         start_date = LOWEST_BOUND
+    else:
+        start_date = context[batch_start_context_key]
     end_date = context[batch_end_context_key]
     return start_date, end_date
