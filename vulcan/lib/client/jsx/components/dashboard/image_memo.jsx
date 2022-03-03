3a8ad81c1d7be5a75ced39dae6e304f753ef483b
Cole <4930129+coleshaw@users.noreply.github.com>
2022-03-03 09:14:36 -0500

    show icon in row

--- vulcan/lib/client/jsx/components/dashboard/image_memo.jsx
+++
@@ -1,9 +0,0 @@
-// Mostly to use React.memo to try and
-//   cache the images.
-import React from 'react';
-
-const ImageMemo = React.memo(({src, className, alt = ''}) => {
-  return <img className={className} src={src} alt={alt} />;
-});
-
-export default ImageMemo;
--- vulcan/lib/client/jsx/components/dashboard/workflows_table.tsx
+++ vulcan/lib/client/jsx/components/dashboard/workflows_table.tsx
@@ -1,17 +1,30 @@
-import React, {useContext, useMemo} from 'react';
+import React, {useContext, useMemo, useState} from 'react';
+import * as _ from 'lodash';
 import 'regenerator-runtime/runtime';
 
 import Chip from '@material-ui/core/Chip';
 import {
   DataGrid,
   GridCellParams,
-  GridValueFormatterParams
+  GridValueFormatterParams,
+  GridRowId,
+  GridSelectionModel,
+  GridColumnHeaderParams
 } from '@material-ui/data-grid';
 import {makeStyles} from '@material-ui/core/styles';
 
 import {useActionInvoker} from 'etna-js/hooks/useActionInvoker';
 
 import {VulcanContext} from '../../contexts/vulcan_context';
+import ImageMemo from './image_memo';
+
+// To get webpack to pick up the files.
+require('../../../img/umap.png');
+require('../../../img/barplot.png');
+require('../../../img/scatter.png');
+require('../../../img/yplot.png');
+require('../../../img/default.png');
+require('../../../img/add_integers.png');
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -23,10 +36,15 @@ const useStyles = makeStyles((theme) => ({
   },
   none: {
     color: '#f44'
+  },
+  thumbnail: {
+    maxWidth: '32px',
+    maxHeight: '32px'
   }
 }));
 
 export default function WorkflowsTable({project_name}: {project_name: string}) {
+  const [selectionModel, setSelectionModel] = useState<GridRowId[]>([]);
   const invoke = useActionInvoker();
   let {state} = useContext(VulcanContext);
   const {workflows} = state;
@@ -41,9 +59,24 @@ export default function WorkflowsTable({project_name}: {project_name: string}) {
 
   const columns = [
     {
+      field: 'image',
+      headerName: '',
+      flex: 1,
+      renderCell: (params: GridCellParams) => {
+        return (
+          <ImageMemo
+            className={classes.thumbnail}
+            src={`/images/${params.value || 'default.png'}`}
+            alt='Workflow image'
+          />
+        );
+      },
+      renderHeader: (params: GridColumnHeaderParams) => <></>
+    },
+    {
       field: 'displayName',
       headerName: 'Name',
-      flex: 1
+      flex: 2
     },
     {
       field: 'authors',
@@ -51,32 +84,33 @@ export default function WorkflowsTable({project_name}: {project_name: string}) {
       valueFormatter: (params: GridValueFormatterParams) => {
         return ((params.value || []) as string[]).join(',');
       },
-      flex: 1
+      flex: 2
     },
     {
       field: 'description',
       headerName: 'Description',
-      flex: 3
+      flex: 4
     },
     {
       field: 'lastModified',
       headerName: 'Last Modified',
-      flex: 1
+      flex: 2
     },
     {
       field: 'tags',
       headerName: 'Tags',
       renderCell: (params: GridCellParams) => {
-        console.log('params', params);
         return (
           <>
-            {((params.value || []) as string[]).map((tag: string) => {
-              return <Chip label={tag} />;
-            })}
+            {((params.value || []) as string[]).map(
+              (tag: string, index: number) => {
+                return <Chip key={index} label={tag} />;
+              }
+            )}
           </>
         );
       },
-      flex: 1
+      flex: 2
     }
   ];
 
@@ -90,11 +124,15 @@ export default function WorkflowsTable({project_name}: {project_name: string}) {
     <DataGrid
       autoHeight={true}
       rows={rows}
+      selectionModel={selectionModel}
       columns={columns}
       getRowId={(row) => row.name}
       pageSize={20}
       onRowClick={(params, e) => {}}
       hideFooterSelectedRowCount={true}
+      onSelectionModelChange={(model: GridSelectionModel) => {
+        setSelectionModel(_.isEqual(selectionModel, model) ? [] : model);
+      }}
     />
   );
 }
