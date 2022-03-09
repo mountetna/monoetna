c484493bee1cef23ffaf87c196d5e599ff060ffb
dtm2451 <daniel.bunis@ucsf.edu>
2022-03-09 02:40:18 -0800

    Vulcan MaterialUI - IntegerInput

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
@@ -2,23 +2,39 @@ import React, {useCallback} from 'react';
 import {WithInputParams} from './input_types';
 import {some} from "../../../../selectors/maybe";
 import {useSetsDefault} from "./useSetsDefault";
-import {selectDefaultNumber} from "./monoids";
 import { TextField } from '@material-ui/core';
 
-export function FloatInput({onChange, label, minWidth, data, ...props}: WithInputParams<{label?: string, minWidth?: number}, number | null, number | null>) {
-  const onNewFloat = useCallback((event: any) => onChange(some(to_num(event.target.value))), [onChange]) // todo: better typing for event
-  const value = useSetsDefault(0, props.value, onChange); // Had to replace selectDefaultNumber(data) with 0 due how the component can be cleared.  NaN was not an option because of cross-language conversion.
+export default function FloatInput({
+  onChange, label, minWidth, data, ...props
+}: WithInputParams<{label?: string, minWidth?: number}, number | null, number | null>) {
+    return NumberInput({onChange, label, minWidth, data, ...props})
+}
 
+export function NumberInput(
+  {onChange, label, minWidth, data, ...props}: WithInputParams<{label?: string, minWidth?: number}, number | null, number | null>,
+  block_decimal=false
+  ) {
+  const onNewFloat = useCallback(
+    (event: any) => {
+      let next_value = event.target.value
+      if ( block_decimal && /\./.test(event.target.value) ) {
+        const before_decimal = (/(.*)\./.exec(event.target.value) as string[])[1]
+        next_value = /^[+-]?$/.test(before_decimal) ? "0" : before_decimal
+      }
+      onChange(some(to_num(next_value)))
+    },
+    [onChange]) // todo: better typing for event
+  const value = useSetsDefault(0, props.value, onChange); // Had to replace selectDefaultNumber(data) with 0 due how the component can be cleared.  NaN was not an option because of cross-language conversion.
+  
   return <TextField
     value={from_num(value)}
     label={label}
     type="number"
-    variant="outlined"
     onChange={onNewFloat}
     size="small"
     style={{minWidth: minWidth || 200}}
   />;
-}
+};
 
 // Being a string input at heart, the MaterialUI TextField can be emptied with a backspace.
 function to_num(str:string) {
@@ -28,5 +44,3 @@ function to_num(str:string) {
 function from_num(num: number | null) {
   return num == null ? "" : num.toString()
 }
-
-export default FloatInput;
