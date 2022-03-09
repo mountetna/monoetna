4a0733db3f35f3f001a93fe1c192c34ec1fb0be2
dtm2451 <daniel.bunis@ucsf.edu>
2022-03-09 14:14:47 -0800

    Vulcan MAterialUI - improve number inputs

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import {WithInputParams} from './input_types';
 import {some} from "../../../../selectors/maybe";
 import {useSetsDefault} from "./useSetsDefault";
@@ -14,31 +14,34 @@ export function NumberInput(
   {onChange, label, minWidth, data, ...props}: WithInputParams<{label?: string, minWidth?: number}, number | null, number | null>,
   block_decimal=false
   ) {
+  const value = useSetsDefault(0, props.value, onChange); // Had to replace selectDefaultNumber(data) with 0 due how the component can be cleared.  NaN was not an option because of cross-language conversion.
+  const [inputState, setInputState] = useState({text: from_num(value), hasError: false})
   const onNewFloat = useCallback(
     (event: any) => {
-      let next_value = event.target.value
-      if ( block_decimal && /\./.test(event.target.value) ) {
-        const before_decimal = (/(.*)\./.exec(event.target.value) as string[])[1]
-        next_value = /^[+-]?$/.test(before_decimal) ? "0" : before_decimal
+      const parser = ( block_decimal ) ? parseIntBetter : Number
+      const parsed = parser(event.target.value)
+      if (isNaN(parsed)) {
+        setInputState({text: event.target.value, hasError: true})
+        onChange(null)
+      } else {
+        setInputState({text: event.target.value, hasError: false})
+        onChange(some(parsed))
       }
-      onChange(some(to_num(next_value)))
     },
     [onChange]) // todo: better typing for event
-  const value = useSetsDefault(0, props.value, onChange); // Had to replace selectDefaultNumber(data) with 0 due how the component can be cleared.  NaN was not an option because of cross-language conversion.
   
   return <TextField
-    value={from_num(value)}
+    value={inputState.text}
     label={label}
-    type="number"
+    error={inputState.hasError}
     onChange={onNewFloat}
     size="small"
     style={{minWidth: minWidth || 200}}
   />;
 };
-
-// Being a string input at heart, the MaterialUI TextField can be emptied with a backspace.
-function to_num(str:string) {
-  return str == "" ? null : Number(str)
+function parseIntBetter(s: string){
+  const parsed = parseInt(s, 10);
+  return parsed + "" == s ? parsed : NaN;
 }
 
 function from_num(num: number | null) {
