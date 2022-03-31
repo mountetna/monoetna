b05a83706d3222a79572b583cb2dc6fe0fa3f2ee
Zachary Collins <recursive.cookie.jar@gmail.com>
2022-03-31 15:45:24 -0700

    Second half of migration

--- metis/db/migrations/026_widen_datablock_size.rb
+++ metis/db/migrations/026_widen_datablock_size.rb
@@ -1,12 +1,18 @@
 Sequel.migration do
   up do
+    # for dev environments where the migration was not run by hand.
+    unless Metis.instance.environment == :production
+      from(:data_blocks).where(size: nil).each do |db|
+        db.update(size: db.actual_size || 0)
+      end
+    end
     alter_table(:data_blocks) do
-      set_column_type :size, :bigint
+      set_column_type :size, :bigint, null: false
     end
   end
   down do
     alter_table(:data_blocks) do
-      set_column_type :size, Integer
+      set_column_type :size, Integer, null: true
     end
   end
 end
