86e4d55c24dd1ac74392d0b7121dafc0c46275f8
Cole <4930129+coleshaw@users.noreply.github.com>
2022-04-06 16:15:44 -0400

    remove try except

--- airflow/opt/providers/etna/etna/plugins/impersonate_user_view.py
+++ airflow/opt/providers/etna/etna/plugins/impersonate_user_view.py
@@ -4,6 +4,8 @@ from wtforms import SelectField
 from wtforms.validators import InputRequired
 from flask_appbuilder.fieldwidgets import Select2Widget
 from flask_appbuilder.forms import DynamicForm
+from flask_appbuilder import SimpleFormView
+from flask_appbuilder.security.decorators import has_access
 
 
 class ImpersonateUserForm(DynamicForm):
@@ -14,41 +16,34 @@ class ImpersonateUserForm(DynamicForm):
         widget=Select2Widget())
 
 
-try:
-    from flask_appbuilder import SimpleFormView
-    from flask_appbuilder.security.decorators import has_access
+class ImpersonateUserView(SimpleFormView):
+    form = ImpersonateUserForm
+    form_title = 'User whose permissions you want to impersonate'
 
-    class ImpersonateUserView(SimpleFormView):
-        form = ImpersonateUserForm
-        form_title = 'User whose permissions you want to impersonate'
+    @has_access
+    def form_get(self, form):
+        def sort_user_name(u):
+            return u.first_name
 
-        @has_access
-        def form_get(self, form):
-            def sort_user_name(u):
-                return u.first_name
+        users = self.appbuilder.sm.get_all_users()
+        users.sort(key=sort_user_name)
+        form.email.choices = [(u.email, u.first_name) for u in users]
 
-            users = self.appbuilder.sm.get_all_users()
-            users.sort(key=sort_user_name)
-            form.email.choices = [(u.email, u.first_name) for u in users]
+    @has_access
+    def form_post(self, form):
+        # post process form
+        subject = self.appbuilder.sm.find_user(email=form.email.data)
 
-        @has_access
-        def form_post(self, form):
-            # post process form
-            subject = self.appbuilder.sm.find_user(email=form.email.data)
+        if subject is None:
+            raise ValueError(f"{form.email.data} not found.")
 
-            if subject is None:
-                raise ValueError(f"{form.email.data} not found.")
+        subject_roles = self.appbuilder.sm.get_user_roles(subject)
 
-            subject_roles = self.appbuilder.sm.get_user_roles(subject)
+        impersonator = self.appbuilder.sm.current_user
 
-            impersonator = self.appbuilder.sm.current_user
+        impersonator.roles = subject_roles
+        self.appbuilder.sm.update_user(impersonator)
 
-            impersonator.roles = subject_roles
-            self.appbuilder.sm.update_user(impersonator)
+        flash(f"Done -- you have the same Airflow permissions as {subject.first_name}. Logout to reset your permissions.", 'info')
 
-            flash(f"Done -- you have the same Airflow permissions as {subject.first_name}. Logout to reset your permissions.", 'info')
-
-    impersonate_user_view = ImpersonateUserView()
-
-except (ImportError, ModuleNotFoundError) as e:
-    impersonate_user_view = None
+impersonate_user_view = ImpersonateUserView()
