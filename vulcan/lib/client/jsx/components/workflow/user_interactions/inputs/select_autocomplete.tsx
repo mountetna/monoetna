f63da06769ec8b5efd71077dedfca88e5118e971
dtm2451 <daniel.bunis@ucsf.edu>
2022-04-15 12:03:40 -0700

    Vulcan nested_select_autocomplete - bug fix: value is invalid mui complaint

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/select_autocomplete.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/select_autocomplete.tsx
@@ -17,6 +17,7 @@ export default function SelectAutocompleteInput(
   Special Case: If any data key is "recommendation", a line of text will display the values of this recommendation to the user.
   */
   const [data_use, suggestion] = useMemoized(pullRecommendation, data)
+  if (data_use == {}) return null
   const options_in = useMemoized(flattenStringOptions, data_use);
   const value = useSetsDefault(null, props.value, onChange);
   const disp_label = useMemo( () => {return suggestion ? suggestion : label}, [suggestion, label]);
@@ -25,7 +26,7 @@ export default function SelectAutocompleteInput(
   const [inputState, setInputState] = React.useState(dispValue(value));
   const [options, setOptionsFull] = useState({
     filtered: options_in,
-    display: options_in
+    display: [...options_in].splice(0,maxOptions)
     });
   function setOptions(filteredOptions: string[]) {
     setOptionsFull({
@@ -44,7 +45,8 @@ export default function SelectAutocompleteInput(
     const query = (inputState!=value) ? inputState : ''
     
     if (options_in.length>1000) {
-      setLoadingOptions(true); setOptions([]);
+      setLoadingOptions(true);
+      // console.log('calculating options - slow')
       getOptionsDelayed(query, options_in, (filteredOptions: string[]) => {
         // console.log('calculating options - slow')
         setLoadingOptions(false)
@@ -68,9 +70,8 @@ export default function SelectAutocompleteInput(
     <Autocomplete
       disableClearable={disableClearable}
       clearOnBlur={true}
-      options={options.display}
-      // disable filtering on client
-      filterOptions={(x: string[]) => x}
+      options={options_in}
+      filterOptions={(x: string[]) => options.display}
       loading={loadingOptions}
       value={value}
       onChange={onChangeAction}
