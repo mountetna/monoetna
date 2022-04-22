eb45dcd3896a65a2eb2544b7796148460ae07d4c
GithubAction <mountetna@users.noreply.github.com>
2022-04-22 22:14:06 +0000

    Squashed 'airflow-code-editor/' changes from 245e94758..14542b0d4
    14542b0d4 fix name of ucsf
    070ea513e Fix to flush issue for temp file
    ebbfca58a Improvements to the airflow-code-editor user experience
    
    git-subtree-dir: airflow-code-editor
    git-subtree-split: 14542b0d42d19dbb5f855a326d7db7fa21315571

--- airflow_code_editor/flask_admin_view.py
+++ airflow_code_editor/flask_admin_view.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #
 #   Copyright 2019 Andrea Bonomi <andrea.bonomi@gmail.com>
+#   Modifications included by DSCOLabs engineering team
+#   University of California, San Francisco, San Francisco, CA
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -32,76 +34,5 @@ __all__ = ["admin_view"]
 # ############################################################################
 # Flask Admin (Airflow < 2.0 and rbac = False)
 
-try:
-    from flask_admin import BaseView, expose
-
-    def login_required(func):
-        # when airflow loads plugins, login is still None.
-        @wraps(func)
-        def func_wrapper(*args, **kwargs):
-            if airflow.login:
-                return airflow.login.login_required(func)(*args, **kwargs)
-            return func(*args, **kwargs)
-
-        return func_wrapper
-
-    class AdminCodeEditorView(BaseView, AbstractCodeEditorView):
-        @expose("/")
-        @login_required
-        def index(self):
-            return self._index()
-
-        @expose("/repo", methods=["POST"])
-        @login_required
-        def repo_base(self, path=None):
-            return self._git_repo(path)
-
-        @expose("/repo/<path:path>", methods=["GET", "HEAD", "POST"])
-        @login_required
-        def repo(self, path=None):
-            return self._git_repo(path)
-
-        @expose("/files/<path:path>", methods=["POST"])
-        @login_required
-        def save(self, path=None):
-            return self._save(path)
-
-        @expose("/files/<path:path>", methods=["GET"])
-        @login_required
-        def load(self, path=None):
-            return self._load(path)
-
-        @expose("/format", methods=["POST"])
-        @login_required
-        def format(self, path=None):
-            return self._load(path)
-
-        @expose("/tree", methods=["GET"])
-        @login_required
-        def tree_base(self, path=None):
-            return self._tree(path, args=request.args)
-
-        @expose("/tree/<path:path>", methods=["GET"])
-        @login_required
-        def tree(self, path=None):
-            return self._tree(path, args=request.args)
-
-        @expose("/ping", methods=["GET"])
-        @login_required
-        def ping(self):
-            return self._ping()
-
-        def _render(self, template, *args, **kargs):
-            return self.render(
-                template + "_admin.html",
-                airflow_major_version=self.airflow_major_version,
-                js_files=JS_FILES,
-                version=VERSION,
-                *args,
-                **kargs
-            )
-
-    admin_view = AdminCodeEditorView(url=ROUTE, category=MENU_CATEGORY, name=MENU_LABEL)
-
-except (ImportError, ModuleNotFoundError):
-    admin_view = None
+# We do not support older airflow versions.
+admin_view = None
