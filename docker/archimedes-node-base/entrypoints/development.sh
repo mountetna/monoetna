e07ad645e235b8d6ff68525c228be27f84195aa9
Cole <4930129+coleshaw@users.noreply.github.com>
2022-04-25 10:43:46 -0400

    start of an archimedes-node image?

---
+++ docker/archimedes-node-base/entrypoints/development.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+if [ -z "$RELEASE_TEST" ]; then
+  /entrypoints/build.sh
+fi
+
+if [ -e /app/development ]; then
+  for hook in /app/development/*; do
+    [ -x "$hook" ] && $hook
+  done
+fi
+
+exec node $@
