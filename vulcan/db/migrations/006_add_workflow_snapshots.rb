af199caa6f81714aa437a717c16cbfbb7c95cee6
Cole <4930129+coleshaw@users.noreply.github.com>
2022-06-07 08:17:02 -0400

    store just raw cwl yaml in snapshot

--- vulcan/db/migrations/006_add_workflow_snapshots.rb
+++ vulcan/db/migrations/006_add_workflow_snapshots.rb
@@ -5,15 +5,11 @@ Sequel.migration do
 
       foreign_key :figure_id, :figures, null: false, unique: true
       json :authors, null: false
-      String :name, null: false
       json :projects
       String :vignette, text: true
       String :display_name
       String :description
-      String :cwl_version, null: false, default: "v1.1"
-      json :inputs
-      json :outputs
-      json :steps, null: false
+      String :cwl_yaml, null: false
       json :other_metadata
       json :tags
       String :image
