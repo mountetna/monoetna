18b72afedf1721a3a8d2f018885418c957c94d6f
Cole <4930129+coleshaw@users.noreply.github.com>
2022-06-08 16:18:44 -0400

    tweak wording

--- vulcan/lib/client/jsx/components/workflow/workflow_manager.tsx
+++ vulcan/lib/client/jsx/components/workflow/workflow_manager.tsx
@@ -66,7 +66,7 @@ export default function WorkflowManager({
             !_.isEqual(localSession.inputs, figureResponse.inputs)
           ) {
             useLocal = confirm(
-              "Your local version of this figure diverges from the last saved version. Do you want to keep your changes? Click 'OK' to use your local version, or 'Cancel' to discard all unsaved changes and load the last saved state."
+              "You have unsaved changes. Click 'OK' to use your local version, or 'Cancel' to discard all unsaved changes and load the last saved state."
             );
           }
 
