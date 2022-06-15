6f0dd0a800bccc318d3848544b0a613c47338512
dtm2451 <daniel.bunis@ucsf.edu>
2022-06-15 13:56:25 -0400

    Archimedes plotnine - re-add 'output_column_types' import in plotting __init__.py

--- archimedes/archimedes/functions/plotting/__init__.py
+++ archimedes/archimedes/functions/plotting/__init__.py
@@ -3,8 +3,9 @@ import plotly.io as pio
 
 import plotnine
 
-from .utils import output_plotly
 from .colors import colors
 from .scatter_plot import scatter_plotly, scatter_plotnine
 from .bar_plot import bar_plotly
 from .y_plot import y_plotly, y_plotnine
+
+from .utils import output_plotly, output_column_types
