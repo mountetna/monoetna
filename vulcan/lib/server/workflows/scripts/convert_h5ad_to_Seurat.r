44900de4d06e88c14e850621c63e6db3c2498534
dtm2451 <daniel.bunis@ucsf.edu>
2022-07-11 11:06:55 -0400

    archimedes-r - clean up docker build & add Seurat conversion to scRNAseq workflow

---
+++ vulcan/lib/server/workflows/scripts/convert_h5ad_to_Seurat.r
@@ -0,0 +1,18 @@
+library(dataflow)
+library(SeuratDisk)
+
+tmp_dir <- tempdir()
+h5ad_path <- file.path(tmp_dir, 'scdata.h5ad')
+h5seurat <- file.path(tmp_dir, 'scdata.h5seurat')
+
+file.copy(input_path("scdata.h5ad"), h5ad_path)
+
+Convert(h5ad_path, dest = "h5seurat")
+
+if (file.size(h5seurat)==0) {
+    stop("scanpy to Seurat conversion error: SeuratDisk::Convert() yielded a 0 byte file.")
+}
+
+scdata <- LoadH5Seurat(h5seurat)
+
+saveRDS(scdata, output_path("scdata.Rds"), compress = FALSE)
