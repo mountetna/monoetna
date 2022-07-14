3445983b0fdc8a3875a9b36f74277fd51671a832
Cole <4930129+coleshaw@users.noreply.github.com>
2022-07-14 14:40:07 -0400

    include prefix in task group name

--- airflow/opt/providers/etna/etna/etls/linkers/single_cell_rna_seq.py
+++ airflow/opt/providers/etna/etna/etls/linkers/single_cell_rna_seq.py
@@ -86,7 +86,7 @@ def link_single_cell_attribute_files_v1(
     if attribute_linker_overrides:
         sc_file_linkers.update(**attribute_linker_overrides)
 
-    with TaskGroup(f"{model_name}_processed"):
+    with TaskGroup(f"{model_name}_{single_cell_root_prefix}_processed"):
         processed_matches = helpers.find_record_folders(
             model_name,
             re.compile(f"^{single_cell_root_prefix}[^/]*/processed/{identifier_prefix}[^/]+"),
@@ -96,7 +96,7 @@ def link_single_cell_attribute_files_v1(
         for attr, matcher in sc_file_linkers.items():
             helpers.link_matching_file(listed_matches, attr, re.compile(matcher), dry_run=dry_run)
 
-    with TaskGroup(f"{model_name}_raw"):
+    with TaskGroup(f"{model_name}_{single_cell_root_prefix}_raw"):
         raw_matches = helpers.find_record_folders(
             model_name,
             re.compile(f"^{single_cell_root_prefix}[^/]*/raw/{identifier_prefix}[^/]+"),
