07925c95b16d8bf815bd803b900e453e22406aa9
dtm2451 <daniel.bunis@ucsf.edu>
2022-07-15 17:16:35 -0400

    Revert "vulcan bug - show non-user value updates in 'NumberInput's"
    This reverts commit 8f31b05573a49453775e1c43f8c4c3a007b81fa7.

--- vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
+++ vulcan/lib/client/jsx/components/workflow/user_interactions/inputs/float.tsx
@@ -1,6 +1,6 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {WithInputParams} from './input_types';
-import {some, withDefault} from "../../../../selectors/maybe";
+import {some} from "../../../../selectors/maybe";
 import {useSetsDefault} from "./useSetsDefault";
 import { TextField } from '@material-ui/core';
 
@@ -17,23 +17,18 @@ export function NumberInput(
   const value = useSetsDefault(0, props.value, onChange); // Had to replace selectDefaultNumber(data) with 0 due how the component can be cleared.  NaN was not an option because of cross-language conversion.
   const [inputState, setInputState] = useState({text: from_num(value), hasError: false})
   const onNewFloat = useCallback(
-    (value: string, onlyInputState: boolean = false) => {
+    (event: any) => {
       const parser = ( block_decimal ) ? parseIntBetter : Number
-      const parsed = parser(value)
+      const parsed = parser(event.target.value)
       if (isNaN(parsed)) {
-        setInputState({text: value, hasError: true})
-        if (!onlyInputState) onChange([null])
+        setInputState({text: event.target.value, hasError: true})
+        onChange([null])
       } else {
-        setInputState({text: value, hasError: false})
-        if (!onlyInputState) onChange(some(parsed))
+        setInputState({text: event.target.value, hasError: false})
+        onChange(some(parsed))
       }
     },
     [onChange]) // todo: better typing for event
-
-  // Catch & show non-user value updates.
-  useEffect(() => {
-    onNewFloat(from_num(withDefault(props.value,null)), true)
-  }, [props.value])
   
   return (
     <div style={{paddingTop: label ? 8 : 0}}>
@@ -41,7 +36,7 @@ export function NumberInput(
         value={inputState.text}
         label={label}
         error={inputState.hasError}
-        onChange={(event) => onNewFloat(event.target.value)}
+        onChange={onNewFloat}
         size="small"
         style={{minWidth: minWidth || 200}}
       />
