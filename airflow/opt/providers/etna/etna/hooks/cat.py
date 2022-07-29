7f7be3635261bf1d2e7233fc05f21b44eab98f61
Cole <4930129+coleshaw@users.noreply.github.com>
2022-07-29 09:48:11 -0400

    update doc; fix spec

--- airflow/opt/providers/etna/etna/hooks/cat.py
+++ airflow/opt/providers/etna/etna/hooks/cat.py
@@ -317,9 +317,8 @@ class Cat(SSHBase):
         Save the cursor to the database.
         """
         latest_cursor = Variable.get(self.variable_key(postfix), default_var={}, deserialize_json=True)
-        latest_cursor.update(self.cursors[postfix])
 
-        self.cursors[postfix] = latest_cursor
+        self.cursors[postfix] = {**latest_cursor, **self.cursors[postfix]}
         Variable.set(self.variable_key(postfix), self.cursors[postfix], serialize_json=True)
 
     def variable_key(self, postfix: str):
