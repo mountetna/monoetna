b17095f88b5e9fdbe3e74c4fe4542ec41145d534
GithubAction <mountetna@users.noreply.github.com>
2022-08-03 19:07:46 +0000

    Squashed 'etna/' changes from 3c18cdd4e..a94c80019
    a94c80019 Whoops, remove debug raise
    b484e9541 Fix tags
    ce706548b Fixes to instrumentation
    e7d8f1b65 Merge branch 'master' into zc/instrumentation-fixes
    e5aa9d0f5 Merge branch 'master' into zc/instrumentation-fixes
    fd0635ee0 WIP
    
    git-subtree-dir: etna
    git-subtree-split: a94c80019344a1eaa6a31e46e186498872506586

---
+++ lib/etna/instrumentation.rb
@@ -0,0 +1,56 @@
+module Etna
+  module Instrumentation
+    def self.included(cls)
+      cls.instance_eval do
+        def self.time_it(method_name, &metric_block)
+          orig_method_name = :"#{method_name}_without_time_it"
+          self.alias_method orig_method_name, method_name
+
+          self.define_method method_name do |*args|
+            time_it(orig_method_name, args, &metric_block)
+          end
+        end
+      end
+    end
+
+    def increment_it(&block)
+      if has_yabeda?
+        metric = yield
+        metric.increment({})
+      end
+    end
+
+    def time_it(method_name, args, &metric_block)
+      if has_yabeda?
+        start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        begin
+          return send(method_name, *args)
+        ensure
+          dur = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
+          if block_given?
+            metric = yield
+            tags = {}
+          else
+            tags = {class_name: self.class.name, method_name: method_name.to_s}
+            metric = Yabeda.etna.perf
+          end
+          metric.measure(tags, dur)
+        end
+      else
+        return send(method_name, *args, **kwds)
+      end
+    end
+
+    def with_yabeda_tags(tags, &block)
+      if has_yabeda?
+        Yabeda.with_tags(tags, &block)
+      else
+        yield
+      end
+    end
+
+    def has_yabeda?
+      defined?(Yabeda) && Yabeda.configured?
+    end
+  end
+end
