82047530b88f12ae2410730bf79b4948da229f52
GithubAction <mountetna@users.noreply.github.com>
2022-08-15 20:23:15 +0000

    Squashed 'etna/' changes from 3be6f9358..a024976b5
    a024976b5 fix spec and return object
    e6acd7f2f fix spec
    a39faab0b etna-py magma should account for tsv queries
    
    git-subtree-dir: etna
    git-subtree-split: a024976b5ec7b73ea49533b305c4a41e7657b670

--- packages/etna-py/src/mountetna/magma.py
+++ packages/etna-py/src/mountetna/magma.py
@@ -1,9 +1,14 @@
 import dataclasses
 import typing
+from io import StringIO
+from functools import partial
 from inspect import isgenerator
 from typing import Dict, Optional, List
 from serde import serialize, deserialize
 from serde.json import from_json, to_json
+
+import pandas as pd
+
 from .etna_base import EtnaClientBase
 from requests import HTTPError
 from .utils.iterables import batch_iterable
@@ -297,6 +302,10 @@ class Magma(EtnaClientBase):
             data=to_json(query),
             headers={"Content-Type": "application/json"},
         )
+        if 'format' in query and 'tsv' == query['format']:
+            pd_wrapper = partial(pd.read_csv, sep="\t")
+            tsv_data = StringIO(response.text)
+            return pd_wrapper(tsv_data)
 
         return from_json(QueryResponse, response.content)
 
