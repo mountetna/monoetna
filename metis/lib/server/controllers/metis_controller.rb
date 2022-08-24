d8025d4fc0dd74e4c2b25f25275a4ce9bc6855ea
Cole <4930129+coleshaw@users.noreply.github.com>
2022-08-24 11:33:10 -0400

    make explicit that is stdlib File

--- metis/lib/server/controllers/metis_controller.rb
+++ metis/lib/server/controllers/metis_controller.rb
@@ -2,7 +2,7 @@ require_relative "../../folder_path_calculator"
 
 class Metis
   class Controller < Etna::Controller
-    VIEW_PATH = File.expand_path("../views", __dir__)
+    VIEW_PATH = ::File.expand_path("../views", __dir__)
 
     def success_json(obj)
       success(obj.to_json, "application/json")
