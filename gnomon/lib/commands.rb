02b48c50128949a183f6cb74b00a98abd8b83593
Saurabh Asthana <faplap@gmail.com>
2022-09-23 13:32:43 -0700

    rm db

--- gnomon/lib/commands.rb
+++ gnomon/lib/commands.rb
@@ -1,28 +1,5 @@
 
 class Gnomon
-  class Migrate < Etna::Command
-    usage 'Run migrations for the current environment.'
-    string_flags << '--version'
-
-    def execute(version: nil)
-      Sequel.extension(:migration)
-      db = Gnomon.instance.db
-
-      if version
-        puts "Migrating to version #{version}"
-        Sequel::Migrator.run(db, 'db/migrations', target: version.to_i)
-      else
-        puts 'Migrating to latest'
-        Sequel::Migrator.run(db, 'db/migrations')
-      end
-    end
-
-    def setup(config)
-      super
-      Gnomon.instance.setup_db
-    end
-  end
-
   class Console < Etna::Command
     usage 'Open a console with a connected Gnomon instance.'
 
--- gnomon/lib/gnomon.rb
+++ gnomon/lib/gnomon.rb
@@ -3,21 +3,4 @@ require 'pathname'
 
 class Gnomon
   include Etna::Application
-  attr_reader :db
-
-  def setup_db
-    return if @db
-    @db = Sequel.connect(config(:db))
-    @db.extension :connection_validator
-    @db.extension :pg_json
-    @db.pool.connection_validation_timeout = -1
-  end
-
-  def setup_sequel
-    Sequel::Model.plugin :timestamps, update_on_create: true
-    Sequel::Model.require_valid_table = false
-    Sequel.extension :inflector
-    Sequel.extension :migration
-    #require_relative 'models/models'
-  end
 end
