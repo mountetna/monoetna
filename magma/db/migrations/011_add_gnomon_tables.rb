976c282419a5d92913bb8bf18f8cf092a8887fe7
Saurabh Asthana <faplap@gmail.com>
2022-09-27 08:51:33 -0700

    rename rule => grammar, add rule field

--- magma/db/migrations/011_add_gnomon_tables.rb
+++ magma/db/migrations/011_add_gnomon_tables.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   change do
-    create_table(:rules) do
+    create_table(:grammars) do
       primary_key :id
 
       String :project_name, null: false
@@ -17,7 +17,8 @@ Sequel.migration do
       String :project_name, null: false
       String :identifier, null: false
       String :author, null: false
-      foreign_key :rule_id, :rules, null: false
+      String :rule, null: false
+      foreign_key :grammar_id, :grammars, null: false
       foreign_key :renamed_to_id, :identifiers
       DateTime :created_at, null: false
 
--- magma/lib/magma/identifier.rb
+++ magma/lib/magma/identifier.rb
@@ -1,6 +1,6 @@
 class Magma
   class Identifier < Sequel::Model
-     many_to_one :rule
+     many_to_one :grammar
      many_to_one :renamed_to, class: self, key: :renamed_to_id
      one_to_many :aliases, class: self, key: :renamed_to_id
   end
