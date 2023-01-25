ef3bac00050f3bd602a1c158dfb23fdc949d3143
Cole <4930129+coleshaw@users.noreply.github.com>
2023-01-25 09:06:31 -0500

    add option to disable closing modal by clicking on backdrop; close #1182

--- etna/packages/etna-js/components/ModalDialogContainer.jsx
+++ etna/packages/etna-js/components/ModalDialogContainer.jsx
@@ -1,34 +1,49 @@
-import React, { useState, createContext, useContext, useCallback } from 'react';
+import React, {useState, createContext, useContext, useCallback} from 'react';
 require('./ModalDialog.css');
 
 const ModalDialogContext = createContext({});
 
-export function ModalDialogContainer({ children }) {
+export function ModalDialogContainer({children}) {
   const [modalState, setModalState] = useState({});
   const dismissModal = useCallback(() => setModalState({}), [setModalState]);
-  const modalContextValue = { modalState, setModalState, dismissModal };
-  const { component } = modalState;
+  const modalContextValue = {modalState, setModalState, dismissModal};
+  const {component, opts} = modalState;
+
+  function closeOnClickBackdrop() {
+    return opts.hasOwnProperty('closeOnClickBackdrop')
+      ? !!opts.closeOnClickBackdrop
+      : true;
+  }
 
   let modal = null;
   if (component) {
-    modal = <div className='etna-modal-window' onClick={dismissModal}>
-      <div onClick={(e) => e.stopPropagation()}>
-        { component }
+    console.log('opts', opts, closeOnClickBackdrop());
+    modal = (
+      <div
+        className='etna-modal-window'
+        onClick={() => (closeOnClickBackdrop() ? dismissModal() : null)}
+      >
+        <div onClick={(e) => e.stopPropagation()}>{component}</div>
       </div>
-    </div>;
+    );
   }
 
-  return <ModalDialogContext.Provider value={modalContextValue}>
-    {children}
-    {modal}
-  </ModalDialogContext.Provider>
+  return (
+    <ModalDialogContext.Provider value={modalContextValue}>
+      {children}
+      {modal}
+    </ModalDialogContext.Provider>
+  );
 }
 
 export function useModal() {
   const {setModalState, dismissModal} = useContext(ModalDialogContext);
-  const openModal = useCallback(function openModal(component) {
-    setModalState({component});
-  }, [setModalState])
+  const openModal = useCallback(
+    function openModal(component, opts = {}) {
+      setModalState({component, opts});
+    },
+    [setModalState]
+  );
 
   return {openModal, dismissModal};
 }
