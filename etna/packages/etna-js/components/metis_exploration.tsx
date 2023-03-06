30ac223aa6c9ed544733e50c952a45332f999fe1
dtm2451 <daniel.bunis@ucsf.edu>
2023-03-06 16:48:17 -0500

    metis paths components - refactor file-folder content management into top-level

--- etna/packages/etna-js/components/metis_exploration.tsx
+++ etna/packages/etna-js/components/metis_exploration.tsx
@@ -5,8 +5,11 @@ import { Grid, TextField } from '@material-ui/core';
 import { json_get } from 'etna-js/utils/fetch';
 import SubdirectoryArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight';
 import FolderSharpIcon from '@material-ui/icons/FolderSharp';
+import {useAsyncCallback} from 'etna-js/utils/cancellable_helpers';
 
 declare const CONFIG: {[key: string]: any};
+
+declare type folderSummary = {folders: string[], files: string[]}
 
 function nullToEmptyString(value: string | null | undefined) {
   return value == null ? '' : value;
@@ -18,9 +21,16 @@ function addSlash(path: string) {
   }
   return path
 }
+function stripSlash(path: string) {
+  if (path.slice(-1) == '/') {
+    path = path.slice(0,-1)
+  }
+  return path
+}
 
 function arrayToPath(pathElements: string[], basePath: string) {
-  return addSlash(basePath) + pathElements.join('/')
+  const base = basePath!='' ? addSlash(basePath) : ''
+  return base + pathElements.join('/')
 }
 
 function pathToArray(path: string, basePath: string) {
@@ -36,6 +46,17 @@ function simplifyFolderListReturn(fullList: any) {
     folders: fullList.folders.map( (f: any) => f.folder_name) as string[],
     files: fullList.files.map( (f: any) => f.file_name) as string[]
   }
+}
+
+async function getFolderContents(path: string, project_name: string, bucket: string) {
+  const full_path = `${bucket}/${path}`
+  let contents = {} as folderSummary
+  await json_get(metisPath(`${project_name}/list/${full_path}`)).then(
+    metis_return => {
+      contents = simplifyFolderListReturn(metis_return)
+    }
+  )
+  return contents
 }
 
 export function PickBucket({ project_name=CONFIG.project_name, setBucket, bucket, label="Bucket"}: {
@@ -54,123 +75,93 @@ export function PickBucket({ project_name=CONFIG.project_name, setBucket, bucket
   }, [project_name] );
 
   return (
-    <Autocomplete
-      options={bucketList}
-      value={nullToEmptyString(bucket)}
-      onChange={ (event: any, e: string | null) => {
-        setBucket(e)
-      }}
-      disableClearable
-      disablePortal
-      inputValue={inputState}
-      onInputChange={(event: any, newInputState: string) => {
-        setInputState(newInputState);
-      }}
-      style={{paddingTop: 8}}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          error={ bucket==null || bucket == '' || inputState != nullToEmptyString(bucket)}
-          label={label}
-          size='small'
-          InputLabelProps={{shrink: true}}
+    <Grid container direction='row'>
+      <Grid item container alignItems='flex-end' style={{width: 'auto'}}>
+        {'\uD83E\uDEA3'}
+      </Grid>
+      <Grid item>
+        <Autocomplete
+          options={bucketList}
+          value={nullToEmptyString(bucket)}
+          onChange={ (event: any, e: string | null) => {
+            setBucket(e)
+          }}
+          disableClearable
+          disablePortal
+          inputValue={inputState}
+          onInputChange={(event: any, newInputState: string) => {
+            setInputState(newInputState);
+          }}
+          style={{paddingTop: 8}}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              error={ bucket==null || bucket == '' || inputState != nullToEmptyString(bucket)}
+              label={label}
+              size='small'
+              InputLabelProps={{shrink: true}}
+            />
+          )}
         />
-      )}
-    />
+      </Grid>
+    </Grid>
   );
 }
 
-function SingleFileOrFolder({ project_name, bucket, path, setTarget, onEmpty, basePath, topLevelPlaceholer, topLevelLabel, allowFiles = true, autoOpen}: {
-  project_name?: string;
-  bucket: string;
+function SingleFileOrFolder({ optionSet, path, setTarget, onEmpty, basePath, topLevelPlaceholer, topLevelLabel, autoOpen, awaitingContents}: {
+  optionSet: string[];
   path: string;
   setTarget: Function;
   onEmpty: Function;
-  allowFiles?: boolean;
   autoOpen: boolean;
   basePath: string;
   topLevelPlaceholer?: string;
   topLevelLabel: string;
+  awaitingContents: boolean;
 }){
-  const [fullTargetList, setFullTargetList] = useState({folders: [] as string[], files: [] as string[]})
-  function restrictTargetList(fullList = fullTargetList, allow_files = allowFiles) {
-    return allow_files ? fullList.folders.concat(fullList.files) : fullList.folders
-  }
-  const [targetsWithContents, setTargetsWithContents] = useState([] as string[])
-  const [targetList, setTargetList] = useState(restrictTargetList(fullTargetList));
   const [target, setTargetInternal] = useState('');
   const [inputState, setInputState] = useState(target);
 
-  useEffect( () => {
-    if (bucket != null) {
-      const full_path = path.length>0 ? `${bucket}/${path}` : bucket
-      json_get(metisPath(`${project_name}/list/${full_path}`)).then(
-        metis_return => {
-          const fullList = simplifyFolderListReturn(metis_return)
-          setFullTargetList(fullList)
-          setTargetList(restrictTargetList(fullList))
-          let withContents = [] as string[]
-          fullList.folders.forEach(
-            (folder) => {
-              json_get(metisPath(`${project_name}/list/${full_path}/${folder}`)).then(
-                child_metis_return => {
-                  const contents_allowed = allowFiles ?
-                    child_metis_return.folders.concat(child_metis_return.files) :
-                    child_metis_return.folders
-                  if (contents_allowed.length > 0 ) {
-                    withContents.push(folder)
-                  }
-              })
-          })
-          setTargetsWithContents(withContents)
-        }
-      );
-    }
-    // console.log(path)
-    // console.log(targetList)
-    // console.log(targetsWithContents)
-  }, [bucket, project_name, path] );
-
-  return project_name == null ? null : (
-    <Autocomplete
-      key={path+'-selection'}
-      disablePortal
-      openOnFocus={autoOpen}
-      autoHighlight
-      options={targetList.concat('')}
-      inputValue={inputState}
-      value={target}
-      onInputChange={(event: any, newInputState: string) => {
-        setInputState(newInputState);
-      }}
-      onChange={ (event: any, e: string | null) => {
-        const nextTarget = nullToEmptyString(e)
-        setTargetInternal(nextTarget)
-        setTarget(nextTarget.concat(targetsWithContents.includes(nextTarget) ? '/' : ''))
-        if (nextTarget == '') {
-          onEmpty()
-        }
-      }}
-      filterOptions={(options, state) => {
-        const query = inputState != target ? inputState : '';
-        return targetList.filter((o) => {
-          return query == null ? true : o.indexOf(query) > -1;
-        });
-      }}
-      style={{paddingTop: samePath(path, basePath) ? 8 : undefined}}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          error={ inputState != nullToEmptyString(target) }
-          autoFocus
-          placeholder={samePath(path, basePath) ? topLevelPlaceholer : undefined}
-          label={samePath(path, basePath) ? topLevelLabel : undefined}
-          size='small'
-          InputLabelProps={{shrink: true}}
-        />
-      )}
-    />
-  );
+  return <Autocomplete
+    key={path+'-selection'}
+    disablePortal
+    openOnFocus={autoOpen}
+    autoHighlight
+    options={optionSet.concat('')}
+    loading={awaitingContents}
+    inputValue={inputState}
+    value={target}
+    onInputChange={(event: any, newInputState: string) => {
+      setInputState(newInputState);
+    }}
+    onChange={ (event: any, e: string | null) => {
+      const nextTarget = nullToEmptyString(e)
+      setTargetInternal(nextTarget)
+      if (nextTarget == '') {
+        onEmpty()
+      } else {
+        setTarget(nextTarget)
+      }
+    }}
+    filterOptions={(options, state) => {
+      const query = inputState != target ? inputState : '';
+      return optionSet.filter((o) => {
+        return query == null ? true : o.indexOf(query) > -1;
+      });
+    }}
+    style={{paddingTop: samePath(path, basePath) ? 8 : undefined}}
+    renderInput={(params) => (
+      <TextField
+        {...params}
+        error={ inputState != nullToEmptyString(target) }
+        autoFocus
+        placeholder={samePath(path, basePath) ? topLevelPlaceholer : undefined}
+        label={samePath(path, basePath) ? topLevelLabel : undefined}
+        size='small'
+        InputLabelProps={{shrink: true}}
+      />
+    )}
+  />
 }
 
 export function PickFileOrFolder({ project_name=CONFIG.project_name, bucket, setPath, path, allowFiles, label, basePath, topLevelPlaceholer=''}: {
@@ -182,50 +173,137 @@ export function PickFileOrFolder({ project_name=CONFIG.project_name, bucket, set
   basePath: string; // an immutable portion of path.  Can be '' to access the entire bucket and to be compatible with exploring across buckets in sync with a PickBucket companion.
   topLevelPlaceholer?: string;
   allowFiles?: boolean
-}){
+}) {
   const [pathArray, setPathArray] = useState(pathToArray(path, basePath));
   const labelUse = label!==undefined ? label : allowFiles ? "File or Folder" : "Folder"
+  const [contentsSeen, setContentsSeen] = useState({} as {[k: string]: folderSummary})
+  const [awaitingContents, setAwaitingContents] = useState(true)
 
+  const contentUse = (folderContents: folderSummary) => {
+    return allowFiles ?
+      folderContents.folders.concat(folderContents.files) :
+      folderContents.folders
+  }
+  const contentKey = useCallback( (folderPath) => {
+    return `${project_name}_${bucket}_${folderPath}`
+  }, [project_name, bucket])
+
+  const pathSeen = useCallback( (folderPath) => {
+    return Object.keys(contentsSeen).includes(contentKey(folderPath))
+  }, [contentKey, contentsSeen])
+  const subPathsSeen = useCallback( (folderPath) => {
+    const pathKey = contentKey(folderPath)
+    if (!pathSeen(folderPath)) return false
+    if (contentsSeen[pathKey].folders.length == 0) {
+      return true
+    } else {
+      return contentsSeen[pathKey].folders.every(
+        (folder: string) => {
+          pathSeen(`${folderPath}/${folder}`)
+        }
+      )
+    }
+  }, [contentKey, contentsSeen])
+  
+  const addNewConstentsSeen = (folderPath: string, newContents: folderSummary) => {
+    const fullContentsSeen = {...contentsSeen}
+    const key = contentKey(folderPath)
+    fullContentsSeen[key] = newContents
+    setContentsSeen(fullContentsSeen)
+  }
+  const [fetchFolderContents] = useAsyncCallback(function* (
+    path: string, callback: Function
+  ) {
+    const full_path = `${bucket}/${path}`
+    const metis_return = yield json_get(metisPath(`${project_name}/list/${full_path}`));
+    callback(metis_return);
+  },
+  [project_name, bucket, contentsSeen]);
+
+  // Buffer current folder contents
   useEffect(() => {
-    setPathArray(pathToArray(path, basePath));
-  }, [path])
+    const newPath = stripSlash(arrayToPath(pathArray, basePath))
+    const newKey = contentKey(newPath)
+    // Obtain contents of current folder
+    if (!pathSeen(newPath)) {
+      fetchFolderContents(newPath, (x: any) => {
+        const contents = simplifyFolderListReturn(x)
+        addNewConstentsSeen(newPath, contents)
+        setAwaitingContents(false)
+      })
+    } else {
+      setAwaitingContents(false)
+    }
+  }, [bucket, project_name, pathArray])
+
+  // Buffer potentially next folder contents
+  useEffect( () => {
+    const newPath = stripSlash(arrayToPath(pathArray, basePath))
+    if (pathSeen(newPath) && !subPathsSeen(newPath)) {
+      const newKey = contentKey(newPath)
+      contentsSeen[newKey].folders.forEach(
+        (folder: string) => {
+          const subPath = newPath=='' ? folder : `${newPath}/${folder}`
+          if (!pathSeen(subPath)) {
+            fetchFolderContents(subPath, (x: any) => {
+              addNewConstentsSeen(subPath, simplifyFolderListReturn(x))
+            })
+          }
+        }
+      )
+    }
+  }, [bucket, project_name, pathArray, contentsSeen])
+
+  // Update main readout (path) whenever anything updates pathArray
+  useEffect(() => {
+    setPath(arrayToPath(pathArray, basePath));
+  }, [pathArray])
 
   // onChange for inners
-  const updateTarget = useCallback(
-    (newPath: string, currentPathSet: string[], depth: number) => {
-      // Also trim at the level just picked in case not actually the deepest
-      // console.log(newPath)
-      let nextPathSet = [...currentPathSet].slice(0,depth+1)
-      nextPathSet[depth] = newPath
-      setPath(arrayToPath(nextPathSet, basePath))
-    }, [setPath])
+  const updateTarget = (newPath: string, currentPathSet: string[], depth: number) => {
+    // Also trim at the level just picked in case not actually the deepest
+    let nextPathSet = [...currentPathSet].slice(0,depth+1)
+    nextPathSet[depth] = newPath
+    if (contentUse(contentsSeen[contentKey(arrayToPath(nextPathSet, basePath))]).length > 0 ) {
+      nextPathSet.push('')
+    }
+    setAwaitingContents(true)
+    setPathArray(nextPathSet)
+  }
   
   // clear for inners
-  const trimPath = useCallback(
-    (depth: number) => {
-    const nextPathSet = depth > 0 ? [...pathArray].slice(0,depth+1) : ['']
-    setPath(arrayToPath(nextPathSet, basePath))
-  }, [setPath])
+  const trimPath = (depth: number) => {
+    const nextPathSet = depth > 0 ? [...pathArray].slice(0,depth) : ['']
+    setPathArray(nextPathSet)
+  }
 
-  const targetSelectors = useMemo(() => {
-    return pathArray.map( (p, index, fullSet) => {
+  // console.log({pathArray})
+  // console.log({contentsSeen})
+
+  return Object.keys(contentsSeen).length==0 ? null : <Grid 
+    key={`file-or-folder-picker-${project_name}-${bucket}`}
+    container
+    direction='column'
+  >
+    {pathArray.map( (p, index, fullSet) => {
       const before = index > 1 ? pathArray[index-1] : '';
-      const icon = index > 0 ? <SubdirectoryArrowRightIcon fontSize='small'/> : <FolderSharpIcon fontSize='small'/>;
+      const icon = index > 0 ? <SubdirectoryArrowRightIcon fontSize='small'/> : '\uD83D\uDCC1';
+      const thisPath = arrayToPath(fullSet.slice(0,index), basePath)
+      const ready = (index+1) == fullSet.length ? !awaitingContents : true
       return (
         <Grid item container
-          key={`sub-picker-${bucket}-${project_name}-${before}`+ index}
+          key={`sub-picker-${project_name}-${bucket}-${before}`+ index}
           >
           <Grid item container alignItems='flex-end' style={{width: 'auto'}}>
             {icon}
           </Grid>
           <Grid item style={{flex: '1 1 auto'}}>
             <SingleFileOrFolder
-              project_name={project_name}
-              bucket={bucket}
+              optionSet={ ready ? contentUse(contentsSeen[contentKey(thisPath)]) : []}
+              awaitingContents={ ready }
               setTarget={(e: string) => updateTarget(e, pathArray, index)}
               onEmpty={ () => { trimPath(index) } }
-              path={arrayToPath(fullSet.slice(0,index), basePath)}
-              allowFiles={allowFiles}
+              path={thisPath}
               autoOpen={index!=0}
               basePath={basePath}
               topLevelPlaceholer={topLevelPlaceholer}
@@ -234,18 +312,7 @@ export function PickFileOrFolder({ project_name=CONFIG.project_name, bucket, set
           </Grid>
         </Grid>
       )
-    })
-  }, [pathArray, project_name, bucket, updateTarget])
-
-  // console.log(path)
-  // console.log(pathArray)
-
-  return bucket == null ? null : <Grid 
-    key={`file-or-folder-picker-${bucket}-${project_name}`}
-    container
-    direction='column'
-  >
-    {targetSelectors}
+    })}
   </Grid>
 }
 
