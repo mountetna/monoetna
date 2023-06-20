295c27fd564decb53e1c1b4762b5e73f25081041
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2023-06-20 14:27:27 -0700

    typos

--- magma/lib/magma/flag.rb
+++ magma/lib/magma/flag.rb
@@ -4,8 +4,8 @@ class Magma
 
   module Flags
     # All hashes nested under this namespace represent flags that can be used in a production.
-    # Each has should define a name key representing the name of a flag and each
-    # hash should also specify all potential values that can be assigned to a flag.
+    # Each hash should define a 'name' key, that represents the name of the flag and it
+    # should also specify all potential values that can be assigned to a flag.
     GNOMON_MODE = {
       name: 'gnomon_mode',
       identifier: 'identifier',
