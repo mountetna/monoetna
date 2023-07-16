182b67015a2b11a580c2d77ced94c3ceaeacf84f
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2023-07-16 12:42:27 -0700

    force identifier and pattern mode to use valid identifiers

--- magma/lib/magma/attributes/identifier_attribute.rb
+++ magma/lib/magma/attributes/identifier_attribute.rb
@@ -24,29 +24,34 @@ class Magma
         # We skip validation if we have the gnomon flag or if it's value is set to none
         return if flag_value.nil? or flag_value == gnomon_mode[:none]
 
+        begin
         # Identifier mode - we just care about the existence of the identifier in the identifier table
-        if flag_value == gnomon_mode[:identifier]
-          return if Magma.instance.db[:identifiers].where(project_name: @model.project_name.to_s, identifier: value).any?
-          yield "The identifier '#{value}' has not been assigned in Gnomon."
-
+          if flag_value == gnomon_mode[:identifier]
+            get_identifier(value)
           # Pattern mode - We must make sure the identifier conforms to a grammar
-        elsif flag_value == gnomon_mode[:pattern]
-          begin
-            identifier = Magma::Gnomon::Identifier.where(project_name: @model.project_name.to_s, identifier: value).first
-            raise "The identifier '#{value}' has not been assigned in Gnomon." unless identifier
-            raise "The identifier '#{value}' is out of date." unless identifier.is_up_to_date?
-
+          elsif flag_value == gnomon_mode[:pattern]
+            identifier = get_identifier(value)
             grammar = Magma::Gnomon::Grammar.for_project(@model.project_name.to_s)
             raise "Grammar not found, identifier is: '#{value}'" unless grammar
 
             rule = grammar.parser.rules[identifier.rule]
             raise "No such #{identifier.rule} for #{@model.project_name.to_s}" unless rule
             raise "The identifier '#{value}' does not conform to a grammar in Gnomon." unless rule.valid?(value)
-          rescue => e
-            yield "#{e.message}"
           end
+        rescue => e
+          yield "#{e.message}"
         end
       end
+
+      private
+
+      def get_identifier(value)
+        identifier = Magma::Gnomon::Identifier.where(project_name: @model.project_name.to_s, identifier: value).first
+        raise "The identifier '#{value}' has not been assigned in Gnomon." unless identifier
+        raise "The identifier '#{value}' is out of date." unless identifier.is_up_to_date?
+        identifier
+      end
+
     end
   end
 end
