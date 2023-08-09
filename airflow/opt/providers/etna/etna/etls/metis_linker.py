4af47406d0d898d433583a529c81aecfa3904527
Saurabh Asthana <faplap@gmail.com>
2023-08-09 08:57:49 -0700

    don't wait for previous metis linker runs

--- airflow/opt/providers/etna/etna/etls/metis_linker.py
+++ airflow/opt/providers/etna/etna/etls/metis_linker.py
@@ -140,8 +140,8 @@ class MetisLoaderConfig(EtlConfigResponse):
     schedule_interval=timedelta(minutes=60),
     start_date=datetime(2023, 4, 12),
     default_args={
-        'depends_on_past': True,
-        'wait_for_downstream': True
+        'depends_on_past': False,
+        'wait_for_downstream': False
     }
 )
 def MetisLinker():
