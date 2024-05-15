0e50e8fe8d5be0f04e9baf916e7fee39236fbd46
Josh Trotter <6855626+maflaven@users.noreply.github.com>
2024-05-15 16:08:17 -0600

    finish typography

--- vesta/ui/src/theme.ts
+++ vesta/ui/src/theme.ts
@@ -6,6 +6,13 @@ import { theFuture, relativeMonoPro10Pitch, relativeMonoPro11Pitch, relativeMono
 // https://mui.com/material-ui/customization/typography/#adding-amp-disabling-variants
 declare module '@mui/material/styles' {
   interface TypographyVariants {
+    h3Digits: React.CSSProperties;
+    h3MediumWt: React.CSSProperties;
+    h3SmallCaps: React.CSSProperties;
+    h4SmallCaps: React.CSSProperties;
+    h5BoldWt: React.CSSProperties;
+    h6BoldWt: React.CSSProperties;
+    h6SmallCaps: React.CSSProperties;
     pTitleRegularWt: React.CSSProperties;
     pTitleMediumWt: React.CSSProperties;
     pTitleMono: React.CSSProperties;
@@ -39,6 +46,13 @@ declare module '@mui/material/styles' {
 
   // allow configuration using `createTheme`
   interface TypographyVariantsOptions {
+    h3Digits?: React.CSSProperties;
+    h3MediumWt?: React.CSSProperties;
+    h3SmallCaps?: React.CSSProperties;
+    h4SmallCaps?: React.CSSProperties;
+    h5BoldWt?: React.CSSProperties;
+    h6BoldWt?: React.CSSProperties;
+    h6SmallCaps?: React.CSSProperties;
     pTitleRegularWt?: React.CSSProperties;
     pTitleMediumWt?: React.CSSProperties;
     pTitleMono?: React.CSSProperties;
@@ -86,6 +100,13 @@ declare module '@mui/material/styles' {
 // Update the Typography's variant prop options
 declare module '@mui/material/Typography' {
   interface TypographyPropsVariantOverrides {
+    h3Digits: true;
+    h3MediumWt: true;
+    h3SmallCaps: true;
+    h4SmallCaps: true;
+    h5BoldWt: true;
+    h6BoldWt: true;
+    h6SmallCaps: true;
     pTitleRegularWt: true;
     pTitleMediumWt: true;
     pTitleMono: true;
@@ -126,7 +147,7 @@ declare module '@mui/material/Typography' {
 }
 
 
-const LETTER_SPACING = '-0.01em'
+const DEFAULT_LETTER_SPACING = '-0.01em'
 
 const theme = createTheme({
   palette: {
@@ -147,81 +168,81 @@ const theme = createTheme({
     pTitleRegularWt: {
       fontSize: 22,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pTitleMediumWt: {
       fontSize: 22,
       fontWeight: 500,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pTitleMono: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 22,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pTitleMonoCaps: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 22,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
       textTransform: 'uppercase',
     },
     pSubtitle: {
       fontSize: 20,
       fontWeight: 500,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pSubtitleBoldWt: {
       fontSize: 20,
       fontWeight: 700,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pSubtitleMono: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 20,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pSubtitleMonoCaps: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 20,
       lineHeight: '140%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
       textTransform: 'uppercase',
     },
     pMedium: {
       fontSize: 18,
       lineHeight: '150%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pMediumMediumWt: {
       fontSize: 18,
       fontWeight: 500,
       lineHeight: '150%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pMediumBoltWt: {
       fontSize: 18,
       fontWeight: 700,
       lineHeight: '150%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pMediumMono: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 18,
       lineHeight: '150%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pMediumMonoCaps: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 18,
       fontWeight: 500,
       lineHeight: '150%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
       textTransform: 'uppercase',
     },
     pBoldMonoCaps: {
@@ -229,76 +250,76 @@ const theme = createTheme({
       fontSize: 18,
       fontWeight: 700,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
       textTransform: 'uppercase',
     },
     pBody: {
       fontSize: 16,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pBodyMediumWt: {
       fontSize: 16,
       fontWeight: 500,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pBodyBoldWt: {
       fontSize: 16,
       fontWeight: 700,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pBodyMono: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 16,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     pXS: {
       fontSize: 15,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p2XS: {
       fontSize: 14,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p2XSBoldWt: {
       fontSize: 14,
       fontWeight: 700,
       lineHeight: '153%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p3XS: {
       fontSize: 12,
       lineHeight: '122%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p3XSMediumWt: {
       fontSize: 12,
       fontWeight: 500,
       lineHeight: '122%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p3XSBoldWt: {
       fontSize: 12,
       fontWeight: 700,
       lineHeight: '122%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p3XSMono: {
       fontFamily: relativeMonoPro10Pitch.style.fontFamily,
       fontSize: 12,
       fontWeight: 700,
       lineHeight: '122%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     p4XS: {
       fontSize: 10,
       lineHeight: '122%',
-      letterSpacing: LETTER_SPACING,
+      letterSpacing: DEFAULT_LETTER_SPACING,
     },
     subtitle1: undefined,
     subtitle2: undefined,
@@ -311,11 +332,190 @@ const theme = createTheme({
   components: {},
 });
 
+theme.typography.h1 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 36,
+  fontWeight: 700,
+  lineHeight: '135%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 46,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 56,
+    lineHeight: '144%',
+  },
+}
+
+theme.typography.h2 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 24,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 36,
+    lineHeight: '135%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 48,
+    lineHeight: '144%',
+  },
+}
+
+theme.typography.h3 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 22,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 36,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 46,
+    lineHeight: '140%',
+  },
+}
+
+theme.typography.h3MediumWt = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 22,
+  fontWeight: 500,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 36,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 46,
+    lineHeight: '140%',
+  },
+}
+
+theme.typography.h3SmallCaps = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 22,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  fontVariant: 'all-small-caps', // TODO: verify this is accurate
+}
+
+theme.typography.h3Digits = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 46,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: '0em',
+}
+
+theme.typography.h4 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 22,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 24,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 36,
+    lineHeight: '135%',
+  },
+}
+
+theme.typography.h4SmallCaps = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 18,
+  fontWeight: 700,
+  lineHeight: '135%',
+  letterSpacing: '0.04em',
+  fontVariant: 'all-small-caps', // TODO: verify this is accurate
+}
+
+theme.typography.h5 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 20,
+  fontWeight: 500,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 22,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 24,
+    lineHeight: '135%',
+  },
+}
+
+theme.typography.h5BoldWt = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 20,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 22,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 24,
+    lineHeight: '135%',
+  },
+}
+
+theme.typography.h6 = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 16,
+  fontWeight: 500,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 18,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 22,
+    lineHeight: '135%',
+  },
+}
+
+theme.typography.h6BoldWt = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 18,
+  fontWeight: 700,
+  lineHeight: '140%',
+  letterSpacing: DEFAULT_LETTER_SPACING,
+  [theme.breakpoints.up('tablet')]: {
+    fontSize: 18,
+    lineHeight: '140%',
+  },
+  [theme.breakpoints.up('desktop')]: {
+    fontSize: 22,
+    lineHeight: '135%',
+  },
+}
+
+theme.typography.h6SmallCaps = {
+  fontFamily: theFuture.style.fontFamily,
+  fontSize: 14,
+  fontWeight: 700,
+  lineHeight: '153%',
+  letterSpacing: '0.05em',
+  fontVariant: 'all-small-caps', // TODO: verify this is accurate
+}
+
 theme.typography.pLarge = {
   fontFamily: theFuture.style.fontFamily,
   fontSize: 18,
   lineHeight: '150%',
-  letterSpacing: LETTER_SPACING,
+  letterSpacing: DEFAULT_LETTER_SPACING,
   [theme.breakpoints.up('tablet')]: {
     fontSize: 20,
     lineHeight: '148%',
@@ -332,7 +532,7 @@ theme.typography.pLargeMediumWt = {
   fontSize: 20,
   fontWeight: 500,
   lineHeight: '148%',
-  letterSpacing: LETTER_SPACING,
+  letterSpacing: DEFAULT_LETTER_SPACING,
   [theme.breakpoints.up('desktop')]: {
     fontSize: 22,
     lineHeight: '148%',
@@ -345,42 +545,10 @@ theme.typography.pLargeBoldWt = {
   fontSize: 18,
   fontWeight: 700,
   lineHeight: '150%',
-  letterSpacing: LETTER_SPACING,
+  letterSpacing: DEFAULT_LETTER_SPACING,
   [theme.breakpoints.up('desktop')]: {
     fontSize: 20,
     lineHeight: '148%',
-  },
-}
-
-theme.typography.h1 = {
-  fontFamily: theFuture.style.fontFamily,
-  fontSize: 36,
-  fontWeight: 700,
-  lineHeight: '135%',
-  letterSpacing: LETTER_SPACING,
-  [theme.breakpoints.up('tablet')]: {
-    fontSize: 46,
-    lineHeight: '140%',
-  },
-  [theme.breakpoints.up('desktop')]: {
-    fontSize: 56,
-    lineHeight: '144%',
-  },
-}
-
-theme.typography.h2 = {
-  fontFamily: theFuture.style.fontFamily,
-  fontSize: 24,
-  fontWeight: 700,
-  lineHeight: '140%',
-  letterSpacing: LETTER_SPACING,
-  [theme.breakpoints.up('tablet')]: {
-    fontSize: 36,
-    lineHeight: '135%',
-  },
-  [theme.breakpoints.up('desktop')]: {
-    fontSize: 48,
-    lineHeight: '144%',
   },
 }
 
