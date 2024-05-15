c956a565fba5e77b50d361546d58c036d10345b6
Josh Trotter <6855626+maflaven@users.noreply.github.com>
2024-05-15 12:17:08 -0600

    basic theme and font setup

--- vesta/ui/src/app/page.tsx
+++ vesta/ui/src/app/page.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
+import Container from '@mui/system/Container';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
+import Box from '@mui/system/Box';
 import NextLink from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/Copyright';
+// import ProTip from '@/components/ProTip';
+// import Copyright from '@/components/Copyright';
 
 export default function Home() {
   return (
@@ -22,9 +21,9 @@ export default function Home() {
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           Material UI - Next.js App Router example in TypeScript
         </Typography>
-        <Link href="/about" color="secondary" component={NextLink}>
+        <NextLink href="/about" color="secondary">
           Go to the about page
-        </Link>
+        </NextLink>
       </Box>
     </Container>
   );
--- vesta/ui/src/theme.ts
+++ vesta/ui/src/theme.ts
@@ -1,19 +1,13 @@
 'use client';
-import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
-
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-});
+import { theFuture, relativeMonoPro10Pitch, relativeMonoPro11Pitch, relativeMonoPro12Pitch } from '@/fonts';
 
 const theme = createTheme({
   palette: {
     mode: 'light',
   },
   typography: {
-    fontFamily: roboto.style.fontFamily,
+    fontFamily: theFuture.style.fontFamily,
   },
   components: {
     MuiAlert: {
