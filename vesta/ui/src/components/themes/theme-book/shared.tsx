7c9c23c05efd0053bc9754d3f8f15c544ad54800
Josh Trotter <6855626+maflaven@users.noreply.github.com>
2024-06-24 14:24:38 -0600

    begin themes tablet+desktop

---
+++ vesta/ui/src/components/themes/theme-book/shared.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import Box from '@mui/system/Box'
+import { StaticImageData } from "next/image"
+
+
+export interface ThemeData {
+    name: string
+    description: string
+    project_count: number
+    projects_link: string
+    color: string
+    image: StaticImageData
+}
+
+
+export function ProjectCount({
+    count,
+}: {
+    count: number,
+}) {
+    return (
+        <Box
+            component='span'
+            sx={{
+                display: 'inline-flex',
+                columnGap: '10px',
+                py: '10px',
+                borderRadius: '30px',
+                color: 'utilityWhite.main',
+                bgcolor: 'ground.grade10',
+                writingMode: 'vertical-lr',
+                textOrientation: 'mixed',
+                rotate: '180deg',
+            }}
+        >
+            <Box component='span'>
+                {count}
+            </Box>
+            <Box component='span'>
+                {`PROJECT${count > 1 ? 'S' : ''}`}
+            </Box>
+        </Box>
+    )
+}
--- vesta/ui/src/components/themes/theme-shelf.tsx
+++ vesta/ui/src/components/themes/theme-shelf.tsx
@@ -4,8 +4,11 @@ import * as React from 'react'
 import Container from '@mui/system/Container'
 import Box from '@mui/system/Box'
 import Typography from '@mui/material/Typography';
+import { useMediaQuery, useTheme } from '@mui/system';
 
-import ThemeBook, { ThemeData } from './theme-book'
+import ThemeBookVertical from './theme-book/theme-book-vertical'
+import ThemeBookHorizontal from './theme-book/theme-book-horizontal'
+import { ThemeData } from './theme-book/shared';
 
 
 
@@ -14,7 +17,14 @@ export default function ThemeShelf({
 }: {
     themeData: ThemeData[],
 }) {
-    const themeBookContainerRef = React.createRef<HTMLDivElement>()
+    const theme = useTheme()
+    const isMobile = useMediaQuery(theme.breakpoints.between(
+        theme.breakpoints.values.mobile,
+        theme.breakpoints.values.tablet,
+    ))
+
+    const mobileThemeBookContainerRef = React.createRef<HTMLDivElement>()
+    const tabletDesktopThemeBookContainerRef = React.createRef<HTMLDivElement>()
 
     // handle book open state to coordinate only having one open at a time
     const bookOpens = themeData.map(() => React.useState(false))
@@ -32,15 +42,14 @@ export default function ThemeShelf({
     }
 
     const scrollToBook = (bookIdx: number) => {
-        const containerEl = themeBookContainerRef.current
+        const containerEl = isMobile ? mobileThemeBookContainerRef.current : tabletDesktopThemeBookContainerRef.current
         if (containerEl === null) {
             return
         }
 
-        const bookEl = containerEl.children[bookIdx]
+        const bookEl = containerEl.children[bookIdx] as HTMLElement
 
-        window.scrollTo({
-            top: bookEl?.offsetTop,
+        bookEl.scrollIntoView({
             behavior: 'smooth',
         })
     }
@@ -72,16 +81,19 @@ export default function ThemeShelf({
             </Typography>
 
             <Box
-                ref={themeBookContainerRef}
+                ref={mobileThemeBookContainerRef}
                 role='list'
                 sx={(theme) => ({
                     display: 'flex',
                     flexDirection: 'column',
                     rowGap: '10px',
+                    [theme.breakpoints.up('tablet')]: {
+                        display: 'none',
+                    },
                 })}
             >
                 {themeData.map((theme, i) => (
-                    <ThemeBook
+                    <ThemeBookVertical
                         key={theme.name}
                         data={theme}
                         onClickSeeProjects={handleClickSeeProjects}
@@ -91,6 +103,34 @@ export default function ThemeShelf({
                     />
                 ))}
             </Box>
+
+            <Box>
+                <Box
+                    ref={tabletDesktopThemeBookContainerRef}
+                    role='list'
+                    sx={(theme) => ({
+                        display: 'none',
+                        [theme.breakpoints.up('tablet')]: {
+                            display: 'flex',
+                            flexDirection: 'row',
+                            columnGap: '10px',
+                            width: '100%',
+                            overflow: 'scroll',
+                        },
+                    })}
+                >
+                    {themeData.map((theme, i) => (
+                        <ThemeBookHorizontal
+                            key={theme.name}
+                            data={theme}
+                            onClickSeeProjects={handleClickSeeProjects}
+                            open={bookOpens[i][0]}
+                            onSetOpen={(open: boolean) => handleSetBookOpen(open, i)}
+                            onFinishOpen={() => scrollToBook(i)}
+                        />
+                    ))}
+                </Box>
+            </Box>
         </Container>
     )
 }
@@ -98,4 +138,5 @@ export default function ThemeShelf({
 const handleClickSeeProjects = (event: React.MouseEvent<HTMLAnchorElement>, href: string) => {
     event.preventDefault()
     console.log(href)
+    // TODO: scroll to filtered projects
 }
