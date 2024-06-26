4d7a81a319d24b0c276440b162b405a7d38b928e
Josh Trotter <6855626+maflaven@users.noreply.github.com>
2024-06-26 14:31:21 -0600

    finish tablet/desktop theme book

--- vesta/ui/src/components/themes/theme-shelf.tsx
+++ vesta/ui/src/components/themes/theme-shelf.tsx
@@ -14,15 +14,11 @@ import { useWindowDimensions } from '@/lib/utils/responsive';
 import { containerPadding } from '@/theme';
 
 
-
 export default function ThemeShelf({
     themeData,
 }: {
     themeData: ThemeData[],
 }) {
-    const _themeData = [...themeData]
-    _themeData.push(..._themeData.slice(0, 8))
-
     const theme = useTheme()
     const isMobile = useMediaQuery(theme.breakpoints.between(
         theme.breakpoints.values.mobile,
@@ -31,11 +27,10 @@ export default function ThemeShelf({
 
     const mobileThemeBookContainerRef = React.createRef<HTMLDivElement>()
     const tabletDesktopThemeBookContainerRef = React.createRef<HTMLDivElement>()
-    const tabletDesktopThemeBookSpacerRef = React.createRef<HTMLDivElement>()
     const tabletDesktopThemeBookListRef = React.createRef<HTMLDivElement>()
 
     // handle book open state to coordinate only having one open at a time
-    const bookOpens = _themeData.map((_, i) => React.useState(i === 0))
+    const bookOpens = themeData.map((_, i) => React.useState(i === 0))
     const [openBookIdx, setOpenBookIdx] = React.useState<number | null>(0)
 
     const handleSetBookOpen = (newOpenState: boolean, bookIdx: number) => {
@@ -51,6 +46,10 @@ export default function ThemeShelf({
         }
     }
 
+    const tabletDesktopThemeBookGapPx = 10
+    const tabletThemeBookFadeMaskWidthPx = 47
+    const desktopThemeBookFadeMaskWidthPx = 220
+
     const scrollToBook = (bookIdx: number) => {
         const containerEl = isMobile ? mobileThemeBookContainerRef.current : tabletDesktopThemeBookListRef.current
         if (containerEl === null) {
@@ -59,34 +58,35 @@ export default function ThemeShelf({
 
         const bookEl = containerEl.children[bookIdx] as HTMLElement
 
-        containerEl.scroll({
-            left: bookEl.offsetLeft,
-            top: bookEl.offsetTop,
-            behavior: 'smooth',
-        })
+        if (isMobile) {
+            window.scroll({
+                top: bookEl.offsetTop,
+                behavior: 'smooth',
+            })
+        } else {
+            containerEl.scroll({
+                // TODO: include navbar offset
+                left: bookEl.offsetLeft,
+                behavior: 'smooth',
+            })
+        }
+
     }
 
-    const tabletDesktopThemeBookGapPx = 10
-
     // handle positioning ThemeBook for max-width desktop
-    const {
-        dimensions: windowDimensions,
-        isResizing: isWindowResizing,
-    } = useWindowDimensions()
+    const { dimensions: windowDimensions } = useWindowDimensions()
+    const [spacerWidthPx, setSpacerWidthPx] = React.useState<number>(0)
 
     React.useEffect(() => {
-        const themeBookSpacerRef = tabletDesktopThemeBookSpacerRef.current
-        if (themeBookSpacerRef === null || windowDimensions === null) return
+        if (windowDimensions === null) return
 
         const windowWidth = windowDimensions[0]
         const containerMaxWidth = theme.breakpoints.values.desktopLg
 
-        if (windowWidth > containerMaxWidth) {
-            themeBookSpacerRef.style.display = 'block'
-            themeBookSpacerRef.style.minWidth = `${(windowWidth - containerMaxWidth) / 2 - tabletDesktopThemeBookGapPx}px`
-        } else {
-            themeBookSpacerRef.style.display = 'none'
-        }
+        setSpacerWidthPx(windowWidth > containerMaxWidth ?
+            (windowWidth - containerMaxWidth) / 2 - tabletDesktopThemeBookGapPx
+            : 0
+        )
     }, [windowDimensions])
 
     return (
@@ -130,7 +130,7 @@ export default function ThemeShelf({
                     },
                 })}
             >
-                {_themeData.map((theme, i) => (
+                {themeData.map((theme, i) => (
                     <ThemeBookVertical
                         key={theme.name}
                         data={theme}
@@ -154,7 +154,37 @@ export default function ThemeShelf({
             >
                 <Box
                     ref={tabletDesktopThemeBookContainerRef}
+                    sx={(theme) => ({
+                        position: 'relative',
+                        '& .fade-mask': {
+                            display: 'none',
+                            [theme.breakpoints.up('tablet')]: {
+                                '&.fade-mask-right': {
+                                    display: 'block',
+                                },
+                                position: 'absolute',
+                                zIndex: 1,
+                                top: 0,
+                                width: `${tabletThemeBookFadeMaskWidthPx}px`,
+                                height: '100%',
+                            },
+                            [theme.breakpoints.up(theme.breakpoints.values.desktopLg + desktopThemeBookFadeMaskWidthPx + 60)]: {
+                                '&.fade-mask-left': {
+                                    display: 'block',
+                                },
+                                width: `${desktopThemeBookFadeMaskWidthPx}px`
+                            },
+                        },
+                    })}
                 >
+                    <Box
+                        className='fade-mask fade-mask-left'
+                        sx={(theme) => ({
+                            left: 0,
+                            background: `linear-gradient(to left, transparent, ${theme.palette.utilityHighlight.main})`,
+                        })}
+                    />
+
                     <Box
                         ref={tabletDesktopThemeBookListRef}
                         role='list'
@@ -162,18 +192,19 @@ export default function ThemeShelf({
                             display: 'flex',
                             flexDirection: 'row',
                             columnGap: `${tabletDesktopThemeBookGapPx}px`,
+                            justifyContent: 'space-between',
                             width: '100%',
                             overflowX: 'scroll',
-                            // p: '10px',
-                            pt: '30px',
+                            ...containerPadding,
+                            pt: '30px !important',
+                            '& .spacer': {
+                                minWidth: `${spacerWidthPx}px`,
+                                minHeight: '100%',
+                            },
                         }}
                     >
-                        <Box
-                            ref={tabletDesktopThemeBookSpacerRef}
-                            sx={{
-                                minHeight: '100%',
-                            }}
-                        />
+                        <Box className='spacer' />
+
                         <Typography
                             variant='h4SmallCaps'
                             component='div'
@@ -189,7 +220,7 @@ export default function ThemeShelf({
                             THEMES
                         </Typography>
 
-                        {_themeData.map((theme, i) => (
+                        {themeData.map((theme, i) => (
                             <ThemeBookHorizontal
                                 key={theme.name}
                                 data={theme}
@@ -199,7 +230,17 @@ export default function ThemeShelf({
                                 onFinishOpen={() => scrollToBook(i)}
                             />
                         ))}
+
+                        <Box className='spacer' />
                     </Box>
+
+                    <Box
+                        className='fade-mask fade-mask-right'
+                        sx={(theme) => ({
+                            right: 0,
+                            background: `linear-gradient(to right, transparent, ${theme.palette.utilityHighlight.main})`
+                        })}
+                    />
                 </Box>
 
                 <Container>
@@ -222,7 +263,7 @@ export default function ThemeShelf({
                             onClickPrev={() => openBookIdx !== null && handleSetBookOpen(true, openBookIdx - 1)}
                             prevDisabled={openBookIdx === null || openBookIdx === 0}
                             onClickNext={() => handleSetBookOpen(true, openBookIdx === null ? 0 : openBookIdx + 1)}
-                            nextDisabled={openBookIdx === _themeData.length - 1}
+                            nextDisabled={openBookIdx === themeData.length - 1}
                         />
                     </Box>
                 </Container>
