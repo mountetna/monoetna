5a973dfa4dbce448508fdf126028384447fa32ba
Josh Trotter <6855626+maflaven@users.noreply.github.com>
2024-10-07 15:20:18 -0500

    add controller for sending contact email

--- vesta/lib/server.rb
+++ vesta/lib/server.rb
@@ -4,6 +4,7 @@ require "rack"
 require_relative "./vesta"
 require_relative "./vesta/controllers/project_controller"
 require_relative "./vesta/controllers/stats_controller"
+require_relative "./vesta/controllers/contact_controller"
 
 class Vesta
   class Server < Etna::Server
@@ -11,6 +12,8 @@ class Vesta
     get "/stats/projects", action: "stats#project_stats", auth: { noauth: true }
 
     get "/projects", action: "project#projects", auth: { noauth: true }
+
+    post "/contact", action: "contact#send_contact_email", auth: { noauth: true }
 
     def initialize
       super
