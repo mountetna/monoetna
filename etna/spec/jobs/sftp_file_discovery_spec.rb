8e9aef40063a99cd1c2f95b6c3efc6bdc67317fc
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2024-12-02 13:28:39 -0800

    some high level specs

--- etna/spec/jobs/sftp_file_discovery_spec.rb
+++ etna/spec/jobs/sftp_file_discovery_spec.rb
@@ -14,14 +14,47 @@ describe SFTPFileDiscoveryJob do
   }
 
   before do
+    # Setup polyphemus client
     configure_etna_yml
     ENV['TOKEN'] = TEST_TOKEN
+
+    # 
+    ENV['ARGO_WORKFLOW_ID'] = "1234567890"
+    # Setup sftp client
   end
 
-  it "fetch the last scan timestamp from the database" do
-    require 'pry'; binding.pry
-    job = SFTPFileDiscoveryJob.new(config, secrets)
-    job.fetch_last_scan
+  context 'first run' do
+
+    before do
+      # clear the db
+      job = SFTPFileDiscoveryJob.new(config, secrets)
+      job.process 
+    end
+
+    it "records the last scan timestamp in the db" do
+      # check the db
+    end
+
+    it 'records the number of files to update in the db' do
+      # Lol
+    end
+
+    it 'writes the files to update to a csv' do
+    end 
+
+
+  end
+
+  context 'subsequent runs' do
+
+    before do
+      # set the db
+    end
+
+    it "fetches the last scan timestamp from the database" do
+      job = SFTPFileDiscoveryJob.new(config, secrets)
+      job.fetch_last_scan
+    end
   end
 
 end
