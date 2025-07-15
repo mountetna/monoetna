799a8f077d42120e538b97219b57519e0fa4ae9b
Saurabh Asthana <faplap@gmail.com>
2025-07-15 14:58:20 -0700

    remove unused etls

--- polyphemus/lib/etls.rb
+++ polyphemus/lib/etls.rb
@@ -1,6 +1,4 @@
 require_relative 'ipi/ipi_helper'
-require_relative 'etls/materialize_gne_magma_records_etl'
-require_relative 'etls/sync_gne_metis_files_etl'
 require_relative 'etls/magma/magma_etl_script_runner'
 require_relative 'etls/redcap/redcap_etl_script_runner'
 require_relative 'etls/ipi/ipi_load_magma_population_tables_etl'
