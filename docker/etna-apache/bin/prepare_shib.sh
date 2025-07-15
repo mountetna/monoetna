491e9c3642862315309a1f9bdbf441a6302e2442
Saurabh Asthana <faplap@gmail.com>
2025-07-15 13:21:27 -0700

    fix shib sha

--- docker/etna-apache/bin/prepare_shib.sh
+++ docker/etna-apache/bin/prepare_shib.sh
@@ -9,7 +9,7 @@ cd $VERIFIED_SHIB_DIR
 curl -o InCommon-metadata.xml https://mdq.incommon.org/entities
 curl -s https://spaces.at.internet2.edu/display/MDQ/production-mdq-signing-key | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/{s/^.*-----BEGIN/-----BEGIN/;p}' > inc-md-cert-mdq.pem
 
-FP1="SHA256 Fingerprint=60:49:74:D6:1F:E0:D7:F4:D6:3D:6C:8D:B9:8A:85:7E:64:2A:B9:B4:70:E3:E8:5D:D5:4D:66:3D:04:96:F9:00"
+FP1="sha256 Fingerprint=60:49:74:D6:1F:E0:D7:F4:D6:3D:6C:8D:B9:8A:85:7E:64:2A:B9:B4:70:E3:E8:5D:D5:4D:66:3D:04:96:F9:00"
 FP2=$(openssl x509 -sha256 -noout -fingerprint -in inc-md-cert-mdq.pem)
 
 # Verify signing key
