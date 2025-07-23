3a6d3fa5d03deeaa1c713fe9a876c2e21b54382d
dtm2451 <daniel.bunis@ucsf.edu>
2025-07-23 17:18:23 -0400

    magmaR - remove unneeded 'check_cassette_names()' call, deprecated in vcr-v2.0.0

--- etna/packages/magmaR/tests/testthat/helper-magmaR.R
+++ etna/packages/magmaR/tests/testthat/helper-magmaR.R
@@ -3,16 +3,6 @@
 ### Setup relating to vcr
 library("vcr")
 
-# vcr_dir <- "../fixtures"
-# if (!nzchar(Sys.getenv("TOKEN"))) {
-#     if (dir.exists(vcr_dir)) {
-#         Sys.setenv("TOKEN" = "foobar")
-#     } else {
-#         stop("No API key nor cassettes, tests cannot be run.",
-#             call. = FALSE)
-#     }
-# }
-
 TOKEN <- .get_sysenv_or_mock("TOKEN")
 URL <- magmaRset("")$url
 
@@ -20,4 +10,3 @@ invisible(vcr::vcr_configure(
     filter_sensitive_data = list("<<<my_token>>>" = TOKEN),
     dir = "../fixtures"
 ))
-vcr::check_cassette_names()
