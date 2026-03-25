39a28a2ff5c42e4c707a252cf3c6c0d3a0f099a7
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2026-03-25 14:17:46 -0700

    simpler logic

--- metis/db/migrations/029_add_blacklisted_to_data_block.rb
+++ metis/db/migrations/029_add_blacklisted_to_data_block.rb
@@ -1,19 +1,13 @@
 Sequel.migration do
   up do
     alter_table(:data_blocks) do
-      add_column :blacklisted, TrueClass, default: false
+      rename_column :removed, :blacklisted
     end
-
-    # Migrate removed: true -> blacklisted: true, removed: false
-    from(:data_blocks).where(removed: true).update(blacklisted: true, removed: false)
   end
 
   down do
-    # Reverse: blacklisted: true -> removed: true
-    from(:data_blocks).where(blacklisted: true).update(removed: true)
-
     alter_table(:data_blocks) do
-      drop_column :blacklisted
+      rename_column :blacklisted, :removed
     end
   end
 end
