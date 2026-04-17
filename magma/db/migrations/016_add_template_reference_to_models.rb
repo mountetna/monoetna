7afc9839add6d2bee57a8672d0a1e6c0f6e4d30d
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2026-04-17 12:49:35 -0700

    remove fk constraint

--- magma/db/migrations/016_add_template_reference_to_models.rb
+++ magma/db/migrations/016_add_template_reference_to_models.rb
@@ -4,15 +4,11 @@ Sequel.migration do
       add_column :template_project_name, String, null: true
       add_column :template_model_name, String, null: true
       add_index [:template_project_name, :template_model_name]
-      add_foreign_key [:template_project_name, :template_model_name], :models,
-        key: [:project_name, :model_name],
-        name: :models_template_reference_fkey
     end
   end
 
   down do
     alter_table(:models) do
-      drop_constraint :models_template_reference_fkey, type: :foreign_key
       drop_index [:template_project_name, :template_model_name]
       drop_column :template_model_name
       drop_column :template_project_name
