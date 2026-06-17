0b498d5ec7ae18a9920a5468c9a92d8325864927
Amadeo <6462800+amadeovezz@users.noreply.github.com>
2026-06-17 15:26:04 -0700

    minimal deps changes necessary for janus tests

--- janus/spec/spec_helper.rb
+++ janus/spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'etna'
 require 'yaml'
 require 'logger'
 require 'factory_bot'
+require 'database_cleaner'
+require 'database_cleaner/sequel'
 require 'simplecov'
 require 'timecop'
 require 'nokogiri'
@@ -163,12 +165,13 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     FactoryBot.find_definitions
-    db = Janus.instance.db
-    db.tables.each { |t| db[t].truncate(cascade: true) rescue nil }
+    DatabaseCleaner[:sequel].db = Janus.instance.db
+    DatabaseCleaner[:sequel].strategy = :transaction
+    DatabaseCleaner[:sequel].clean_with(:truncation)
   end
 
   config.around(:each) do |example|
-    Janus.instance.db.transaction(rollback: :always, savepoint: true) do
+    DatabaseCleaner[:sequel].cleaning do
       example.run
     end
   end
